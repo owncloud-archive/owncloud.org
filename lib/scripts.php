e8768dbabf9596a33cfb599281952c368df716e2
Marieke <marieke@frogtips.nl>
2014-01-20 20:59:07 +0100

    Font resize on all headers and adjustments to header margins and paddings.
    Further adjustments to News page.
    
    Changed img paths to relative.
    
    Added latest buttons and new splash image.

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '31107d6fa47321fd0f36db34a04d0786');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '73876ee3bdca60f44cab5ea43b8e5aec');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
