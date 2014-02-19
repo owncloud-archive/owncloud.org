c9469d685d6ccc77c98bf30ba17f6638d803944d
Tom Needham <tom@owncloud.com>
2014-02-19 22:11:31 +0100

    Fix homepage: client buttons, contribook columns

--- page-homepage.php
+++ page-homepage.php
@@ -1,3 +1,4 @@
+<?php //require(dirname(__FILE__).'/../../../contribook/main/contribook/lib_contribook.php'); ?>
 <div class="header"><div class="row image"><div class="col-lg-12 col-md-12 col-xs-12 col-sm-12"><img style="display: block; width: 100%; min-width: 320px; margin: 3em auto 10px;" alt="image about owncloud sync" src="/wp-content/themes/owncloudorgnew/assets/img/home/ocsplash.png" />
 </div></div>
 <div class="row">
@@ -25,7 +26,7 @@
 <div class="row" id="getapps">
 <div class="col-sm-12">
 <div class="row">
-<div class="col-sm-8">
+<div class="col-sm-6">
 <h2>Get your ownCloud</h2>
 <p style="margin-bottom: 1em;">Choose <strong>your preferred way</strong> to install your ownCloud Server.</p>
 For server owners:
@@ -34,13 +35,13 @@ For server owners:
 <a id="ebs-button-1" class="btn btn-default btn-lg hostingbtn" href="/providers/"  target="_blank"><i class="icon-search" ></i> Find hosting</a>
 
 </div>
-<div class="col-sm-4">
+<div class="col-sm-6">
 <h2>Sync your files</h2>
 <div style="margin-bottom: 1em;">Sync your files using our ownCloud sync clients for desktop and mobile devices.</div>
 <div class="row" style="padding-top: 0; padding-bottom: 0;">
-<div class="col-md-12" style="margin-bottom: 0.5em;"><a href=" http://owncloud.org/sync-clients/" target="_blank"><img style="width: 160px;" alt="" src="/wp-content/themes/owncloudorgnew/assets/img/clients/buttons/desktopapp.png" /></a></div>
-<div class="col-md-12" style="margin-bottom: 0.5em;"><a id="appstore" href="http://itunes.apple.com/us/app/owncloud/id543672169?ls=1&amp;mt=8" target="_blank"><img style="width: 160px;" alt="" src="/wp-content/themes/owncloudorgnew/assets/img/clients/buttons/appstore.png" /></a></div>
-<div class="col-md-12"><a id="googleplay" href="https://play.google.com/store/apps/details?id=com.owncloud.android" target="_blank"><img style="width: 160px;" alt="" src="/wp-content/themes/owncloudorgnew/assets/img/clients/buttons/googleplay.png" /></a></div>
+<div class="col-md-4" style="margin-bottom: 0.5em;"><a id="desktopclient" href=" http://owncloud.org/sync-clients/" target="_blank"><img style="width: 160px;" alt="" src="/wp-content/themes/owncloudorgnew/assets/img/clients/buttons/desktopapp.png" /></a></div>
+<div class="col-md-4" style="margin-bottom: 0.5em;"><a id="appstore" href="http://itunes.apple.com/us/app/owncloud/id543672169?ls=1&amp;mt=8" target="_blank"><img style="width: 160px;" alt="" src="/wp-content/themes/owncloudorgnew/assets/img/clients/buttons/appstore.png" /></a></div>
+<div class="col-md-4"><a id="googleplay" href="https://play.google.com/store/apps/details?id=com.owncloud.android" target="_blank"><img style="width: 160px;" alt="" src="/wp-content/themes/owncloudorgnew/assets/img/clients/buttons/googleplay.png" /></a></div>
 </div>
 </div>
 </div>
@@ -58,22 +59,17 @@ Sed sapien orci, pretium eu mollis et, volutpat a nisi. Maecenas facilisis sagit
 Curabitur rhoncus vulputate elit, ut ullamcorper justo ornare pretium. Praesent orci sem, pellentesque vitae volutpat sit amet, fringilla sit amet lacus. Curabitur sollicitudin felis sit amet orci placerat, a molestie odio mattis. Proin malesuada, felis eu luctus pulvinar, arcu est aliquam est, a mattis magna sem sit amet leo.
 </div>
 </div>
-<?php
-//require(dirname(__FILE__).'/../../../contribook/main/contribook/lib_contribook.php');
-
-  echo('<table width="100%" border="0" cellpadding="10"><tr><td valign="top">');
-  echo('<h2><i class="icon-twitter"></i> The latest twitter posts</h2>');
-//  CONTRIBOOK_MICROBLOG::show(0,10);
-
-  echo('</td><td valign="top">');
-  echo('<h2><i class="icon-comment"></i> The latest from the forum</h2>');
-  //CONTRIBOOK_FORUM::show(0,10);
-  echo('<h2><i class="icon-cloud"></i> ownCloud.com news</h2>');
-  //CONTRIBOOK_NEWS::show(0,10);
-
-  echo('</td></tr></table>');
-
-
-
-
-?>
+<div class="row 3-column-lg-md-sm">
+	<div class="col-lg-4 col-md-4 col-xs-12 col-sm-4">
+		<h2><i class="icon-twitter"></i> The latest twitter posts</h2>
+		<?php //CONTRIBOOK_MICROBLOG::show(0,10); ?>
+	</div>
+	<div class="col-lg-4 col-md-4 col-xs-12 col-sm-4">
+		<h2><i class="icon-comment"></i> The latest from the forum</h2>
+		<?php //CONTRIBOOK_FORUM::show(0,10); ?>
+	</div>
+	<div class="col-lg-4 col-md-4 col-xs-12 col-sm-4">
+		<h2><i class="icon-cloud"></i> ownCloud.com news</h2>
+		<?php //CONTRIBOOK_NEWS::show(0,10); ?>
+	</div>
+</div>
