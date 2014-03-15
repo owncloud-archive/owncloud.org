ba360134a8d37172d8a24c360126a84f1a54e28e
Tom Needham <needham.thomas@gmail.com>
2014-03-15 14:38:37 +0000

    Load contributor information on contact page

--- page-contact.php
+++ page-contact.php
@@ -9,3 +9,24 @@ For the latest news, subscribe to our <a href="http://owncloud.org/feed/">RSS fe
 
 Also find us on <a href="http://joindiaspora.com/u/owncloud"><img alt="" src="http://owncloud.org/diaspora.png" /> Diaspora</a>, <a href="http://identi.ca/owncloud"><img alt="" src="http://identi.ca/favicon.ico" width="16px" height="16px" /> Identi.ca</a> and <a href="http://twitter.com/ownclouders"><img alt="" src="http://owncloud.org/wp-content/uploads/2012/06/twitter.png" width="16px" height="16px" />Twitter</a>.
 <h2>Active core contributors:</h2>
+<?php
+
+require(dirname(__FILE__).'/../../../contribook/main/contribook/lib_contribook.php');
+
+$users=CONTRIBOOK_USER::getusers();
+echo('<table>');
+foreach($users as $user) {
+	$data = CONTRIBOOK_USER::getuser($user);
+	echo('<tr><td style="padding:5px;">');
+	if($data['picture_50'] <> ''){
+		echo('<span class="contribook_microbloguserpicture"><a href="'.CONTRIBOOK_USER_URL.$user.'"><img src="'.CONTRIBOOK_PHOTO_URL.$data['picture_50'].'" border="0" /></a></span>');
+	}
+	echo('</td><td style="padding:5px;">');
+
+	echo('<a href="'.CONTRIBOOK_USER_URL.$user.'">'.$data['name'].'</a><br />');
+	echo($data['role']);
+
+	echo('</td></tr>');
+}
+
+echo('</table>');
