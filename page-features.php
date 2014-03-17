11e6c09bde0303ac9dcd73c645956a436a9942be
Tom Needham <needham.thomas@gmail.com>
2014-03-17 12:50:57 +0000

    Fix features page pictures

--- page-features.php
+++ page-features.php
@@ -1,8 +1,7 @@
 <div class="row featurerow topfeature">
 <div class="col-md-6"><iframe src="//www.youtube.com/embed/xsqSLeppxm0" height="310" width="95%" style="margin-top:10px;" allowfullscreen="" frameborder="0"></iframe></div>
 <div class="col-md-6">
-<h2 class="featuretitle" style="margin-top: 0;">ownCloud Documents</h2>
-<span class="label label-success label-free">New!</span>
+<h2 class="featuretitle" style="margin-top: 0;">ownCloud Documents<span class="label label-success label-free">New!</span></h2>
 <p style="text-align: justify; margin-top: 10px;">ownCloud Documents is <strong>collaborative editing</strong> of rich-text documents. The documents can be created from within the
 web-interface or existing documents can be uploaded. Sharing and editing can be done securely in the browser and be shared inside ownCloud or via a public link. User that have an account on the same server can be invited or public invitations can also be sent be email. The editing works on top of normal ODF files that are stored in ownCloud. ownCloud Documents is built in cooperation with <a href="http://kogmbh.com/">KO GmbH</a></p>
 <p>Watch a feature overview video here: <a href="https://www.youtube.com/watch?v=70pCBnNPdew">YouTube</a><br />Install and try ownCloud 6 <a href="/install">now</a>.</p>
@@ -12,19 +11,19 @@ web-interface or existing documents can be uploaded. Sharing and editing can be 
 <div class="row featurerow featuresheader">
 <div class="col-md-4 featureblock">
 <h3 class="ft-title">Access Your Data</h3>
-<img class="thumbnail-ft" alt="" src="http://owncloud.org/wp-content/uploads/2013/10/newlogin.png" />
+<img class="thumbnail-ft" alt="" src="<?php bloginfo('template_directory'); ?>/assets/img/features/login-wide.png" />
 <p class="featuredesc">Store your files, folders, contacts, photo galleries, calendars and more on a server of your choosing. Access that folder from your mobile device, your desktop, or a web browser. Access your data wherever you are, when you need it.</p>
 
 </div>
 <div class="col-md-4 featureblock">
 <h3 class="ft-title">Sync Your Data</h3>
-<img class="thumbnail-ft" alt="" src="/wp-content/uploads/2012/05/versioning-feature.png" />
+<img class="thumbnail-ft" alt="" src="<?php bloginfo('template_directory'); ?>/assets/img/features/versioning-wide.png" />
 <p class="featuredesc">Keep your files, contacts, photo galleries, calendars and more synchronized amongst your devices. One folder, two folders and more – get the most recent version of your files with the desktop and web client or mobile app of your choosing, at any time.</p>
 
 </div>
 <div class="col-md-4 featureblock">
 <h3 class="ft-title">Share Your Data</h3>
-<img class="thumbnail-ft" alt="" src="/wp-content/uploads/2012/05/sharing-feature.png" />
+<img class="thumbnail-ft" alt="" src="<?php bloginfo('template_directory'); ?>/assets/img/features/sharing-wide.png" />
 <p class="featuredesc">Share your data with others, and give them access to your latest photo galleries, your calendar, your music, or anything else you want them to see. Share it publicly, or privately. It is your data, do what you want with it.</p>
 </div>
 </div>
@@ -34,19 +33,19 @@ web-interface or existing documents can be uploaded. Sharing and editing can be 
 <div class="row featurerow">
 
 <div class="col-md-4 featureblock">
-<img class="thumbnail" alt="something" src="http://owncloud.org/wp-content/uploads/2013/10/avatar.png" />
+<img class="thumbnail" alt="something" src="<?php bloginfo('template_directory'); ?>/assets/img/features/avatar.png" />
 <h3 class="featuretitle">User Avatars</h3></br>
 <p class="featuredesc">User can upload avatar pictures so that they are easier to recognize by others. This is especially useful for bigger organizations and enterprises.</p>
 </div>
 
 <div class="col-md-4 featureblock">
-<img class="thumbnail" alt="something" src="http://owncloud.org/wp-content/uploads/2013/10/previews.png" />
+<img class="thumbnail" alt="something" src="<?php bloginfo('template_directory'); ?>/assets/img/features/previews.png" />
 <h3 class="featuretitle">File Previews</h3></br>
 <p class="featuredesc">We now show thumbnails of documents in the web-interface in all the relevant places. This helps the users to recognize their files easier.</p>
 </div>
 
 <div class="col-md-4 featureblock">
-<img class="thumbnail" alt="something" src="http://owncloud.org/wp-content/uploads/2013/10/conflict.png" />
+<img class="thumbnail" alt="something" src="<?php bloginfo('template_directory'); ?>/assets/img/features/conflict.png" />
 <h3 class="featuretitle">Conflict Handling</h3></br>
 <p class="featuredesc">A new web conflict dialog if a file is uploaded that already exists. Users have the option to replace or rename a file and compare them with thumbnails.</p>
 </div>
@@ -55,19 +54,19 @@ web-interface or existing documents can be uploaded. Sharing and editing can be 
 <div class="row featurerow">
 
 <div class="col-md-4 featureblock">
-<img class="thumbnail" alt="something" src="/wp-content/uploads/2012/05/themeing-feature.png" />
+<img class="thumbnail" alt="something" src="<?php bloginfo('template_directory'); ?>/assets/img/features/themeing.png" />
 <h3 class="featuretitle">Activity Feed</h3></br>
 <p class="featuredesc">See what is going on in your ownCloud. You can see the latest files and folders that you have created changed or deleted. You can see when someone shared something with you or you shared something with someone and if the shared files are changed. This feed can also be accessed with an RSS feed and can be ready via an REST api by Desktop and Mobile client.</p>
 </div>
 
 <div class="col-md-4 featureblock">
-<img class="thumbnail" alt="something" src="http://owncloud.org/wp-content/uploads/2013/10/newdesign.png" />
+<img class="thumbnail" alt="something" src="<?php bloginfo('template_directory'); ?>/assets/img/features/design.png" />
 <h3 class="featuretitle">New Design</h3></br>
 <p class="featuredesc">Improved design. Less visual clutter and more space for a better focus on your content.</p>
 </div>
 
 <div class="col-md-4 featureblock">
-<img class="thumbnail" alt="something" src="http://owncloud.org/wp-content/uploads/2013/03/oc5undelete.png" />
+<img class="thumbnail" alt="something" src="<?php bloginfo('template_directory'); ?>/assets/img/features/undelete.png" />
 <h3 class="featuretitle">Undelete</h3></br>
 <p class="featuredesc">Finally, now users can undelete a file that was accidentally deleted through the web interface. Simply select the files in the trash bin and they are returned to where they were deleted from, with versions maintained.</p>
 </div>
@@ -78,19 +77,19 @@ web-interface or existing documents can be uploaded. Sharing and editing can be 
 <div class="row featurerow">
 
 <div class="col-md-4 featureblock">
-<img class="thumbnail" alt="something" src="/wp-content/uploads/2012/05/calendar-feature.png" />
+<img class="thumbnail" alt="something" src="<?php bloginfo('template_directory'); ?>/assets/img/features/calendar.png" />
 <h3 class="featuretitle">Calendars</h3></br>
 <p class="featuredesc">Want to share your calendar with other users of ownCloud? Enable the Calendar Application, open your calendar, select share, and choose the users or groups you want. You can be sharing your important calendar and important events in no time.</p>
 </div>
 
 <div class="col-md-4 featureblock">
-<img class="thumbnail" alt="something" src="http://owncloud.org/wp-content/uploads/2013/03/oc5external.png" />
+<img class="thumbnail" alt="something" src="<?php bloginfo('template_directory'); ?>/assets/img/features/external.png" />
 <h3 class="featuretitle">External Storage</h3></br>
 <p class="featuredesc">Mounting DropBox, Swift, FTPs, GoogleDocs, S3 and external WebDAV servers now works, and is significantly more performant than it used to be.</p>
 </div>
 
 <div class="col-md-4 featureblock">
-<img class="thumbnail" alt="something" src="/wp-content/uploads/2012/05/gallery-feature.png" />
+<img class="thumbnail" alt="something" src="<?php bloginfo('template_directory'); ?>/assets/img/features/gallery.png" />
 <h3 class="featuretitle">Galleries</h3></br>
 <p class="featuredesc">Want better control over your shared photo gallery? Now you can specify the ownCloud photo directories, sort order, share your galleries with any email address you choose, and control whether they can share those photos with anyone else.</p>
 </div>
@@ -99,19 +98,19 @@ web-interface or existing documents can be uploaded. Sharing and editing can be 
 <div class="row featurerow">
 
 <div class="col-md-4 featureblock">
-<img class="thumbnail" alt="something" src="/wp-content/uploads/2012/05/themeing-feature.png" />
+<img class="thumbnail" alt="something" src="<?php bloginfo('template_directory'); ?>/assets/img/features/themeing.png" />
 <h3 class="featuretitle">Themeing</h3></br>
 <p class="featuredesc">Want to make ownCloud look and feel like the rest of your site? Use the new theming directory functionality. Any style or image that you place in this directory will be used in place of standard ownCloud fonts, colors and icons.</p>
 </div>
 
 <div class="col-md-4 featureblock">
-<img class="thumbnail" alt="something" src="http://owncloud.org/wp-content/uploads/2013/03/oc5external.png" />
+<img class="thumbnail" alt="something" src="<?php bloginfo('template_directory'); ?>/assets/img/features/external.png" />
 <h3 class="featuretitle">PDF Viewer</h3></br>
 <p class="featuredesc">Want to read PDF files without downloading them? Enable this Application and you can click on any PDF document and read it in your web browser with no download required.</p>
 </div>
 
 <div class="col-md-4 featureblock">
-<img class="thumbnail" alt="something" src="/wp-content/uploads/2012/05/api-feature.png" />
+<img class="thumbnail" alt="something" src="<?php bloginfo('template_directory'); ?>/assets/img/features/api.png" />
 <h3 class="featuretitle">Public API's</h3></br>
 <p class="featuredesc">Want to add features and functions to ownCloud? New publicly defined APIs make creating applications for ownCloud much simpler, enabling add on functions a stable integration point for future versions.</p>
 </div>
@@ -120,19 +119,19 @@ web-interface or existing documents can be uploaded. Sharing and editing can be 
 <div class="row featurerow">
 
 <div class="col-md-4 featureblock">
-<img class="thumbnail" alt="something" src="/wp-content/uploads/2012/05/backup-feature.png" />
+<img class="thumbnail" alt="something" src="<?php bloginfo('template_directory'); ?>/assets/img/features/backup.png" />
 <h3 class="featuretitle">Migration and Backup</h3></br>
 <p class="featuredesc">Do you have multiple ownCloud instances, perhaps a primary and a backup installation? Now you can easily move your ownCloud user accounts between ownCloud instances, and have a backup ready when you need it.</p>
 </div>
 
 <div class="col-md-4 featureblock">
-<img class="thumbnail" alt="something" src="/wp-content/uploads/2012/05/tasks-feature.png" />
+<img class="thumbnail" alt="something" src="<?php bloginfo('template_directory'); ?>/assets/img/features/tasks.png" />
 <h3 class="featuretitle">Tasks</h3></br>
 <p class="featuredesc">Want to keep track of that all-important to do list? With the Tasks Application, you can easily sync your to do lists with your ownCloud instance.</p>
 </div>
 
 <div class="col-md-4 featureblock">
-<img class="thumbnail" alt="something" src="/wp-content/uploads/2012/05/apps-feature.png" />
+<img class="thumbnail" alt="something" src="<?php bloginfo('template_directory'); ?>/assets/img/features/apps.png" />
 <h3 class="featuretitle">Application Store</h3></br>
 <p class="featuredesc">Want to add one of the existing applications to ownCloud? Simply enable a new application in settings, and it will be automatically downloaded and installed in your ownCloud instance.</p>
 </div>
