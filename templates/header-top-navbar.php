03b0584d952cc5caa4f2413765c1eb87fa2d5afa
Tom Needham <tom@owncloud.com>
2014-05-13 09:28:49 +0100

    Add contribute to the menu

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -16,6 +16,7 @@
         <li class="menu-features"><a href="/features/">Features</a></li>
         <li class="menu-demo"><a href="http://demo.owncloud.org">Demo</a></li>
         <li class="menu-documentation"><a href="http://doc.owncloud.org">Documentation</a></li>
+        <li class="menu-contribute"><a href="/contribute/">Contribute</a>
         <li class="menu-commercial"><a target="_blank" href="https://owncloud.com">Enterprise Edition <span style="font-size: 16px">&#x2197;</span></span></a></li>
         <li class="menu-install"><a href="/install/">Install</a></li>
       </ul>
