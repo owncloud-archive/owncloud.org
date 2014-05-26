789020c55f262deb8aa4835ede0d3bdcfa7e076f
Jos Poortvliet <jospoortvliet@gmail.com>
2014-05-26 19:57:07 +0200

    add first text to conference page

--- page-conference-summer-2014.php
+++ page-conference-summer-2014.php
@@ -1,14 +1,23 @@
 <div class="page-header">
 	<h1><a href="/events">Events</a> > ownCloud Summer Conference 2014</h1>
 </div>
-<h2>Introduction</h2>
-<p>Woo conference. Yay conference.</p>
+<h2>Welcome to the ownCloud Contributor Conference!</h2>
+<p>This is where you find out what is going on in ownCloud, learn how to change ownCloud, be inspired by fellow ownClouders and build your ownCloud!</p>
+<p>With millions of users, ownCloud is THE open source file sync and sharing solution and that is because of its awesome contributors. For the second time, they will gather at the TU Berlin for 5 days of writing code and sharing beer, knowledge and inspiration.</p>
 <h2>Where and when?</h2>
-<p>Berlin?</p>
+<p>Our conference takes place at the TU Berlin, XXXXXX (address).</p>
+<p>The coding starts on Wednesday 27th of August and continues until the 31st. Saturday the 30th features a conference track with keynotes, lightning talks and workshops, providing insight in ownCloud and a chance to learn new skills.</p>
 <iframe width="100%" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.openstreetmap.org/export/embed.html?bbox=13.1890869140625%2C52.43445541622349%2C13.627166748046875%2C52.61159566845733&amp;layer=mapnik&amp;marker=52.52311483252328%2C13.408126831054688"></iframe><br/><small><a href="http://www.openstreetmap.org/?mlat=52.5231&amp;mlon=13.4081#map=12/52.5231/13.4081">View Larger Map</a></small>
 <h2>Activities</h2>
+<p>Only on Saturday the 30th we feature a program, the rest of the days are just a pure, undilluted coding sprint. We only stop for food and sleep!</p>
+<p>a <strong>Keynote</strong> by XYZ opens the ownCloud Contributor Conference day on Saturday.</p>
+<p>In our lightning tracks, we share what is going on in the ownCloud apps like Read Later, Calendar, News, Notes, Music, Documents, Activity and many more.</p>
+<p>Afteroon workshops include:</p>
 <ul>
-	<li>Hacking</li>
-	<li>Eating</li>
-	<li>Hacking</li>
+	<li>Learn to write your first ownCloud App</li>
+	<li>Secure your PHP application</li>
+	<li>Learn how to write code tests</li>
+	<li>Connect your JS app to ownCloud</li>
+	<li>Add the ownCloud Share API to your application</li>
 </ul>
+
