7546df8f409d914e0509d52ca50e14db1b262d12
Tom Needham <tom@owncloud.com>
2014-06-18 15:36:19 +0100

    Again tweak homepage columns

--- page-homepage.php
+++ page-homepage.php
@@ -85,7 +85,7 @@
 	</div>
 	<div class="col-lg-4 col-md-4 col-xs-12 col-sm-4">
 		<h2><i class="icon-comment"></i> The latest from the forum</h2>
-		<?php if(CONTRIBOOK) { CONTRIBOOK_FORUM::show(0,5); }?>
+		<?php if(CONTRIBOOK) { CONTRIBOOK_FORUM::show(0,7); }?>
 	</div>
 	<div class="col-lg-4 col-md-4 col-xs-12 col-sm-4">
 		<h2><i class="icon-cloud"></i> ownCloud.com news</h2>
