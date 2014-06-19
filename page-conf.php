738d80bcd5d151e8ef952ae0509480c7373f2175
Tom Needham <tom@owncloud.com>
2014-06-19 16:53:58 +0100

    Adjust layout to bootstrap 3

--- page-conf.php
+++ page-conf.php
@@ -2,8 +2,8 @@
 	<img class="conf-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/conf-logo.svg" alt="logo" />
 	<h2 class="conf-location">August 2014, Berlin</h2>
 </div>
-<div class="row conf-info">
-	<div class="span6">
+<div class="row 2-column-lg-md-sm conf-info">
+	<div class="col-lg-6 col-md-6 col-xs-12 col-sm-6">
 		<h1>Hackathon</h1>
 		<h3>August 26-31</h3>
 		<p>For the second time, the awesome community behind ownCloud will get together at the TU Berlin for 5 days of writing code and sharing beer, knowledge and inspiration. We will get work done:</p>
@@ -15,7 +15,7 @@
 			<li>Marketing &amp; Promo</li>
 		</ul>
 	</div>
-	<div class="span6">
+	<div class="col-lg-6 col-md-6 col-xs-12 col-sm-6">
 		<h1>Techtalks &amp; Workshops</h1>
 		<h3>August 30</h3>
 		<p>The Conference Day on Saturday the 30th provides a chance to learn and improve your coding skills on and around ownCloud.</p>
@@ -30,21 +30,21 @@
 		</ul>
 	</div>
 </div>
-<div class="row conf-info">
-	<div class="span6">
+<div class="row 2-column-lg-md-sm conf-info">
+	<div class="col-lg-6 col-md-6 col-xs-12 col-sm-6">
 		<h2>Location</h2>
 		<p>Our conference takes place in hipster Berlin at the <em>Technische Universität Berlin</em>, in the "Elektrotechnische Institute" building at Einsteinufer 17, Charlottenburg, 10587 Berlin.</p>
 		<iframe width="100%" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.openstreetmap.org/export/embed.html?bbox=13.32318127155304%2C52.51362881752727%2C13.330262303352356%2C52.5165081342206&amp;layer=mapnik&amp;marker=52.5150684994582%2C13.326721787452698"></iframe><br/><small><a href="http://www.openstreetmap.org/?mlat=52.51507&amp;mlon=13.32672#map=18/52.51507/13.32672">View Larger Map</a></small>
 	</div>
-	<div class="span6">
+	<div class="col-lg-6 col-md-6 col-xs-12 col-sm-6">
 		<h2>Register</h2>
 		<p>The ownCloud Contributor Conference is entirely open. We do ask you to register to help us plan better for the event. Please register <a href="https://conference.owncloud.org/conference/OCC14/register">here</a>.</p>
 		<h2 class="conf-acc">Berlin and accommodation</h2>
 		<p>Berlin is a great place and it is easy to find a place to sleep. You can find options online, for example at <a href="http://www.tripadvisor.com/SmartDeals-g187323-Berlin-Hotel-Deals.html">trip advisor</a> and <a href="http://www.lonelyplanet.com/germany/berlin/hotels/best-places-to-stay-in-berlin">lonely planet</a>. We're looking for a convenient hotel option close to the venue and we'll put some details here once we have them.</p>
 	</div>
 </div>
-<div class="row conf-about">
-	<div class="span8 offset2">
+<div class="row 2-column-lg-md-sm conf-about">
+	<div class="col-lg-6 col-md-6 col-xs-12 col-sm-6 .col-md-offset-3 .col-lg-offset-3">
 		<h2>About</h2>
 		<p>With 1.3 million users, ownCloud is THE open source file sync and sharing solution. Interested in getting involved? Check out <a href="http://owncloud.org/contribute">owncloud.org/contribute</a> and get on it!</p>
 		<strong>Thanks!</strong>
