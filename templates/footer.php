f9c9a6f8fa01974c55231c58b8f0df5590349d88
Tom Needham <tom@owncloud.com>
2014-06-25 19:46:31 +0100

    Fix docs links, contact -> people, fix ml link, remove security link

--- templates/footer.php
+++ templates/footer.php
@@ -5,7 +5,7 @@
         <div class="footer-nav">
           <h4>About ownCloud</h4>
           <ul id="menu-about" class="menu">
-            <li class="menu-contact"><a href="<?php get_bloginfo('url'); ?>/contact/">Contact</a></li>
+            <li class="menu-contact"><a href="<?php get_bloginfo('url'); ?>/contact/">People</a></li>
             <li class="menu-history"><a href="<?php get_bloginfo('url'); ?>/history/">History</a></li>
           </ul>
         </div>
@@ -16,11 +16,10 @@
           <ul id="menu-support-and-documentation" class="menu">
             <li class="menu-admin-manual"><a href="<?php echo $DOCUMENTATION_ADMIN; ?>">Admin manual</a></li>
             <li class="menu-user-manual"><a href="<?php echo $DOCUMENTATION_USER; ?>">User manual</a></li>
-            <li class="menu-security"><a href="<?php get_bloginfo('url'); ?>/security/">Security</a></li>
             <li class="dropdown menu-resources"><a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">Resources <b class="caret"></b></a>
               <ul class="dropdown-menu">
                 <li class="menu-irc-channel"><a href="http://webchat.freenode.net/?channels=owncloud">IRC Channel</a></li>
-                <li class="menu-mailing-list"><a href="https://mail.kde.org/mailman/listinfo/owncloud">Mailing List</a></li>
+                <li class="menu-mailing-list"><a href="http://mailman.owncloud.org/mailman/listinfo/user">Mailing List</a></li>
                 <li class="menu-forums"><a href="http://forum.owncloud.org/">Forums</a></li>
                 <li class="menu-bug-tracker"><a href="https://github.com/owncloud/core/issues">Bug Tracker</a></li>
               </ul>
