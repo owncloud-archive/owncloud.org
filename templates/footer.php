d34fbfb34a8df1c45f7c9f62d9286186f1999131
Tom Needham <tom@owncloud.com>
2014-06-27 00:17:10 +0100

    Reorder footer, add social icons

--- templates/footer.php
+++ templates/footer.php
@@ -7,34 +7,34 @@
           <ul id="menu-about" class="menu">
             <li class="menu-contact"><a href="<?php get_bloginfo('url'); ?>/contact/">People</a></li>
             <li class="menu-history"><a href="<?php get_bloginfo('url'); ?>/history/">History</a></li>
+            <li class="menu-security"><a href="<?php get_bloginfo('url'); ?>/security/">Security</a></li>
+            <li class="menu-design"><a href="<?php get_bloginfo('url'); ?>/contribute/design/">Design</a></li>
           </ul>
         </div>
      </div>
      <div class="col-sm-4">
         <div class="footer-nav">
-          <h4>Support &#38; Documentation</h4>
+          <h4>Resources</h4>
           <ul id="menu-support-and-documentation" class="menu">
             <li class="menu-admin-manual"><a href="<?php echo $DOCUMENTATION_ADMIN; ?>">Admin manual</a></li>
             <li class="menu-user-manual"><a href="<?php echo $DOCUMENTATION_USER; ?>">User manual</a></li>
-            <li class="dropdown menu-resources"><a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">Resources <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li class="menu-irc-channel"><a href="http://webchat.freenode.net/?channels=owncloud">IRC Channel</a></li>
-                <li class="menu-mailing-list"><a href="http://mailman.owncloud.org/mailman/listinfo/user">Mailing List</a></li>
-                <li class="menu-forums"><a href="http://forum.owncloud.org/">Forums</a></li>
-                <li class="menu-bug-tracker"><a href="https://github.com/owncloud/core/issues">Bug Tracker</a></li>
-              </ul>
-            </li>
+            <li class="menu-user-manual"><a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>">Developer manual</a></li>
           </ul>
         </div>
      </div> 
      <div class="col-sm-4">
         <div class="footer-nav">
-          <h4>Developer Resources</h4>
-          <ul id="menu-developer" class="menu">
-            <li class="menu-contribute"><a href="<?php get_bloginfo('url'); ?>/contribute/">Contribute</a></li>
-            <li class="menu-security"><a href="<?php get_bloginfo('url'); ?>/security/">Security</a></li>
-            <li class="menu-design"><a href="<?php get_bloginfo('url'); ?>/contribute/design/">Design</a></li>
-            <li class="menu-developer-docs"><a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>">Developer docs</a></li>
+          <h4>Interact</h4>
+          <ul id="menu-interact" class="menu">
+            <li class="social-icons">
+                <a href="https://plus.google.com/102805591550379137055/about"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/social/googleplus.png"></img></a>
+                <a style="margin: 0 10px;" href="https://www.facebook.com/owncloud"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/social/facebook.png"></img></a>
+               <a href="https://twitter.com/ownClouders"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/social/twitterround.png"></img></a>
+            </li>
+            <li class="menu-irc-channel"><a href="http://webchat.freenode.net/?channels=owncloud">IRC Channel</a></li>
+            <li class="menu-mailing-list"><a href="http://mailman.owncloud.org/mailman/listinfo/user">Mailing List</a></li>
+            <li class="menu-bug-tracker"><a href="https://github.com/owncloud/core/issues">Bug Tracker</a></li>
+            <li class="menu-forums"><a href="http://forum.owncloud.org/">Forums</a></li>
           </ul>
         </div>
      </div>
