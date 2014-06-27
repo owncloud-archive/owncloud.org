49a922a95e618b22802627a4a6c575516f07fa47
Jos Poortvliet <jospoortvliet@gmail.com>
2014-06-27 22:46:01 +0200

    Move icons to bottom

--- templates/footer.php
+++ templates/footer.php
@@ -26,11 +26,6 @@
         <div class="footer-nav">
           <h4>Interact</h4>
           <ul id="menu-interact" class="menu">
-            <li class="social-icons">
-                <a href="https://plus.google.com/102805591550379137055/about"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/social/googleplus.png"></img></a>
-                <a style="margin: 0 10px;" href="https://www.facebook.com/owncloud"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/social/facebook.png"></img></a>
-               <a href="https://twitter.com/ownClouders"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/social/twitterround.png"></img></a>
-            </li>
             <li class="menu-irc-channel"><a href="http://webchat.freenode.net/?channels=owncloud">IRC Channel</a></li>
             <li class="menu-mailing-list"><a href="http://mailman.owncloud.org/mailman/listinfo/user">Mailing List</a></li>
             <li class="menu-bug-tracker"><a href="https://github.com/owncloud/core/issues">Bug Tracker</a></li>
@@ -41,7 +36,10 @@
    </div>
    <div class="row">
     <div class="col-lg-12 footer-text">
-        <p>&copy; <?php echo date('Y'); ?> ownCloud.org</p>
+        <p><font face="sans"> &copy; </font> <?php echo date('Y'); ?> ownCloud.org</p>
+               <a href="https://plus.google.com/102805591550379137055/about"><img width=50 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/googleplus.png"></img></a>
+               <a style="margin: 0 10px;" href="https://www.facebook.com/owncloud"><img width=50 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/facebook.png"></img></a>
+               <a href="https://twitter.com/ownClouders"><img width=50 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/twitterround.png"></img></a>
     </div>
   </div>
 </div>
