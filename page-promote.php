6d83a52b200e99e4ed8e4cc12f938628ec5d1f70
Tom Needham <tom@owncloud.com>
2014-07-03 00:03:49 +0100

    Remove unnecessary text

--- page-promote.php
+++ page-promote.php
@@ -1,12 +1,11 @@
 <div class="page-header">
 	<h1>Promote ownCloud</h1>
 </div>
-<p>Help spread the word about ownCloud.</p>
 <div class="row">
 	<div class="col-md-12 col-sm-4 promote-buttons" style="text-align: center">
-		<a style="width: 300px;" target="_blank" href="https://twitter.com/intent/tweet/?url=https%3A%2F%2Fowncloud.org&text=Check%20out%20ownCloud%20for%20open%20source%20data%20sync%2C%20sharing%20and%20more!&via=owncloud&hashtags=owncloud,opensource" class="btn btn-info btn-lg"><i class="icon-twitter"></i> Tweet about ownCloud</a>
-		<a style="width: 300px; margin: 0 13px;" target="_blank" href="https://www.facebook.com/sharer.php?u=owncloud.org" class="btn btn-primary btn-lg facebook-button"><i class="icon-facebook"></i> Share on Facebook</a>
-		<a style="width: 300px" target="_blank" href="https://plus.google.com/share?url=https%3A%2F%2Fowncloud.org" class="btn btn-danger btn-lg"><i class="icon-google-plus"></i> Post on Google+</a>
+		<a style="width: 250px;" target="_blank" href="https://twitter.com/intent/tweet/?url=https%3A%2F%2Fowncloud.org&text=Check%20out%20ownCloud%20for%20open%20source%20data%20sync%2C%20sharing%20and%20more!&via=owncloud&hashtags=owncloud,opensource" class="btn btn-info btn-lg"><i class="icon-twitter"></i> Tweet about ownCloud</a>
+		<a style="width: 250px; margin: 0 13px;" target="_blank" href="https://www.facebook.com/sharer.php?u=owncloud.org" class="btn btn-primary btn-lg facebook-button"><i class="icon-facebook"></i> Share on Facebook</a>
+		<a style="width: 250px" target="_blank" href="https://plus.google.com/share?url=https%3A%2F%2Fowncloud.org" class="btn btn-danger btn-lg"><i class="icon-google-plus"></i> Post on Google+</a>
 	</div>
 </div>
 <div class="row">
