28a0bc9c63e7a2b17df7df485facd69e3031f70f
Tom Needham <tom@owncloud.com>
2014-07-02 16:16:33 +0100

    Add rss icon, remove redundant css, move to above footer nag

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,11 @@
+<div class="row">
+    <div class="col-lg-12 footer-social-icons">
+      <a href="https://plus.google.com/102805591550379137055/about"><img width=50 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/googleplus.png"></img></a>
+      <a style="margin: 0 7px 0 14px;" href="https://www.facebook.com/ownClouders"><img width=50 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/facebook.png"></img></a>
+      <a style="margin: 0 14px 0 7px;"href="https://twitter.com/ownClouders"><img width=50 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/twitterround.png"></img></a>
+      <a href="https://owncloud.org/blogfeed"><img class="img-circle" width=50 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/rss.png"></img></a>
+    </div>
+  </div>
 <footer class="content-info" role="contentinfo">
 <div class="container">   
    <div class="row">
@@ -36,11 +44,7 @@
    </div>
    <div class="row">
     <div class="col-lg-12 footer-text">
-        <p><font face="sans"> &copy; </font> <?php echo date('Y'); ?> ownCloud.org</p>
-               <a href="https://plus.google.com/102805591550379137055/about"><img width=50 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/googleplus.png"></img></a>
-               <a style="margin: 0 10px;" href="https://www.facebook.com/ownClouders"><img width=50 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/facebook.png"></img></a>
-               <a href="https://twitter.com/ownClouders"><img width=50 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/twitterround.png"></img></a>
-    </div>
+      <p><font face="sans"> &copy; </font> <?php echo date('Y'); ?> ownCloud.org</p>
   </div>
 </div>
 </footer>
