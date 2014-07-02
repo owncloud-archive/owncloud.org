945b8321d1929e540e96cf551a49bc3062d164f2
Tom Needham <tom@owncloud.com>
2014-07-03 00:00:34 +0100

    Updated promotion page layout

--- page-promote.php
+++ page-promote.php
@@ -3,18 +3,23 @@
 </div>
 <p>Help spread the word about ownCloud.</p>
 <div class="row">
-	<div style="text-align: center" class="col-md-12">
-		<a style="width: 300px; margin-bottom: 10px" target="_blank" href="https://twitter.com/intent/tweet/?url=https%3A%2F%2Fowncloud.org&text=Check%20out%20ownCloud%20for%20open%20source%20data%20sync%2C%20sharing%20and%20more!&via=owncloud&hashtags=owncloud,opensource" class="btn btn-info btn-lg"><i class="icon-twitter"></i> Tweet about ownCloud</a></br>
-		<a style="width: 300px; margin-bottom: 10px;" target="_blank" href="https://www.facebook.com/sharer.php?u=owncloud.org" class="btn btn-primary btn-lg"><i class="icon-facebook"></i> Share on Facebook</a></br>
-	<a style="width: 300px" target="_blank" href="https://plus.google.com/share?url=https%3A%2F%2Fowncloud.org" class="btn btn-danger btn-lg"><i class="icon-google-plus"></i> Post on Google+</a>
+	<div class="col-md-12 col-sm-4 promote-buttons" style="text-align: center">
+		<a style="width: 300px;" target="_blank" href="https://twitter.com/intent/tweet/?url=https%3A%2F%2Fowncloud.org&text=Check%20out%20ownCloud%20for%20open%20source%20data%20sync%2C%20sharing%20and%20more!&via=owncloud&hashtags=owncloud,opensource" class="btn btn-info btn-lg"><i class="icon-twitter"></i> Tweet about ownCloud</a>
+		<a style="width: 300px; margin: 0 13px;" target="_blank" href="https://www.facebook.com/sharer.php?u=owncloud.org" class="btn btn-primary btn-lg facebook-button"><i class="icon-facebook"></i> Share on Facebook</a>
+		<a style="width: 300px" target="_blank" href="https://plus.google.com/share?url=https%3A%2F%2Fowncloud.org" class="btn btn-danger btn-lg"><i class="icon-google-plus"></i> Post on Google+</a>
 	</div>
 </div>
 <div class="row">
-	<div class="col-md-12">
-		<ul>
-			<li>Write a blog post, and get it included in our <a href="/news">planet site</a> (ask <a href="/user/?user=tomneedham">Tom Needham</a>)</li>
-			<li>Organise an ownCloud event (talks, meetups, tradeshow booths)</li>
-			<li>Recommend ownCloud to your company, University or service provider</a>
-		</ul>
+	<div class="col-md-4">
+		<h2>Write a blog post</h2>
+		<p>Share your experiences with ownCloud in a blog post, and get it included in our <a href="/news/">planet site</a> (contact <a href="/user/>user=tomneedham">Tom Needham</a>).</p>
+	</div>
+	<div class="col-md-4">
+		<h2>Organise an event</h2>
+		<p>Talks, meetups, or even tradeshow booths.
+	</div>
+	<div class="col-md-4">
+		<h2>Recommend ownCloud</h2>
+		<p>Speak to friends, family, colleagues, or your existing service providers and recommend ownCloud.</p>
 	</div>
 </div>
