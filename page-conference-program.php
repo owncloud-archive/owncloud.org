fee152309215073fa84398690d1a136445ee12c8
Tom Needham <tom@owncloud.com>
2014-07-04 21:16:35 +0100

    Add WIP events pages

---
+++ page-conference-program.php
@@ -0,0 +1,69 @@
+<div class="page-header">
+	<h1><a href="/events">Events</a> > Conference Program</h1>
+</div>
+<div class="sub-nav"><a href="/events/meetups">Meetups</a>&nbsp;&nbsp;|&nbsp;&nbspConference Program</div>
+<div class="row">
+	<div class="col-md-12">
+		<p>ownCloud community members all around the globe organize ownCloud presence at trade shows and conferences to present our community and technology. 
+			You want to organize ownCloud attendance to an event? You're in the right place! Below you will find some practical tips on how to organize attendance 
+			to an event as well as how to tell us about it and how to get some ownCloud goodies!</p>
+
+		<h2>ownCloud at conferences and trade shows</h2>
+		<p>As ownCloud appeals to both home users and businesses, we can be found at a wide variety of conferences and trade shows.</p>
+
+		<h3>Giving a presentation or workshop</h3>
+		<p>If you want to give a presentation or workshop on ownCloud, you can get some ideas for subjects and slides <a href="">here</a>. There are also 
+			presentation templates which you can adapt to your needs.</p>
+
+		<h2>Organizing a booth</h2>
+		<p>Going to a trade show or conference for ownCloud is not hard. All you need are one or more volunteers and some positive energy!</p>
+
+		<h3>Tell everybody about the booth!</h3>
+		<p>Share the ownCloud attendance to the event over social media - set up an event page on Facebook and Google plus, ask people to share the news. </p>
+			And tell us about it!</p>
+
+		<h3>Setting up a booth</h3>
+		<p>A crucial thing when setting up a booth is to create open space. This is very hard, as many events put a row of tables in front of the visitors 
+			and it is of course easy to go and sit behind them. But that is hardly an effective strategy. Instead, put the table back against the wall if you 
+			can, and stand in front. That is much nicer for visitors! Nobody likes to be the first person at a booth, and if you and your booth friends are 
+			standing in front, it seems there is always something going on.</p>
+
+		<p>Also, don't hack at the booth unless you are giving a workshop. It makes it seem very uninviting.</p>
+
+		<p>On the other hand, having a screen with ownCloud in action is of course good - you can demo it to people! Having something 'interesting' happen 
+			there is good, too. A video of ownCloud in action, for example. Get one <a href="">here</a>.</p>
+
+		<h3>Promoting the booth</h3>
+		<p>Get people to the booth! You can put some leaflets or hang up some posters around the venue, and if you give a talk you should certainly 
+			mention that there is an ownCloud booth.</p>
+
+		<h3>Handeling visitors</h3>
+		<p>There are four kind of visitors you get at the booth: newbies, advanced users, business users and (potential) contributors. You're there to get 
+			newbies interested in ownCloud, answer questions of the advanced users and convince the (potential) business users that their company needs 
+			ownCloud! Last but not least, you should convince (potential) contributors to get on board with ownCloud hacking: it is fun and easy.</p>
+
+		<p>To help you talk about ownCloud, we have a <a href="">conference team FAQ</a> and some <a href="">conference talking points</a>. Print these pages, 
+			read them and bring them for the others on the booth to read in a quiet moment. Of course, use our flyers, posters and other materials. You 
+			can <a href="https://github.com/owncloud/promo">download things here</a> and <a href="">request some materials here</a>.</p>
+
+		<h3>Tips</h3>
+		<p>Always approach people with a question: do you know ownCloud, do you use dropbox or Google drive, do you share files with other people... Find out 
+			what their problem is and then tell them how ownCloud can help them. If they have no problem or ownCloud can't help, well, that's it. No 
+			reason to make them listen to anything they won't be interested in, while you could be talking to people who DO need ownCloud!</p>
+
+		<p>If you encounter negativity or users with problems, of course try to help them. But don't spend too much time on it - rather, ask them to ask on the forums 
+			or on the mailing list. You sure won't be able to solve their complicated problem on the spot and there are lots of other people who want to hear about 
+			ownCloud. Nothing is perfect, neither is ownCloud - but we are best at dealing with that online, rather than at events.</p>
+
+		<p>Organize little talks or workshops at the booth. Like "building your first ownCloud app". They don't attract a lot of people, but that means you get 
+			to spend some quality time with a few!</p>
+
+		<h2>Getting some goodies</h2>
+		<p>We can send you some materials like stickers and flyers for your booth. We have limited supply and shipping takes time, so ask us in time! Click here for a request 
+			form to fill in.</p>
+
+		<h2>Telling us about it</h2>
+		<p>It is great if you share what you did at the conference or trade show with the international community!</p>
+
+	</div>
+</div>
