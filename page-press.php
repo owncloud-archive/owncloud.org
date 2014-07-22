10d518336ce13fd9ed2d225db3dfdbba431b47d7
Jos Poortvliet <jospoortvliet@gmail.com>
2014-07-22 12:18:29 +0200

    implement changes requested by Tom

--- page-press.php
+++ page-press.php
@@ -2,15 +2,15 @@
 <h2>Latest News and Annoucements</h2>
 <?php wp_get_archives( array( 'type' => 'postbypost', 'limit' => 5, 'format' => 'html' ) ); ?>
 <h2>Current Release Information <small>Version 7.0</small></h2>
-<a href="http://owncloud.org/blog/owncloud-7-released-with-more-sharing-and-control" class="btn btn-lg btn-default">Annoucement</a>
-<a href="http://owncloud.org/features" class="btn btn-lg btn-default">Key Features</a>
-<a href="http://owncloud.org/install" class="btn btn-lg btn-default">Download</a>
+<a href="/blog/owncloud-7-released-with-more-sharing-and-control" class="btn btn-lg btn-default">Annoucement</a>
+<a href="/features" class="btn btn-lg btn-default">Key Features</a>
+<a href="/install" class="btn btn-lg btn-default">Download</a>
 <h2>ownCloud Resources</h2>
 <div class="row">
 	<div class="col-md-3"><a href="https://github.com/owncloud/screenshots/archive/stable7.zip"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/screenshots/screenshots.png" style="width: 100%" alt="Download a zip file with screenshots" class="img-thumbnail"></a></div>
 	<div class="col-md-3"><a href="<?php echo get_template_directory_uri(); ?>/assets/files/logo_files.zip"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/screenshots/logofiles.png" style="width: 100%" alt="Download a zip file with logo files" class="img-thumbnail"></a></div>
 	<div class="col-md-3"><a href="http://www.youtube.com/ownclouders"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/screenshots/youtube.png" style="width: 100%" alt="View the ownCloud community youtube channel" class="img-thumbnail"></a></div>
-	<div class="col-md-3"><a href="http://owncloud.org/7"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/screenshots/newfeatures.png" style="width: 100%" alt="See what is new in ownCloud 7" class="img-thumbnail"></a></div>
+	<div class="col-md-3"><a href="/7"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/screenshots/newfeatures.png" style="width: 100%" alt="See what is new in ownCloud 7" class="img-thumbnail"></a></div>
 </div>
 
 
