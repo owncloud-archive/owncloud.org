f39dc9af01cece1dac03d417fc943cabd4ed1803
Tom Needham <tom@owncloud.com>
2014-07-23 05:12:31 +0100

    Update feature video

--- page-features.php
+++ page-features.php
@@ -1,6 +1,6 @@
 <div class="row featurerow topfeature">
 	<div class="col-md-5">
-		<iframe src="//www.youtube.com/embed/xsqSLeppxm0" height="310" width="95%" style="margin-top:10px;" allowfullscreen="" frameborder="0"></iframe>
+		<iframe src="//player.vimeo.com/video/101482326" height="310" width="95%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> 
 	</div>
 	<div class="col-md-7">
 		<h2 class="featuretitle" style="margin-top: 0;">Access, Sync and Share your data, under your control!</h2>
