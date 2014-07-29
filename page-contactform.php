d369662ca587529531e0ff760dbcd5b66511b0dd
Jos Poortvliet <jospoortvliet@gmail.com>
2014-07-29 18:29:05 +0200

    change wording courtesy of @kelleybrooks

--- page-contactform.php
+++ page-contactform.php
@@ -6,7 +6,7 @@
   |&nbsp;&nbsp;<a href="/events/conference-program">Conference Program</a>
   |&nbsp;&nbsp;Contact
 </div></br>
-<p>Inform us about your release party, conference or meetup, request materials or ask for help. Include links to blogs, pictures or the conference if you can!</p>
+<p>Planning a release party, conference or Meetup? Let us know the details – dates, links, session details, etc – and we’ll promote it on the Events page.</p>
 <p><strong>Note:</strong> if you request materials, use the <a href="<?php echo get_template_directory_uri(); ?>/assets/files/request_form.pdf">request form</a> and make sure you fill in as many details as possible!</p>
 <form action="#" method="post" id="messageform">
   <div class="form-group">
