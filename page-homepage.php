524b06ec5a86d91966820296dcf1e71653e4b2f5
Tom Needham <tom@owncloud.com>
2014-08-19 14:10:59 +0100

    WIP Homepage layout fixes

--- page-homepage.php
+++ page-homepage.php
@@ -1,4 +1,9 @@
 <?php if(CONTRIBOOK) { require(dirname(__FILE__).'/../../../contribook/main/contribook/lib_contribook.php'); } ?>
+<div class="row" style="float: left;
+text-align: center;
+width: 100%;">
+	<h2 class="slogan">Your data - under your control</h2>
+</div>
 <a href="/features">
 <div class="header"><div class="row image">
 	<div class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
@@ -6,9 +11,6 @@
 	</div>
 </div>
 </a>
-<div class="row">
-	<h2 class="slogan">Your data - under your control</h2>
-</div>
 <div class="row">
 	<div class="col-md-10 col-md-offset-1">
 		<p class="explain-short"><strong>With ownCloud you can sync & share your files, calendar, contacts and more.</strong><br/> 
@@ -21,75 +23,64 @@
 	August 2014, Berlin</a></h3>
 </div>
 </div>
-	<div class="row 3-column-lg-md-sm">
-
-		<div class="col-lg-4 col-md-4 col-xs-12 col-sm-4">
-			<h2><i class="icon-globe">&nbsp;</i> Access</h2>
-			<p>Access your data from your web browser, Android or iOS devices and your laptop and desktop. ownCloud integrates with your existing services thanks to open standards. <a href="https://owncloud.org/features" class="btn btn-lg btn-standard alignright">More features</a></p>
+	<div class="row home-row 	3-column-lg-md-sm">
+		<div class="col-xs-12 col-sm-6 col-md-3">
+			<i class="icon-globe">&nbsp;</i>
+			<h2>Access</h2>
+			<p>Access your data from your web browser, Android or iOS devices and your laptop and desktop.</p>
+			<a href="https://owncloud.org/features" class="btn btn-lg alignright">More features...</a>
 		</div>
 
-		<div class="col-lg-4 col-md-4 col-xs-12 col-sm-4">
-			<h2><i class="icon-exclamation-circle">&nbsp;</i> Privacy</h2>
-			<p>ownCloud gives you control over your data. You decide what is shared with who and for how long. Server-side encryption can secure data hosted on external services.<a href="https://owncloud.org/blog/how-owncloud-uses-encryption-to-protect-your-data/" class="btn btn-lg btn-standard alignright">More about security</a></p>
+		<div class="col-xs-12 col-sm-6 col-md-3">
+			<i class="icon-exclamation-circle">&nbsp;</i>
+			<h2>Privacy</h2>
+			<p>ownCloud gives you control over your data. You decide what is shared with who and for how long.</p>
+			<a href="https://owncloud.org/blog/how-owncloud-uses-encryption-to-protect-your-data/" class="btn btn-lg alignright">More about security...</a>
 		</div>
 
-		<div class="col-lg-4 col-md-4 col-xs-12 col-sm-4">
-			<h2><i class="icon-unlock">&nbsp;</i> Extensible</h2>
-			<p>Extend ownCloud with apps, external storage and your own code. ownCloud is <a href="http://github.com/ownCloud">developed on github</a>, features easy to use API's and has a friendly and open community. <a href="https://owncloud.org/contribute" class="btn btn-lg btn-standard alignright">Get involved</a></p>
+		<div class="col-xs-12 col-sm-6 col-md-3">
+			<i class="icon-unlock">&nbsp;</i>
+			<h2>Extensible</h2>
+			<p>Add apps, external storage, or your own code! Join the friendly and open community.</p>
+			<a href="https://owncloud.org/contribute" class="btn btn-lg alignright">Get involved...</a>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-3">
+			<i class="icon-briefcase">&nbsp;</i>
+			<h2>For Enterprise</h2>
+			<p>Additional features and support to ensure your sensitive corporate data remains in your control.</p>
+			<a href="https://owncloud.com" style="color: #e8733b" class="btn btn-lg alignright">Enterprise Edition...</a>
 		</div>
 	</div>
 	<div class="row" id="getapps">
 		<div class="col-sm-12">
 			<div class="row">
-				<div class="col-sm-6">
+				<div class="col-sm-6 col-xs-12">
 					<h2><i class="icon-cloud-download">&nbsp;</i> Get your ownCloud</h2>
-					<p>Choose <strong>your preferred way</strong> to install your ownCloud Server.</p>
+				</div>
+				<div class="col-sm-6 col-xs-12 install-buttons">
 					<div class="btn-group">
 						<a class="btn btn-primary btn-lg installbtn" href="/install"><i class="icon-cloud-download" ></i> Host ownCloud</br><small>For server owners</small></a>
 						<a class="btn btn-default btn-lg hostingbtn" href="/providers/"><i class="icon-search" ></i> Find hosting</br><small>with a third party</small></a>
 					</div>
+					<a class="btn btn-default btn-lg hostingbtn" href="/providers/"><i class="icon-mobile" ></i> Install Clients</br><small>for desktop and mobile</small></a>
 				</div>
-				<div class="col-sm-6">
-					<h2><i class="icon-refresh">&nbsp;</i> Sync your files</h2>
-					<p>Sync your files using our sync clients for desktop and mobile devices.</p>
-					<ul class="list-unstyled list-inline">
-					  <li>
-						<a id="desktopclient" href="/install#desktop" role="button">
-							<img id="sync-button" alt="desktopclient" src="<?php echo get_template_directory_uri(); ?>/assets/img/clients/buttons/desktopapp.png" />
-						</a>
-					  </li>
-					  <li>
-						<a id="appstore" href="<?php echo $DOWNLOAD_CLIENT_MOBILE_IOS; ?>" target="_blank"  role="button">
-							<img id="sync-button" alt="appstore" src="<?php echo get_template_directory_uri(); ?>/assets/img/clients/buttons/appstore.png" />
-						</a>
-					  </li>
-					  <li>
-						<a id="googleplay" href="<?php echo $DOWNLOAD_CLIENT_MOBILE_ANDROID; ?>" target="_blank"  role="button">
-							<img id="sync-button" alt="googleplay" src="<?php echo get_template_directory_uri(); ?>/assets/img/clients/buttons/googleplay.png" />
-						</a>
-					  </li>
-					</ul>
 			</div>
 		</div>
 	</div>
 </div>
 <div class="row 2-column-lg-md-sm" style="margin-top: -0.5em">
-	<div class="col-lg-4 col-md-4 col-xs-12 col-sm-4">
+	<div class="col-xs-12 col-sm-6">
 		<h2><i class="icon-rocket">&nbsp;</i> Get involved</h2>
 		<p>Join a large and growing community supporting the development and testing of ownCloud to help make it better for everyone. Help answer posts in the <a href="http://forum.owncloud.org/">Forum</a>, post bugs in the <a href="https://github.com/owncloud/core/issues">tracker</a>, test ownCloud using the <a href="http://doc.owncloud.org/server/7.0/developer_manual/testing/index.html">test plan</a>, hang out in the <a href="http://webchat.freenode.net/?channels=owncloud">IRC channel</a> or help <a href="https://www.transifex.com/projects/p/owncloud/">translate</a> ownCloud.</p>
+		<a class="btn btn-block btn-lg join-button conf-button" href="/conf" role="button">Hackathon, Berlin</a>
 	</div>
-	<div class="col-lg-4 col-md-4 col-xs-12 col-sm-4">
+	<div class="col-xs-12 col-sm-6">
 		<h2><i class="icon-users">&nbsp;</i> Join and help</h2>
 		<div class="join">
-			<a class="btn btn-block btn-lg join-button conf-button" href="/conf" role="button">Hackathon, Berlin</a>
 			<a class="btn btn-block btn-primary btn-lg join-button" href="/contribute" role="button">Join development</a>
 			<a class="btn btn-block btn-primary btn-lg join-button" href="https://github.com/owncloud/core/issues" role="button">Report an issue</a>
 			<a class="btn btn-block btn-primary btn-lg join-button" href="https://www.transifex.com/projects/p/owncloud/" role="button">Help translating</a>
 		</div>
-	</div>
-	<div class="col-lg-4 col-md-4 col-xs-12 col-sm-4">
-	<h2><i class="icon-briefcase">&nbsp;</i> ownCloud for the Enterprise</h2>
-	<p>ownCloud Enterprise Edition was created with additional features and support to ensure your sensitive corporate data remains in your control. See what is a href="https://owncloud.com/products/enterprise/">best for you</a>.</p>
 	</div>
 </div>
 <div class="row 3-column-lg-md-sm">
