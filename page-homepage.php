44f143a54af7a30a09c3bd1983d65d9ebfa5578b
Tom Needham <tom@owncloud.com>
2014-08-20 12:33:10 +0100

    Left align feature links

--- page-homepage.php
+++ page-homepage.php
@@ -27,28 +27,36 @@ width: 100%;">
 		<div class="col-xs-12 col-sm-6 col-md-3">
 			<i class="icon-globe">&nbsp;</i>
 			<h2>Access</h2>
-			<p>Access your data from your web browser, Android or iOS devices and your laptop and desktop.</p>
-			<a href="https://owncloud.org/features" class="btn btn-lg feature-link alignright">More features...</a>
+			<div class="home-ft-content">
+				<p>Access your data from your web browser, Android or iOS devices and your laptop and desktop.</p>
+				<a href="https://owncloud.org/features" class="feature-link">More features...</a>
+			</div>
 		</div>
 
 		<div class="col-xs-12 col-sm-6 col-md-3">
 			<i class="icon-exclamation-circle">&nbsp;</i>
 			<h2>Privacy</h2>
-			<p>ownCloud gives you control over your data. You decide what is shared with who and for how long.</p>
-			<a href="https://owncloud.org/blog/how-owncloud-uses-encryption-to-protect-your-data/" class="btn feature-link btn-lg alignright">More about security...</a>
+			<div class="home-ft-content">
+				<p>ownCloud gives you control over your data. You decide what is shared with who and for how long.</p>
+				<a href="https://owncloud.org/blog/how-owncloud-uses-encryption-to-protect-your-data/" class="feature-link">More about security...</a>
+			</div>
 		</div>
 
 		<div class="col-xs-12 col-sm-6 col-md-3">
 			<i class="icon-unlock">&nbsp;</i>
 			<h2>Extensible</h2>
-			<p>Add apps, external storage, or your own code! Join the friendly and open community and get started!</p>
-			<a href="https://owncloud.org/contribute" class="btn feature-link btn-lg alignright">Get involved...</a>
+			<div class="home-ft-content">
+				<p>Add apps, external storage, or your own code! Join the friendly and open community and get started!</p>
+				<a href="https://owncloud.org/contribute" class="feature-link">Get involved...</a>
+			</div>
 		</div>
 		<div class="col-xs-12 col-sm-6 col-md-3">
 			<i class="icon-briefcase">&nbsp;</i>
 			<h2>For Enterprise</h2>
-			<p>Additional features and support to ensure your sensitive corporate data remains in your control.</p>
-			<a href="https://owncloud.com" style="color: #e8733b" class="btn feature-link btn-lg alignright">Enterprise Edition...</a>
+			<div class="home-ft-content">
+				<p>Additional features and support to ensure your sensitive corporate data remains in your control.</p>
+				<a href="https://owncloud.com" style="color: #e8733b" class="feature-link">Enterprise Edition...</a>
+			</div>
 		</div>
 	</div>
 	<div class="row pad-bottom" id="getapps">
