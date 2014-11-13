6d85241690d321af68b4cd27f0dfe7367c009469
Jos Poortvliet <jospoortvliet@gmail.com>
2014-11-13 12:33:32 +0100

    first draft, half done

---
+++ page-speaking.php
@@ -0,0 +1,44 @@
+<div class="page-header">
+	<h1><a href="/events">Events</a> > <a href="/events/conference-program">Conference program</a> > Speaking</h1>
+</div>
+<div class="sub-nav">
+	Meetups&nbsp;&nbsp;
+	|&nbsp;&nbsp;<a href="/events/meetups">Meetups</a>&nbsp;&nbsp;
+	|&nbsp;&nbsp;<a href="/events/contactform">Contact</a>
+</div>
+<div class="row">
+	<div class="col-md-12">
+	<h2>Speaking tips</h2>
+	<p>Giving a good presentation is not that hard. You <strong>don't</strong> have to be a professional speaker to give a compelling talk.
+		There are plenty of great talks given by un-experienced speakers. The story matters, don't let details get in the way of your enthusiasm!</p>
+		<p>On this page, we'll help you with some tips and tricks on giving a great talk.</p>
+
+	<h3>Preparation</h2>
+	<p>Key is a good subject. Pick something close to you, something you care about and know about. Think of what you want people to take away from the talk, put it in a single sentence. Something actionable is best: "I want the audience to try out X" or "I want the audience to contribute to Y".</p>
+
+	<p>Then think of 3-4 things you want the audience to remember. Yes, they won't remember much more and it'll be generics what they remember anyway, so don't bother putting more in. If you really want to dive in code or other details, a workshop is probably what you want to do!</p>
+	<p>Let's say, as example, we talk about an application - ownCloud. We want people to try it out, and we want to tell them three things: <em>why it matters</em>, <em>how it helps them</em> and <em>how easy it is</em>.</p>
+	<h3>Slides</h3>
+	<p>Once you have a subject and a few main points, you have a structure. So it's time to grab a presentation tool and get on it. Create an agenda with these three points. With this slide, you introduce to the audience that these are the things you want to talk about: "<em>why you want ownCloud</em>", "<em>what ownCloud does</em>" and "<em>how easy it is</em>". Or, if you talk about a project: "<em>Why are we cool</em>", "<em>Why should you care</em>" and "<em>How can you help</em>"... And if you talk about something you're good at: "<em>Why is this difficult</em>" "<em>How did I solve it</em>" "<em>Why/how that works</em>".</p>
+	<p>You can now also create a title slide. For the title, go back to your 'single sentence goal' and think how to put that into a few actionable words! So, "<em>Try out ownCloud!</em>" would work well for this talk. Add a slide about yourself and the start is there.</p>
+	<h3>Filling in the details</h3>
+	<p>Then go through the items. Why does it matter? For ownCloud, privacy is a reason to want to host your data. Or the large amounts of data it can hold without a monthly payment, perhaps. Depending on the subject, you might want to talk about history, with a time line or pictures. Remember to keep things simple, as people won't remember details anyway!</p>
+	<p>It is important to talk about things from the point of view of users and listeners. Look for situations many people recognize and build on that and talk from use cases, rather than technical abilities.</p>
+	<p><strong>Style tip</strong>: simplicity is key.
+	<ul><li><strong>Lists</strong> should be short (under 5 items if possible). And don't use more than 2 words for each item or you'll be reading from the slide rather than giving a talk!</li>
+	<li><strong>Quotes</strong> are awesome. Put a single quote on a slide and cut them to at most 20 words. If they're longer, speak the rest during the talk!</li>
+	<li><strong>Pictures</strong> rock, too. You can discuss what is on the picture during the talk but don't describe it on the slide. A full-screen picture is fine!</li>
+	<li><strong>Graphs</strong> should be simple. No labels, no grid lines - focus on the message of the graph, rather than the data behind it. You'll introduce the most important details in your talk: "<em>this graph shows the number of users over time</em>".</li>
+	<li><strong>Variation</strong> keeps people awake. Keep words on a leash. Less of them is better. Usually, just the keywords are enough! Try making a slide with just one to three words on them, have a graph instead of statistics, a picture of a calendar rather than a list of events, a photo of somebody instead of the quote.</li>
+	</p>
+	<h4>Final steps</h4>
+	<p>While making slides, keep in mind what your key messages are. Summarize frequently, repetition is what makes sure your audience remembers the key points!</p>
+	<p>Finally tell people where to find more information/get it/how to contact you, and answer their questions. Done!</p>
+	<p>You can find many more tips on the web. These are a few good ones: <a href="http://blog.hubspot.com/blog/tabid/6307/bid/5975/10-Rules-to-Instantly-Improve-Your-Presentations.aspx">10 rules to instantly improve your presentations</a></p>
+	<h3>Test runs</h3>
+
+
+		<p><strong>Note</strong> that we have limited supply and shipping and handling takes time, so don't be late! In general, count on two weeks at least and please understand that currently we only ship in (most countries in) Europe and the USA. <a href="<?php echo get_template_directory_uri(); ?>/assets/files/request_form.pdf">Click here</a> for a request form to fill.</p>
+
+	</div>
+</div>
