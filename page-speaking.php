80787aaba124b4383347ad8989c3ef93121923bf
Jos Poortvliet <jospoortvliet@gmail.com>
2014-11-14 11:43:50 +0100

    ready to add pictures

--- page-speaking.php
+++ page-speaking.php
@@ -8,46 +8,91 @@
 </div>
 <div class="row">
 	<div class="col-md-12">
-	<p>Giving a good presentation is not that hard. You <strong>don't</strong> have to be a professional speaker to give a compelling talk. There are plenty of great talks given by un-experienced speakers.</p>
-	<p>On this page, we'll help you with some tips and tricks on giving a great talk.</p>
+	<p>You <strong>don't</strong> have to be a professional speaker to give a compelling talk. Many great talks are given by un-experienced speakers.</p>
+	<p>On this page, we'll help you with some tips and tricks to become a better speaker.</p>
 
 	<h2>A great talk in 5 steps</h2>
-	<p>Key to a good talk the realization that the story matters! Don't let details get in the way of your enthusiasm. Here are 5 steps to being a great speaker.</p>
+	<p>Key to a good talk the realization that the story matters most! Let's get from story to speaking in 5 steps.</p>
 	<picture: choose, by wikisid on wikimedia>
+	<a href="http://owncloud.org/wp-content/uploads/2014/11/encryption-documentation.png"><img class="alignright  wp-image-7532" alt="encryption documentation screenshot" src="http://owncloud.org/wp-content/uploads/2014/11/encryption-documentation.png" width="279" height="335" /></a>
 	<h3>1. Subject</h2>
-	<p>First, pick a subject close to you, something you care about and know about. And think of what you want people to take away from the talk, put it in a single sentence. Something actionable is best: "I want the audience to try out X" or "I want the audience to contribute to Y".</p>
-	<p>Then think of 3-4 things you want the audience to remember. Yes, they won't remember much more and it'll be generics what they remember anyway, so don't bother putting more in. If you really want to dive in code or other details, a workshop is probably what you want to do!</p>
-	<p>Let's say, as example, we talk about an application - ownCloud. We want people to try it out, and we want to tell them three things: <em>why it matters</em>, <em>how it helps them</em> and <em>how easy it is</em>.</p>
+	<p>First, pick a subject you care and know about. And, keeping the audience you'll be talking to in mind, think of what you want people to take away from the talk in one sentence. For biggest impact, pick something actionable. Examples:
+	<ul>
+		<li>"I want the audience to try out X"</li>
+		<li>"I want the audience to contribute to Y".</li>
+	</ul></p>
+	<p>Then think of 3-4 things you want the audience to remember. People rarely remember many more details than that from a talk and this way you make sure they remember what you want them to. Again, keep the audience in mind!
+	<ul>
+		<strong>hint:</strong> If you really want to dive in code or other details, a workshop is a much better format. People remember better what they did than what somebody told them.
+	</ul></p>
+	<p>Let's say, as example, we talk about our web application - ownCloud. We want people to try it out, and we want to tell them three things:
+	<ul>
+		<li><em>why it matters</em></li>
+		<li><em>how it helps them</em></li>
+		<li><em>how easy it is</em></li>
+	<ul>
+	Another example, if you talk about the ownCloud project:
+	<ul>
+		<li><em>Why are we cool</em></li>
+		<li><em>Why should you care</em></li>
+		<li><em>How can you help</em></li>
+	</ul>
+	Of course, there are many other possibilities.
+	</p>
 	<picture: slides, by Geo087 on wikimedia>
 	<h3>2. Slides</h3>
-	<p>Once you have a subject and a few main points, you have a structure. So it's time to grab a presentation tool and get on it. Create an agenda with these three points. With this slide, you introduce to the audience that these are the things you want to talk about: "<em>why you want ownCloud</em>", "<em>what ownCloud does</em>" and "<em>how easy it is</em>". Or, if you talk about a project: "<em>Why are we cool</em>", "<em>Why should you care</em>" and "<em>How can you help</em>"... And if you talk about something you're good at: "<em>Why is this difficult</em>" "<em>How did I solve it</em>" "<em>Why/how that works</em>".</p>
-	<p>You can now also create a title slide. For the title, go back to your 'single sentence goal' and think how to put that into a few actionable words! So, "<em>Try out ownCloud!</em>" would work well for this talk. Add a slide about yourself and the start is there.</p>
+	<p>Once you have a subject and a few main points, you have a structure. So it's time to grab a presentation tool and get on it. Create an agenda with these three points. With this slide, you introduce to the audience that these are the things you want to talk about:
+	<ul>
+		<li><em>why you want ownCloud</em></li>
+		<li><em>what ownCloud does</em></li>
+		<li><em>how easy it is</em></li>
+	</ul></p>
+	<p>You can now also create a title slide. For the title, go back to your 'single sentence goal' and think how to put that into a few actionable words!
+	<ul>
+		<em>Try out ownCloud!</em>
+	</ul>
+	would work well for this talk. Add a slide about yourself and the start is there.</p>
+
 	<h4>Filling in the details</h4>
-	<p>Then go through the items. Why does it matter? For ownCloud, privacy is a reason to want to host your data. Or the large amounts of data it can hold without a monthly payment, perhaps. Depending on the subject, you might want to talk about history, with a time line or pictures. Remember to keep things simple, as people won't remember details anyway!</p>
-	<p>It is important to talk about things from the point of view of users and listeners. Look for situations many people recognize and build on that and talk from use cases, rather than technical abilities.</p>
+	<p>Then go through the three subjects and add details. Why does it matter? For ownCloud, privacy is a reason to want to host your data. Or the large amounts of data it can hold without a monthly payment, perhaps. Depending on the subject and audience, you might want to talk about history, with a time line or pictures. Remember to keep things simple, as people won't remember details anyway!</p>
+	<p><strong>Content tips</strong> - focus is key:
+	<ul>
+		<li>Think for each slide what the main point is, and build around that, rather than trying to shove in everything you would be able to say.</li>
+		<li>It is important to talk about things from the point of view of users and listeners. Look for situations many people recognize, build on that and talk from use cases, rather than technical abilities. Add examples!</li>
+	</ul></p>
 	<p><strong>Style tips</strong> - simplicity is key:
 	<ul>
-	<li><strong>Limit</strong> the number of elements on a slide. Ideally, just one - be it a list, a image, a graph. Don't have more than two, really!</li>
-	<li><strong>Lists</strong> should be short (under 5 items if possible). And don't use more than 2 words for each item or you'll be reading from the slide rather than giving a talk!</li>
-	<li><strong>Quotes</strong> are awesome. Put a single quote on a slide and cut them to at most 20 words. If they're longer, speak the rest during the talk!</li>
-	<li><strong>Pictures</strong> rock, too. You can discuss what is on the picture during the talk but don't describe it on the slide. Keep it full-screen, ideally!</li>
-	<li><strong>Graphs</strong> should be simple. No labels, no grid lines - focus on the message of the graph, rather than the data behind it. You'll introduce the most important details in your talk: "<em>this graph shows the number of users over time</em>".</li>
-	<li><strong>Variation</strong> keeps people awake. Keep words on a leash. Less of them is better. Usually, just the keywords are enough! Try making a slide with just one to three words on them, have a graph instead of statistics, a picture of a calendar rather than a list of events, a photo of somebody instead of the quote.</li>
+		<li><strong>Limit</strong> the number of elements on a slide. Ideally, just one - be it a list, a image, a graph. Don't have more than two, really!</li>
+		<li><strong>Lists</strong> should be short (under 5 items if possible). And don't use more than 2 words for each item or you'll be reading from the slide rather than giving a talk!</li>
+		<li><strong>Quotes</strong> are awesome. Put a single quote on a slide and cut them to at most 20 words. If they're longer, speak the rest during the talk!</li>
+		<li><strong>Pictures</strong> rock, too. You can discuss what is on the picture during the talk but don't describe it on the slide. Keep it full-screen, ideally!</li>
+		<li><strong>Graphs</strong> should be simple. No labels, no grid lines - focus on the message of the graph, rather than the data behind it. You'll introduce the most important details in your talk: "<em>this graph shows the number of users over time</em>".</li>
+		<li><strong>Variation</strong> keeps people awake. Keep words on a leash. Less of them is better. Usually, just the keywords are enough! Try making a slide with just one to three words on them, have a graph instead of statistics, a picture of a calendar rather than a list of events, a photo of somebody instead of the quote.</li>
+	</ul></p>
+	<p>A good approach keeping all of the above in mind is this:
+	<ul>
+		<ol>1. throw everything you (might) want to say on the slide in a document</ol>
+		<ol>2. Pick out the common theme, the most important point</ol>
+		<ol>3. Restructure everything to support the main point. Content which doesn't you move to another page or slide</ol>
+		<ol>4. Now you have the 'message' of the slide. Think of the best format to convey it and create the slide. Done!</ol>
 	</ul>
-	</p>
+	The details in the document can be useful as notes while talking. And the content which didn't fit the main subject of the slide is either not needed at all or useful for another slide.</p>
 	<h4>Final steps</h4>
-	<p>While making slides, keep in mind what your key messages are. Summarize frequently, repetition is what makes sure your audience remembers the key points!</p>
+	<p>While making slides, keep in mind what your key messages are. Summarize frequently, repetition is what makes sure your audience remembers the key points! After each main section, be sure to summarize, reminding people of where they are in the agenda, what you discussed and what comes next.</p>
 	<p>Finally tell people where to find more information/get it/how to contact you, and answer their questions. Done!</p>
 	<p>You can find many more tips on the web. These are a few good ones: <a href="http://blog.hubspot.com/blog/tabid/6307/bid/5975/10-Rules-to-Instantly-Improve-Your-Presentations.aspx">10 rules to instantly improve your presentations</a></p>
 	<picture: practice by woodleywonderworks on flickr>
 	<h3>3. Study</h3>
-	<p>Author Malcolm Gladwell concluded that it <a href="http://www.wisdomgroup.com/blog/10000-hours-of-practice/">takes 10,000 hours of practice to master a skill</a>, and facts back him up in that assertion. Violin players, programmers and football players - it doesn't matter what profession. So this will be true for public speaking as well. Now do not despair, you do not have to practice your talk 10.000 hours before you can do it live - but realize that every time you practice, you get that much closer to perfection!</p>
+	<p>Now the slides are done, but you're not.</p>
+	<p>Author Malcolm Gladwell concluded that it <a href="http://www.wisdomgroup.com/blog/10000-hours-of-practice/">takes 10,000 hours of practice to master a skill</a>, and this is true for public speaking as well. You don't have to practice your talk 10.000 hours - but realize that every time you practice, you get that much closer to perfection! And, also noted by Malcolm, <em>everything is practice</em>. If you sit in the bus and go over your main topic in your head or talk to yourself on the streets, during a shower or in bed - it all helps.</p>
 	<h4>The story</h4>
-	<p>You now have an idea of what you want to talk about and you have slides which add up to a story. Next is thus to turn it into the presentation itself. The best way to do it: grab the slides, either on your laptop or printed, and practice the presentation. At every step, you will find things to improve - make notes, changes, and re-do. A good trick is to simply write the presentation out entirely, or with short key words - what works for you. You will find that it easily takes 5-8 times of going over the slides before your story starts to settle down, but that is perfectly fine.</p>
+	<p>You now have an idea of what you want to talk about and you have slides which add up to a story. Next is thus to turn it into the presentation itself. The best way to do it: grab the slides, either on your laptop or printed, and practice the presentation. The details you have 'left over' from creating the slides at step 2 can guide you. At every step, you will find things to improve - make notes, changes, and re-do. A good trick is to simply write the presentation out entirely, or with short key words - what works best for you. You will find that it easily takes 5-8 times of going over the slides before your story starts to settle down, but that is perfectly fine!</p>
 	<p>Of course, as you mostly start at the beginning, this means the first half of the presentation is practiced 8 times, while the end only twice or trice, but that is fine, too: getting started is always the hard part of a talk!</p>
 	<h4>Timing</h4>
 	<p>Once the story is stable, do one or two more runs through and time them. You can now adjust how fast you speak. Cut the presentation in sections (perhaps each of the major points?) so you can check during the presentation if you're going too fast or too slow!</p>
-	<p><strong>A tip</strong> is to put a series of screen shots or a little product demo at the end of your presentation. Depending on how you went, time-wise, you can slowly through them, go faster or skip them completely. It creates a nice buffer, ensuring you're always ending on time!</p>
+	<p><ul>
+		<strong>A tip</strong> is to put a series of screen shots or a little product demo at the end of your presentation. Depending on how you went, time-wise, you can slowly through them, go faster or skip them completely. It creates a nice buffer, ensuring you're always ending on time!
+	</ul></p>
 	<h4>Final preparation</h4>
 	<p>If you wrote it out (and adjusted with every test run) your presentation, try to condense it down to very few words or use strategic bolding on the text to create a backup for yourself. In any case, put the end result, be it short or full text, on paper (printed) or in the notes section of the slides. If, at any point during the presentation, you get stuck, you can resort to reading for a few seconds!</p>
 	<p>Your practice will ensure it is unlikely you will get really stuck. Yes, you might go off track, but the notes will pull you back, no worries. Don't rely too much on them - they are a backup plan!</p>
@@ -57,6 +102,8 @@
 	<h4>Tools and setup</h4>
 	<p>It is important to be well prepared with a stable, known-working setup. Updating your Linux to an unstable tree an hour before presenting isn't wise! Make sure you have the slides on both your laptop <em>and</em> on a USB stick. And perhaps one more time on your ownCloud instance!</p>
 	<p>Once you have had a chance to test your equipment and are on stage, be sure to have something with you to hold in your hands. Most people are very conscious of themselves if they are getting on stage, and hands are the biggest issue. A bottle of water, a pen or paper - these are all great to have with you.</p>
+	<h4>Go, get them, tiger!</h4>
+	<p>If you followed the guide, you will notice that while speaking, you will barely need your notes. You have a good, consistent story and you practiced many times, so while you won't say <em>exactly</em> what it says in your notes, you'll do fine. Better than you expected yourself! But keep an eye on the time, buddy, you might lose yourself in details and talk too much, or talk to fast. No worries, the screenshots at the end will save you!</p>
 	<h3>5. sustain</h3>
 	<p>Once you're done, be sure to not let your skills and experience slip. Watch the video or recording, if they were made, improve the slides, submit them to other events - get better and enjoy!</p>
 	</div>
