902c0fde177e3516297ba56403c55f735ca8447b
Tom Needham <tom@owncloud.com>
2014-11-28 20:34:13 +0000

    First attempt at getstarted page

---
+++ page-getstarted.php
@@ -0,0 +1,46 @@
+<div class="page-header">
+  <h1>Get Started!</h1>
+</div>
+<hr class="wide"></hr>
+<div class="row">
+  <div class="col-md-3">
+    <hr class="narrow"></hr>
+    <div class="numbadge centre">1</div>
+    <h3>Getting Started</h3>
+    <p>Setup an ownCloud server yourself, or purchase one from a provider.</p>
+    <a class="btn btn-primary btn-lg" role="button" href="/features">Install your own</a></br>
+    <a class="btn btn-primary btn-lg" style="margin-top: 10px" role="button" href="/features">Find a provider</a>
+    <hr></hr>
+    <p>Connect to your ownCloud with our sync clients</p>
+    <a class="btn btn-primary btn-lg" role="button" href="/features">Desktop clients</a></br>
+    <a class="btn btn-primary btn-lg" style="margin-top: 10px" role="button" href="/features">Mobile clients</a>
+  </div>
+  <div class="col-md-3">
+    <hr class="narrow"></hr>
+    <div class="numbadge centre">2</div>
+    <h3>Extend ownCloud</h3>
+    <p>Browse hundreds of free apps to extend your ownCloud on <a href="http://apps.owncloud.com" target="_blank">apps.owncloud.com</a></p>
+    <hr></hr>
+    <p>Connect services you already use to ownCloud to keep your content in one place. See a <a href="#">list of compatible services and applications</a>.</p>
+    <h4></h4>
+  </div>
+  <div class="col-md-3">
+    <hr class="narrow"></hr>
+    <div class="numbadge centre">3</div>
+    <h3>Documentation</h3>
+    <p>Manuals are provided in HTML and PDF form:</p>
+    <a href="">User Manual</a></br>
+    <a href="">Admin Manual</a></br>
+    <a href="">Developer Manual</a>
+  </div>
+  <div class="col-md-3">
+    <hr class="narrow"></hr>
+    <div class="numbadge centre">4</div>
+    <h3>Getting help</h3>
+    <p>We hang out on IRC at <a href="">#owncloud</a> on Freenode</p>
+    <p>Join the forums over at <a href="">forums.owncloud.org</a>.</p>
+    <p>Take a look at our dedicated <a href="">contact page</a>.</p>
+    <p>Or give us a shout via social media using the links in the footer!</p>
+    <p>
+  </div>
+</div>
