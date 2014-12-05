25d7e56d970a3d554d5e0ee6b0aab2cef658cd99
Jos Poortvliet <jospoortvliet@gmail.com>
2014-12-05 10:32:04 +0100

    a dirty work-in-progress to be finished later today

--- page-getstarted.php
+++ page-getstarted.php
@@ -8,16 +8,9 @@
     <div class="numbadge centre">1</div>
     <h3>Download</h3>
     <p>Set up a server, buy one or find a provider.</p>
-    <a class="btn btn-default btn-lg" role="button" href="/features"><i class="icon-download"> </i>Download ownCloud</a></br>
-    <a class="btn btn-default btn-lg" style="margin-top: 10px" role="button" href="/features"><i class="icon-cloud"> </i>Find a provider</a>
-    <a class="btn btn-default btn-lg" style="margin-top: 10px" role="button" href="/features"><i class="icon-database"> </i>Buy a server</a>
-<!--    <hr></hr>
-    <h3>Resources</h3>
-    <li><a href="http://doc.owncloud.org/server/7.0/admin_manual/#installation">ownCloud Installation Guide</a></li>
-    <li>The <a href="http://forums.owncloud.org">ownCloud Forums</a> with <a href="https://forum.owncloud.org/viewforum.php?f=17">troubleshooting FAQ</a></li>
-    <li><a href="https://mailman.owncloud.org/mailman/listinfo/user">the ownCloud User mailing list</a></li>
-    <li><a href="irc://#owncloud@freenode.net">the ownCloud IRC chat channel</a> on freenode.net (via <a href="http://webchat.freenode.net/?channels=owncloud">webchat</a>)</li>
-    <li><a href="">Enterprise support</a></li>-->
+    <a class="btn btn-default btn-lg" role="button" href="#install"><i class="icon-download"> </i>Download ownCloud</a></br>
+    <a class="btn btn-default btn-lg" style="margin-top: 10px" role="button" href="/providers"><i class="icon-cloud"> </i>Find a provider</a>
+    <a class="btn btn-default btn-lg" style="margin-top: 10px" role="button" href="/hardware-TBD"><i class="icon-database"> </i>Buy a server</a>
   </div>
   <div class="col-md-4">
     <hr class="narrow"></hr>
@@ -26,13 +19,6 @@
     <p>Connect to your ownCloud with our clients</p>
     <a class="btn btn-default btn-lg" role="button" href="/features"><i class="icon-windows"> </i><i class="icon-apple"> </i><i class="icon-linux"> </i></br>Desktop clients</a></br>
     <a class="btn btn-default btn-lg" style="margin-top: 10px" role="button" href="/features"><i class="icon-android"> </i><i class="icon-apple"></br> </i>Mobile clients</a>
-<!--    <hr></hr>
-    <h3>Resources</h3>
-    <li>Documentation for <a href="http://doc.owncloud.org/server/7.0/admin_manual/#installation">ownCloud desktop client</a>, <a href="">Android client</a> and <a href="">iOS client</a></li>
-    <li><a href="http://forums.owncloud.org">ownCloud Forums</a></li>
-    <li><a href="https://mailman.owncloud.org/mailman/listinfo/user">the ownCloud User mailing list</a></li>
-    <li><a href="irc://#owncloud@freenode.net">the ownCloud IRC chat channel</a> on freenode.net (via <a href="http://webchat.freenode.net/?channels=owncloud">webchat</a>)</li>
-    <li><a href="">Enterprise support</a></li>-->
   </div>
   <div class="col-md-4">
     <hr class="narrow"></hr>
@@ -42,29 +28,7 @@
     <p><a class="btn btn-primary btn-lg" style="margin-top: 10px" role="button" href="/features">ownCloud apps</a></p>
     <p>Connect services you already use to ownCloud.</p>
     <a class="btn btn-primary btn-lg" style="margin-top: 10px" role="button" href="/features">3rd party apps</a>
-<!--    <hr></hr>
-    <h3>Resources</h3>
-    <li><a href="http://doc.owncloud.org/server/7.0/admin_manual/#installation">ownCloud App installation documentation</a></li>
-    <li><a href="http://forums.owncloud.org">ownCloud Forums</a></li>
-    <li><a href="https://mailman.owncloud.org/mailman/listinfo/user">the ownCloud User mailing list</a></li>
-    <li><a href="irc://#owncloud@freenode.net">the ownCloud IRC chat channel</a> on freenode.net (via <a href="http://webchat.freenode.net/?channels=owncloud">webchat</a>)</li>
-    <li><a href="/contribute">Get involved!</a></li>-->
   </div>
-<!--  <div class="col-md-3">
-    <hr class="narrow"></hr>
-    <div class="numbadge centre">4</div>
-    <h3>Troubleshoot</h3>
-    <p>Manuals are provided in HTML and PDF form:</p>
-    <a href="">User Manual</a></br>
-    <a href="">Admin Manual</a></br>
-    <a href="">Developer Manual</a>
-    <hr></hr>
-    <h3>Support channels</h3>
-    <a href="http://forums.owncloud.org">Forum</a> (<a href="https://forum.owncloud.org/viewforum.php?f=17">FAQ page</a>)</br>
-    <a href="https://mailman.owncloud.org/mailman/listinfo/user">User mailing list</a></br>
-    <a href="irc://#owncloud@freenode.net">IRC channel</a> (<a href="http://webchat.freenode.net/?channels=owncloud">webchat</a>)</br>
-    <a href="">Enterprise support</a></br>
-  </div>-->
 </div>
 <hr></hr>
 <div class="row troubleshoot">
