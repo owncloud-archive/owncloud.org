5e28b976f786bd282c68bfafaeb615b9c16bf33d
Jos Poortvliet <jospoortvliet@gmail.com>
2014-12-11 23:24:40 +0100

    update to promote page

--- page-promote.php
+++ page-promote.php
@@ -7,19 +7,31 @@
 		<a style="width: 250px;" target="_blank" href="https://twitter.com/intent/tweet/?url=https%3A%2F%2Fowncloud.org&text=Check%20out%20ownCloud%20for%20open%20source%20data%20sync%2C%20sharing%20and%20more!&via=owncloud&hashtags=owncloud,opensource" class="btn btn-info btn-lg promote"><i class="icon-twitter"></i> Tweet about ownCloud</a>
 		<a style="width: 250px; margin: 0 13px;" target="_blank" href="https://www.facebook.com/sharer.php?u=owncloud.org" class="btn btn-primary btn-lg facebook-button promote"><i class="icon-facebook"></i> Share on Facebook</a>
 		<a style="width: 250px" target="_blank" href="https://plus.google.com/share?url=https%3A%2F%2Fowncloud.org" class="btn btn-danger btn-lg promote"><i class="icon-google-plus"></i> Post on Google+</a>
-		<a style="width: 250px" target="_blank" href="http://sharetodiaspora.github.io/?title=ownCloud rocks!&url=http://owncloud.org" class="btn btn-default btn-lg promote">
+		<a style="width: 250px" target="_blank" href="http://sharetodiaspora.github.io/?title=ownCloud%20rocks!&url=http://owncloud.org" class="btn btn-default btn-lg promote">
 		<i class="icon-asterisk"></i> Share on Diaspora</a>
 	</div>
 </div>
 <div style="display: none;" id="insiders">
-	<p><strong>thank you</strong> for supporting ownCloud! We have a treat for you: an invitation to our ownCloud Insiders program!
+	<p><strong>Thank you</strong> for using and supporting ownCloud.</p>
+
+	<p>We would like to invite you become an ownCloud Insider!</p>
+<!--	<h2>Insiders move forward</h2>
+	<p>Think of ownCloud Insiders as that place where connections are made, big ideas take off and magic happens. We reserved a seat at the table just for you!</p>-->
+	<a href="http://insiders.owncloud.com/join/aDb26fKzL22"><img class="insiders-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/events/insiders.jpg" alt="logo" /></a>
 	<div class="row">
 		<div class="col-md-6">
-			TBD
+			<h2>What to expect</h2>
+			<p>ownCloud Insiders is a portal where you can connect with other advocates and do challenges we provide that help you, or ownCloud, or are just plain fun.</p>
+			<ul>
+				<li><strong>Learn and Grow</strong> – Learn about ownCloud, reading interesting industry blogs and posts and discussing them with others</li>
+				<li><strong>Support and Influence</strong> – From social campaigns to product launches, you choose how and when you would like to show your support and spread your influence.</li>
+				<li><strong>Fun Opportunities</strong> – Compete with peers for points, badges and rewards as you take on new challenges and opportunities</li>
+			</ul>
 		</div>
 		<div class="col-md-6">
-			<h2>Join</h2>
-			<p>http://insiders.owncloud.com/join/aDb26fKzL22</p>
+			<h2>What it is about</h2>
+			<p><iframe width="480" height="240" src="//www.youtube.com/embed/p_OxfRsmmfs" frameborder="0" allowfullscreen></iframe></p>
+			<p><a style="width: 250px; margin: 0 13px;" target="_blank" href="http://insiders.owncloud.com/join/aDb26fKzL22" class="btn btn-warning btn-lg">sign up here</a></p>
 		</div>
 	</div>
 </div>
