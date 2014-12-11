9cde50f75b571b51838fa71b281ad9437f2a7822
Jos Poortvliet <jospoortvliet@gmail.com>
2014-12-11 13:21:25 +0100

    some minor thingy

--- page-promote.php
+++ page-promote.php
@@ -1,24 +1,44 @@
 <div class="page-header">
 	<h1>Promote ownCloud</h1>
 </div>
+<p>If you are happy with ownCloud, consider sharing your enthusiasm with the world!</p>
 <div class="row">
 	<div class="col-md-12 col-sm-4 promote-buttons" style="text-align: center">
-		<a style="width: 250px;" target="_blank" href="https://twitter.com/intent/tweet/?url=https%3A%2F%2Fowncloud.org&text=Check%20out%20ownCloud%20for%20open%20source%20data%20sync%2C%20sharing%20and%20more!&via=owncloud&hashtags=owncloud,opensource" class="btn btn-info btn-lg"><i class="icon-twitter"></i> Tweet about ownCloud</a>
-		<a style="width: 250px; margin: 0 13px;" target="_blank" href="https://www.facebook.com/sharer.php?u=owncloud.org" class="btn btn-primary btn-lg facebook-button"><i class="icon-facebook"></i> Share on Facebook</a>
-		<a style="width: 250px" target="_blank" href="https://plus.google.com/share?url=https%3A%2F%2Fowncloud.org" class="btn btn-danger btn-lg"><i class="icon-google-plus"></i> Post on Google+</a>
+		<a style="width: 250px;" target="_blank" href="https://twitter.com/intent/tweet/?url=https%3A%2F%2Fowncloud.org&text=Check%20out%20ownCloud%20for%20open%20source%20data%20sync%2C%20sharing%20and%20more!&via=owncloud&hashtags=owncloud,opensource" class="btn btn-info btn-lg promote"><i class="icon-twitter"></i> Tweet about ownCloud</a>
+		<a style="width: 250px; margin: 0 13px;" target="_blank" href="https://www.facebook.com/sharer.php?u=owncloud.org" class="btn btn-primary btn-lg facebook-button promote"><i class="icon-facebook"></i> Share on Facebook</a>
+		<a style="width: 250px" target="_blank" href="https://plus.google.com/share?url=https%3A%2F%2Fowncloud.org" class="btn btn-danger btn-lg promote"><i class="icon-google-plus"></i> Post on Google+</a>
+		<a style="width: 250px" target="_blank" href="http://sharetodiaspora.github.io/?title=ownCloud rocks!&url=http://owncloud.org" class="btn btn-default btn-lg promote">
+		<i class="icon-asterisk"></i> Share on Diaspora</a>
 	</div>
 </div>
-<div class="row">
+<div style="display: none;" id="insiders">
+	<p><strong>thank you</strong> for supporting ownCloud! We have a treat for you: an invitation to our ownCloud Insiders program!
+	<div class="row">
+		<div class="col-md-6">
+			TBD
+		</div>
+		<div class="col-md-6">
+			<h2>Join</h2>
+			<p>http://insiders.owncloud.com/join/aDb26fKzL22</p>
+		</div>
+	</div>
+</div>
+<!--<div class="row">
 	<div class="col-md-4">
 		<h2>Write a blog post</h2>
 		<p>Share your experiences with ownCloud in a blog post, and get it included in our <a href="/news/">planet site</a> (contact <a href="/user/?user=tomneedham">Tom Needham</a>).</p>
 	</div>
 	<div class="col-md-4">
 		<h2>Organise an event</h2>
-		<p>Talks, meetups, or even tradeshow booths. See <a href="/events/">this page</a> for more info on ownCloud events.</p>
+		<p>Talks, meetups, or even tradeshow booths. See <a href="/events/" class="promote" target="_blank">this page</a> for more info on ownCloud events.</p>
 	</div>
 	<div class="col-md-4">
 		<h2>Recommend ownCloud</h2>
 		<p>Speak to friends, family, colleagues, or your existing service providers and recommend ownCloud.</p>
 	</div>
-</div>
+</div>-->
+<script>
+  $( ".promote" ).click(function() {
+      $( "#insiders" ).show();
+  });
+</script>
