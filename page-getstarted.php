7d7940d5e6a6da060b9b838d7c8f30db173c99f5
Jos Poortvliet <jospoortvliet@gmail.com>
2014-12-16 11:42:20 +0100

    disable 'buy a server' for now

--- page-getstarted.php
+++ page-getstarted.php
@@ -7,10 +7,10 @@
     <hr class="narrow"></hr>
     <div class="numbadge centre">1</div>
     <h3><i class="icon-download"></i> Download ownCloud</h3>
-    <p>Set up a server, buy one or find a provider:</p>
+    <p>Set up a server yourself or find a provider:</p>
     <div class="btn-group">
       <a class="btn btn-default btn-lg" role="button" href="#instructions-server" rel="tooltip" title="Install instructions" data-toggle="popover">Download</a>
-      <a class="btn btn-default btn-lg" role="button" href="/hardware-TBD">Servers</a>
+<!--       <a class="btn btn-default btn-lg" role="button" href="/hardware-TBD">Servers</a> - disabled for now, until we have a page like that... -->
       <a class="btn btn-default btn-lg" role="button" href="/providers">Providers</a>
     </div>
   </div>
