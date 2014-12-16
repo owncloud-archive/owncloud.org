af4a3d65b58b481e8b42da3c91cd3327d9b64c02
Camila San <smayres@gmail.com>
2014-12-16 14:46:18 +0100

    Changes the page-contribute layout from accordion structure to 2 columns.

--- page-contribute.php
+++ page-contribute.php
@@ -11,126 +11,57 @@ Do you want to <strong>help</strong> translate, promote or document ownCloud?</p
   <a href="/promote" class="btn btn-default btn-lg"><i class="icon-bullhorn"></i> Promote ownCloud</a>
 </div>
 <h2>How to participate:</h2>
-
-<div class="panel-group" id="accordionContribute">
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordionContribute" href="#collapseCoding">
-          <i class="icon-thumbs-up"></i>&nbsp;&nbsp;Improve ownCloud
-        </a>
-      </h4>
-    </div>
-    <div id="collapseCoding" class="panel-collapse collapse">
-      <div class="panel-body">
-      	<ul>
-          <li>Develop your own ownCloud app (<a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>/app">app development documentation</a>)</li>
-      	  <li>Get a bug fixed (<a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>bugtracker">bugtracker documentation</a> and <a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>general">general guidelines and setup</a>)</li>
-      	  <li>Improve the ownCloud Core (<a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>core">core development documentation</a>)</li>
-      	  <li>Learn to write ownCloud code. (Try your hands on a a <a href="https://github.com/owncloud/core/issues?labels=Junior+Job&state=open/">Junior Job</a>
-        and see the <a href="https://github.com/owncloud/core/wiki/Project-ideas#coding">project ideas page</a>)
-        </ul>
-      	<p>Talk to us <a href="irc://#owncloud-dev@freenode.net">on IRC</a> and on our
-      	<a href="http://mailman.owncloud.org/mailman/listinfo/devel">mailing list</a>! We look forward to your code and thoughts.</p>
-        </div>
-    </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordionContribute" href="#collapseIssues">
-          <i class="icon-refresh"></i>&nbsp;&nbsp;Test ownCloud
-        </a>
-      </h4>
-    </div>
-    <div id="collapseIssues" class="panel-collapse collapse">
-      <div class="panel-body">
-      	<p>Testing upcoming ownCloud releases is the best way of making sure the new release can do what you need it 
-      	to do. And of course, only if you report a problem to us we know about it and can fix it! If you have a bug to report,
-      	find the <a href="https://github.com/owncloud/core/blob/master/CONTRIBUTING.md">issue submission guidelines here</a>.</p>
-      	<p>Anybody interested in helping out with ownCloud testing is invited to join the ownCloud Test Pilot team.
-      	See <a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>testing">the Test Pilot documentation</a> for information!.</p>
-      </div>
-    </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordionContribute" href="#collapseTranslate">
-          <i class="icon-book"></i>&nbsp;&nbsp;Translate and document ownCloud
-        </a>
-      </h4>
-    </div>
-    <div id="collapseTranslate" class="panel-collapse collapse">
-      <div class="panel-body">
-        <p>Translate ownCloud to your language. Go to <a href="https://www.transifex.com/projects/p/owncloud/">Transifex</a> and get going!</p>
+<div class="row">
+	<div class="col-md-5">
+            <h3><i class="icon-thumbs-up"></i>&nbsp;&nbsp;Improve ownCloud</h3>
+            <ul>
+            <li>Develop your own ownCloud app (<a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>/app">app development documentation</a>)</li>
+            <li>Get a bug fixed (<a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>bugtracker">bugtracker documentation</a> and <a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>general">general guidelines and setup</a>)</li>
+            <li>Improve the ownCloud Core (<a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>core">core development documentation</a>)</li>
+            <li>Learn to write ownCloud code. (Try your hands on a a <a href="https://github.com/owncloud/core/issues?labels=Junior+Job&state=open/">Junior Job</a>
+          and see the <a href="https://github.com/owncloud/core/wiki/Project-ideas#coding">project ideas page</a>)
+          </ul>
+          <p>Talk to us <a href="irc://#owncloud-dev@freenode.net">on IRC</a> and on our
+          <a href="http://mailman.owncloud.org/mailman/listinfo/devel">mailing list</a>! We look forward to your code and thoughts.</p>
+	</div>
+	<div class="col-md-5">
+            <h3><i class="icon-refresh"></i>&nbsp;&nbsp;Test ownCloud</h3>
+            <p>Testing upcoming ownCloud releases is the best way of making sure the new release can do what you need it 
+            to do. And of course, only if you report a problem to us we know about it and can fix it! If you have a bug to report,
+            find the <a href="https://github.com/owncloud/core/blob/master/CONTRIBUTING.md">issue submission guidelines here</a>.</p>
+            <p>Anybody interested in helping out with ownCloud testing is invited to join the ownCloud Test Pilot team.
+            See <a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>testing">the Test Pilot documentation</a> for information!.</p>
+	</div>
+</div>
+<div class="row">
+    <div class="col-md-5">
+        <h3><i class="icon-book"></i>&nbsp;&nbsp;Translate and document ownCloud</h3>
+         <p>Translate ownCloud to your language. Go to <a href="https://www.transifex.com/projects/p/owncloud/">Transifex</a> and get going!</p>
         <p>Help us improve the ownCloud documentation! Find it in our <a href="https://github.com/owncloud/documentation">github repository</a>. Scroll down for instructions!</p>
-      </div>
+        <h3> <i class="icon-desktop"></i>&nbsp;&nbsp;Improve the ownCloud website</h3>
+      	Help out with the <a href="https://github.com/owncloud/www/blob/master/README.md">website</a> and
+      	<a href="https://github.com/owncloud/documentation/blob/stable7/CONTRIBUTING.rst">write
+      	documentation</a> (contact tpn or deryo in the <a href="irc://#owncloud-dev@freenode.net">irc channel</a>)
     </div>
-  </div>
-    <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordionContribute" href="#collapseAnswer">
-          <i class="icon-book"></i>&nbsp;&nbsp;Answer questions about ownCloud use and development
-        </a>
-      </h4>
-    </div>
-    <div id="collapseAnswer" class="panel-collapse collapse">
-      <div class="panel-body">
+    <div class="col-md-5">
+        <h3><i class="icon-book"></i>&nbsp;&nbsp;Answer questions about ownCloud use and development</h3>
         <p>Help answer questions users and developer have about ownCloud!</p>
         <ul><li>Join the <a href="https://forum.owncloud.org">ownCloud forums</a> and look for <a href="http://forum.owncloud.org/search.php?search_id=unanswered">unanswered questions</a></li>
         <li>Answer ownCloud development questions on <a href="https://stackoverflow.com/questions/tagged/owncloud">Stack Overflow</a> and <a href="http://community.spiceworks.com/">Spiceworks</a></li>
         <li>Answer ownCloud user questions on <a href="http://mailman.owncloud.org/mailman/listinfo/user">the ownCloud user mailing list</a></li></ul>
-      </div>
     </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordionContribute" href="#collapseWebsite">
-          <i class="icon-desktop"></i>&nbsp;&nbsp;Improve the ownCloud website
-        </a>
-      </h4>
-    </div>
-    <div id="collapseWebsite" class="panel-collapse collapse">
-      <div class="panel-body">
-      	Help out with the <a href="https://github.com/owncloud/www/blob/master/README.md">website</a> and
-      	<a href="https://github.com/owncloud/documentation/blob/stable7/CONTRIBUTING.rst">write
-      	documentation</a> (contact tpn or deryo in the <a href="irc://#owncloud-dev@freenode.net">irc channel</a>)
-      </div>
-    </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordionContribute" href="#collapseDesign">
-          <i class="icon-pencil"></i>&nbsp;&nbsp;Design ownCloud
-        </a>
-      </h4>
-    </div>
-    <div id="collapseDesign" class="panel-collapse collapse">
-      <div class="panel-body">
+</div>
+<div class="row">
+    <div class="col-md-5">
+        <h3><i class="icon-pencil"></i>&nbsp;&nbsp;Design ownCloud</h3>
       	Help out with ownCloud user interaction design or graphic design! Get involved on 
       	<a href="design/">the design page</a>!
-      </div>
     </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordionContribute" href="#collapseMoney">
-          <i class="icon-money"></i>&nbsp;&nbsp;Support ownCloud development with bounties
-        </a>
-      </h4>
-    </div>
-    <div id="collapseMoney" class="panel-collapse collapse">
-      <div class="panel-body">
+    <div class="col-md-5">
+        <h3><i class="icon-money"></i>&nbsp;&nbsp;Support ownCloud development with bounties</h3>
         Support ownCloud development by offering some money on features you would like to have!
         <a href="https://www.bountysource.com/teams/owncloud">Check our bountysource page</a>!
-      </div>
     </div>
-  </div>
 </div>
 <br />
 <p>Please be considerate of our <a href="/community/code-of-conduct/">code of conduct</a>! We like to provide a positive and enjoyable environment for everybody.</p>
