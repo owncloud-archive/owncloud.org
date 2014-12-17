5fb984503e553f99185bfd4e5dbc33bfa439abb3
Camila San <smayres@gmail.com>
2014-12-17 13:57:31 +0100

    Change column size from 5 to 6.

--- page-contribute.php
+++ page-contribute.php
@@ -12,7 +12,7 @@ Do you want to <strong>help</strong> translate, promote or document ownCloud?</p
 </div>
 <h2>How to participate:</h2>
 <div class="row">
-	<div class="col-md-5">
+	<div class="col-md-6">
             <h3><i class="icon-thumbs-up"></i>&nbsp;&nbsp;Improve ownCloud</h3>
             <ul>
             <li>Develop your own ownCloud app (<a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>/app">app development documentation</a>)</li>
@@ -24,7 +24,7 @@ Do you want to <strong>help</strong> translate, promote or document ownCloud?</p
           <p>Talk to us <a href="irc://#owncloud-dev@freenode.net">on IRC</a> and on our
           <a href="http://mailman.owncloud.org/mailman/listinfo/devel">mailing list</a>! We look forward to your code and thoughts.</p>
 	</div>
-	<div class="col-md-5">
+	<div class="col-md-6">
             <h3><i class="icon-refresh"></i>&nbsp;&nbsp;Test ownCloud</h3>
             <p>Testing upcoming ownCloud releases is the best way of making sure the new release can do what you need it 
             to do. And of course, only if you report a problem to us we know about it and can fix it! If you have a bug to report,
@@ -34,7 +34,7 @@ Do you want to <strong>help</strong> translate, promote or document ownCloud?</p
 	</div>
 </div>
 <div class="row">
-    <div class="col-md-5">
+    <div class="col-md-6">
         <h3><i class="icon-book"></i>&nbsp;&nbsp;Translate and document ownCloud</h3>
          <p>Translate ownCloud to your language. Go to <a href="https://www.transifex.com/projects/p/owncloud/">Transifex</a> and get going!</p>
         <p>Help us improve the ownCloud documentation! Find it in our <a href="https://github.com/owncloud/documentation">github repository</a>. Scroll down for instructions!</p>
@@ -43,7 +43,7 @@ Do you want to <strong>help</strong> translate, promote or document ownCloud?</p
       	<a href="https://github.com/owncloud/documentation/blob/stable7/CONTRIBUTING.rst">write
       	documentation</a> (contact tpn or deryo in the <a href="irc://#owncloud-dev@freenode.net">irc channel</a>)
     </div>
-    <div class="col-md-5">
+    <div class="col-md-6">
         <h3><i class="icon-book"></i>&nbsp;&nbsp;Answer questions about ownCloud use and development</h3>
         <p>Help answer questions users and developer have about ownCloud!</p>
         <ul><li>Join the <a href="https://forum.owncloud.org">ownCloud forums</a> and look for <a href="http://forum.owncloud.org/search.php?search_id=unanswered">unanswered questions</a></li>
@@ -52,12 +52,12 @@ Do you want to <strong>help</strong> translate, promote or document ownCloud?</p
     </div>
 </div>
 <div class="row">
-    <div class="col-md-5">
+    <div class="col-md-6">
         <h3><i class="icon-pencil"></i>&nbsp;&nbsp;Design ownCloud</h3>
       	Help out with ownCloud user interaction design or graphic design! Get involved on 
       	<a href="design/">the design page</a>!
     </div>
-    <div class="col-md-5">
+    <div class="col-md-6">
         <h3><i class="icon-money"></i>&nbsp;&nbsp;Support ownCloud development with bounties</h3>
         Support ownCloud development by offering some money on features you would like to have!
         <a href="https://www.bountysource.com/teams/owncloud">Check our bountysource page</a>!
