a8d7c5946c3e1714fb3e68cbcfdba81a9fa81e76
Jos Poortvliet <jospoortvliet@gmail.com>
2015-01-22 14:43:08 +0100

    making progress

--- page-conf.php
+++ page-conf.php
@@ -1,11 +1,21 @@
-<div class="row conf-header">
-	<div class="col-sm-6 col-sm-offset-3">
-		<img class="conf-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/5years/5years.svg" alt="logo" />
-		<h2 class="conf-location">January, 2015</h2>
-        	<p><strong>ownCloud is 5 years old!</strong><br /> ownCloud was <a href="/history">announced in 2010</a> by Frank Karlitschek.<br />Having started with just a handful of enthusiasts, it has grown to a community with hundreds of developers and millions of users!</p>
-        	We proudly present you some of what makes ownCloud strong:<br /> <strong>the ideas and creativity of its users and contributors!</strong>
-        </div>
+<div class="row col-md-12">
+	<h2 class="conf-location">January, 2015</h2>
 </div>
+<div class="row conf-info">
+	<div class="col-md-4  col-md-offset-2">
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/img/5years/5years.svg" alt="logo" />
+	</div>
+	<div class="col-md-6">
+		<strong>ownCloud is 5 years old!</strong>
+		<p>ownCloud was <a href="/history">announced in 2010</a> by Frank Karlitschek.<br />
+		Having started with just a handful of enthusiasts, it has grown to a community with hundreds of developers and millions of users! Find our blog about the anniversary with <a href="https://owncloud.org/blog/owncloud-is-5-years-old">a time line of ownCloud events here</a>.</p>
+		<p>Below we proudly present you some of what makes ownCloud strong:<br />
+		<strong>the ideas and creativity of its users and contributors!</strong>
+		</p>
+	</div>
+</div>
+
+<hr>
 <!--<div class="row conf-info">
 	<div class="col-sm-6">
 		<i class="icon-code"></i>
@@ -54,55 +64,76 @@
 	</div>
 </div>
 <div class="row conf-partners">
-	<div class="col-sm-4 ">
-		<p class="text-center">"Here’s to another five amazing, innovative years of freedom of choice!"</p>
-		<p class="text-right">–ownCloud user <i>Nassos Kourentas</i>, Greece</p>
+	<div class="col-sm-6">
+		<p>"It is unbelievable that ownCloud is just five year old. Looking at the amazing feature set one could think it has been in development for ten years at least."</p>
+		<p>–contributor since 2011 <i>Jakob Sack</i>, Germany</p>
 	</div>
-	<div class="col-sm-8">
-		<p class="text-center">"very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - "</p>
-		<p class="text-right">–ownCloud user <i>bla</i></p>
-	</div>
-</div>
-
-<div class="row conf-partners">
-	<div class="col-sm-8">
-		<p class="text-center">"It is unbelievable that ownCloud is just five year old. Looking at the amazing feature set one could think it has been in development for ten years at least."</p>
-		<p class="text-right">–contributor since 2011 <i>Jakob Sack</i>, Germany</p>
-	</div>
-<div class="row conf-partners">
-	<div class="col-xs-12 col-sm-4">
-		<p class="text-center">"Love, love, love my owncloud. Just recently upgraded from version 5 to version 7."</p>
-		<p class="text-right">–ownCloud user <i>Jody Hoagland</i>, USA</p>
+	<div class="col-sm-6">
+		<p>"Here’s to another five amazing, innovative years of freedom of choice!"</p>
+		<p>–ownCloud user <i>Nassos Kourentas</i>, Greece</p>
 	</div>
 </div>
-
-
-
-
+<hr>
 <div class="row conf-partners">
-	<div class="col-sm-4 ">
-		<p class="text-center">"It works. Thats the most important reason I use it. And it didn´t cause much maintenance overhead so far."</p>
-		<p class="text-right">–ownCloud user <i>Martin 'Helios' Steigerwald</i>, Germany</p>
+	<div class="col-sm-6">
+		<p>"Let’s rock the world and make open-source a more important thing. We’re quite a great showcase project when somebody asks about some cool open-source stuff"</p>
+		<p>–ownCloud contributor since 2012 <i>Lukas Reschke</i>, Switzerland</p>
 	</div>
-	<div class="col-sm-8">
-		<p class="text-center">"very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - "</p>
-		<p class="text-right">–ownCloud user since 2013 <i>Faisal-ur-Rehman</i>, Pakistan</p>
+	<div class="col-sm-6">
+		<p>"Love, love, love my owncloud. Just recently upgraded from version 5 to version 7."</p>
+		<p>–ownCloud user <i>Jody Hoagland</i>, USA</p>
 	</div>
 </div>
-
-
+<hr>
 <div class="row conf-partners">
-	<div class="col-sm-8">
-		<p class="text-center">"very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - very long text - "</p>
-		<p class="text-right">–ownCloud user <i>bla</i></p>
+	<div class="col-sm-6">
+		<p>"Keep up the good work, delivering a powerful AND simple solution running like a charm on my Pi"</p>
+		<p>–ownCloud user since 2013 <i>Antoine Guilloux</i>, France</p>
 	</div>
-	<div class="col-sm-4 ">
-		<p class="text-center">"Wish all the best for the next years.<br />
+	<div class="col-sm-6">
+		<p>"I simply love the community which was roughly 45-50 contributors when I had joined and is now touching 200.
+		So, a very very happy ownCloud! You are our 5 year old but an extremely mature kid now</p>
+		<p>–ownCloud contributor since 2012 <i>Raghu Nayyar</i>, India</p>
+	</div>
+</div>
+<hr>
+<div class="row conf-partners">
+	<div class="col-sm-6">
+		<p>"Before OC, I made and used my own bash-script to sync my files online but OC has ajaxed drag and drop feature in browser like a desktop. And it can sync on multiple devices."</p>
+		<p>–ownCloud user since 2013 <i>Faisal-ur-Rehman</i>, Pakistan</p>
+	</div>
+	<div class="col-sm-6">
+		<p>"Wish all the best for the next years.<br />
 		Thanks Frank for the idea.<br />
 		Thanks Jos for helping us promote it.<br />
 		Thanks community developing the tool!!!"</p>
-		<p class="text-right">–ownCloud user since 2011 <i>Efstathios (Stathis) Iosifidis</i>, Greece</p>
+		<p>–ownCloud user since 2011 <i>Efstathios (Stathis) Iosifidis</i>, Greece</p>
 	</div>
-
-
 </div>
+<hr>
+<div class="row conf-partners">
+	<div class="col-sm-6">
+		<p>"It works. Thats the most important reason I use it. And it did not cause much maintenance overhead so far."</p>
+		<p>–ownCloud user <i>Martin 'Helios' Steigerwald</i>, Germany</p>
+	</div>
+	<div class="col-sm-6">
+		<p>"I am using owncloud since version 5.x somewhere around and it get's better from version to version. At university a few students I know, also use OwnCloud. More and more use owncloud. It's simple and it's a very good solution!"</p>
+		<p>–ownCloud user since 2013 <i>Christoph S</i>, Germany</p>
+	</div>
+</div>
+<hr>
+<div class="row conf-partners">
+	<div class="col-sm-6">
+		<p>"I've been ownclouding since version 5 and it's getting better and better.<br>
+		Hopefully we'll enjoy ownCloud for years to come"</p>
+		<p>–ownCloud user since 2013 <i>Philip Lages</i>, Netherlands</p>
+	</div>
+	<div class="col-sm-6">
+		<p>"Wish all the best for the next years.<br />
+		Thanks Frank for the idea.<br />
+		Thanks Jos for helping us promote it.<br />
+		Thanks community developing the tool!!!"</p>
+		<p>–ownCloud user since 2011 <i>Efstathios (Stathis) Iosifidis</i>, Greece</p>
+	</div>
+</div>
+<hr>
