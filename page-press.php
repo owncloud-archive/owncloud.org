541d049a234e2acfee2b5b45ce2f2179a791c9bc
Tom Needham <tom@owncloud.com>
2015-02-08 23:09:29 +0000

    Remove announcement link for now, fix link to feature page

--- page-press.php
+++ page-press.php
@@ -1,6 +1,6 @@
 <?php get_template_part('templates/parts/title'); ?>
 <h2>Current Release Information <small>Version 8.0</small></h2>
-<a target="_blank" href="https://owncloud.org/blog/owncloud-server-8-0-released" class="btn btn-lg btn-default">Announcement</a>
+<!-- <a target="_blank" href="https://owncloud.org/blog/owncloud-server-8-0-released" class="btn btn-lg btn-default">Announcement</a> -->
 <a href="/features" class="btn btn-lg btn-default">Key Features of ownCloud</a>
 <a href="/install" class="btn btn-lg btn-default">Download ownCloud Server 8.0</a>
 <h2>Latest News and Annoucements</h2>
@@ -13,7 +13,7 @@
 		style="width: 100%" alt="Download a zip file with logo files" class="img-thumbnail"></a></div>
 	<div class="col-md-3"><a href="http://www.youtube.com/ownclouders"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/screenshots/youtube.png" style="width: 100%" 
 		alt="View the ownCloud community youtube channel" class="img-thumbnail"></a></div>
-	<div class="col-md-3"><a href="/8-0"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/screenshots/newfeatures.png" style="width: 100%" alt="See what is new in ownCloud Server 8.0"
+	<div class="col-md-3"><a href="/eight"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/screenshots/newfeatures.png" style="width: 100%" alt="See what is new in ownCloud Server 8.0"
 		class="img-thumbnail"></a></div>
 </div>
 <h2>Branding Guidelines</h2>
