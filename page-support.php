2d94705bc8b172f4e4975e6692d4f71422a59c41
Jos Poortvliet <jospoortvliet@gmail.com>
2015-03-09 20:57:14 +0100

    make table work and look better

--- page-support.php
+++ page-support.php
@@ -9,49 +9,50 @@
 
 <h2>Where should I look?</h2>
 <p>Not sure where to look? Find out below what your use case is and where to find the information you are looking for!</p>
-
-<table class="table table-striped">
-<tr>
-  <td class="active"></td>
-  <td class="info"><i class="icon-users"></i>&nbsp;&nbsp;Private use</td>
-  <td class="info"><i class="icon-briefcase"></i>&nbsp;&nbsp;Professional use</td>
-</tr>
-<tr>
-  <td class="active">Server install</td>
-  <td class="info">Own server, (Raspberry Pi, VPS, NAS) or rented space at <a href="/providers" target="_blank">ownCloud provider</a></td>
-  <td class="info">Spread out over multiple servers for high availability and performance</td>
-</tr>
-<tr>
-  <td class="active">Data stored</td>
-  <td class="info">Private data from self, friends and family</td>
-  <td class="info">Company, customer, client or student data</td>
-</tr>
-<tr>
-  <td class="active">Storage</td>
-  <td class="info">Local storage, Dropbox, my NAS, an FTP drive at university, ...</td>
-  <td class="info">Our existing enterprise data storage solution (S3, SharePoint, OpenStack, ...)</td>
-</tr>
-<tr>
-  <td class="active">Use case</td>
-  <td class="info">Sync data to devices and share with friends and family</td>
-  <td class="info">Make data available everywhere including employees, customers and partners</td>
-</tr>
-<tr>
-  <td class="active">Used by</td>
-  <td class="info">Maybe a dozen family members, friends and co-workers</td>
-  <td class="info">Hundreds to hundreds of thousands of users authenticated by SAML, LDAP, Active Directory or other authentication service</td>
-</tr>
-<tr>
-  <td class="warning">Where do I find what I need to keep ownCloud running smooth?</td>
-  <td class="warning"><p>Find the knowledge you need to enjoy ownCloud in the <a href="http://doc.owncloud.org" target="_blank">documentation</a> and get more help from <a href="/faq/#install" target="_blank">other community resources</a> like social media, forum, IRC chat and so on.</p>
-  <p>Use and enjoy the many apps from our build in app store, or find even more on <a href="https://apps.owncloud.com" target="_blank">our app store</a>. Find <a href="<?php echo $DOCUMENTATION_ADMIN; ?>installation/apps_management_installation.html">documentation on handling Apps here</a>.
-  </td>
-  <td class="warning"><p>Find what you need to tune ownCloud performance and meet business requirements in our <a href="http://doc.owncloud.org" target="_blank">documentation</a> as well as the <a href="http://owncloud.com/whitepapers" target="_blank">resources provided by ownCloud, Inc.</a>
-            <ul>
-		  <li>If you need file sync and share as provided by ownCloud Server and want the confidence of knowing that your ownCloud deployment is backed by technical expertise with 8x5 mail support, the <a href="https://owncloud.com/get-started-standard-subscription" target="_blank">Standard Subscription by ownCloud, Inc.</a> is a cost-effective solution which may meet your requirements.</li>
-		  <li>If you administer more complex deployments of ownCloud, servicing hundreds to hundreds of thousands of users, handling terabytes to Petabytes of data, require high availability and multi-server scaling, need integration in Enterprise infrastructure and/or have specific needs around compliance in regulated industries, <a href="https://owncloud.com/products/enterprise/" target="_blank">Enterprise Subscription from ownCloud, Inc.</a> is available with 12x5 email and phone support, additional <a href="https://owncloud.com/features" target="_blank">enterprise functionality</a> and deployment and branding tools and more. ownCloud, Inc. can also work with you to develop <a href="https://owncloud.com/subscriptions" target="_blank">custom support</a> that can include up to 24x7 coverage.</li>
-		  </ul>
-	    </p>
-  </td>
-</tr>
-</table>
+<!-- <div class="table-responsive"> -->
+  <table class="table table-striped">
+    <tr>
+      <td class="active" style="min-width:150px"></td>
+      <td class="info" style="min-width:300px"><i class="icon-users"></i>&nbsp;&nbsp;Private use</td>
+      <td class="info" style="min-width:300px"><i class="icon-briefcase"></i>&nbsp;&nbsp;Professional use</td>
+    </tr>
+    <tr>
+      <td class="active">Server install</td>
+      <td class="info">Own server, (Raspberry Pi, VPS, NAS) or rented space at <a href="/providers" target="_blank">ownCloud provider</a></td>
+      <td class="info">Spread out over multiple servers for high availability and performance</td>
+    </tr>
+    <tr>
+      <td class="active">Data stored</td>
+      <td class="info">Private data from self, friends and family</td>
+      <td class="info">Company, customer, client or student data</td>
+    </tr>
+    <tr>
+      <td class="active">Storage</td>
+      <td class="info">Local storage, Dropbox, my NAS, an FTP drive at university, ...</td>
+      <td class="info">Our existing enterprise data storage solution (S3, SharePoint, OpenStack, ...)</td>
+    </tr>
+    <tr>
+      <td class="active">Use case</td>
+      <td class="info">Sync data to devices and share with friends and family</td>
+      <td class="info">Make data available everywhere including employees, customers and partners</td>
+    </tr>
+    <tr>
+      <td class="active">Used by</td>
+      <td class="info">Maybe a dozen family members, friends and co-workers</td>
+      <td class="info">Hundreds to hundreds of thousands of users authenticated by SAML, LDAP, Active Directory or other authentication service</td>
+    </tr>
+    <tr>
+      <td class="warning">Where do I find what I need to keep ownCloud running smooth?</td>
+      <td class="warning"><p>Find the knowledge you need to enjoy ownCloud in the <a href="http://doc.owncloud.org" target="_blank">documentation</a> and get more help from <a href="/faq/#install" target="_blank">other community resources</a> like social media, forum, IRC chat and so on.</p>
+      <p>Use and enjoy the many apps from our build in app store, or find even more on <a href="https://apps.owncloud.com" target="_blank">our app store</a>. Find <a href="<?php echo $DOCUMENTATION_ADMIN; ?>installation/apps_management_installation.html">documentation on handling Apps here</a>.
+      </td>
+      <td class="warning"><p>Find what you need to tune ownCloud performance and meet business requirements in our <a href="http://doc.owncloud.org" target="_blank">documentation</a> as well as the <a href="http://owncloud.com/whitepapers" target="_blank">resources provided by ownCloud, Inc.</a>
+		<ul>
+		      <li>If you need file sync and share as provided by ownCloud Server and want the confidence of knowing that your ownCloud deployment is backed by technical expertise with 8x5 mail support, the <a href="https://owncloud.com/get-started-standard-subscription" target="_blank">Standard Subscription by ownCloud, Inc.</a> is a cost-effective solution which may meet your requirements.</li>
+		      <li>If you administer more complex deployments of ownCloud, servicing hundreds to hundreds of thousands of users, handling terabytes to Petabytes of data, require high availability and multi-server scaling, need integration in Enterprise infrastructure and/or have specific needs around compliance in regulated industries, <a href="https://owncloud.com/products/enterprise/" target="_blank">Enterprise Subscription from ownCloud, Inc.</a> is available with 12x5 email and phone support, additional <a href="https://owncloud.com/features" target="_blank">enterprise functionality</a> and deployment and branding tools and more. ownCloud, Inc. can also work with you to develop <a href="https://owncloud.com/subscriptions" target="_blank">custom support</a> that can include up to 24x7 coverage.</li>
+		      </ul>
+		</p>
+      </td>
+    </tr>
+  </table>
+<!-- </div> -->
