05818c413d6be8f8d4658754e7a396e2c7fc0f36
Jos Poortvliet <jospoortvliet@gmail.com>
2015-03-11 12:25:15 +0100

    improve social media pics on promote page

--- page-promote.php
+++ page-promote.php
@@ -20,15 +20,13 @@
         <li>Join the <a href="https://forum.owncloud.org">ownCloud forums</a> and our <a href="irc://#owncloud@freenode.net" target="_blank">IRC channel</a> (<a href="http://webchat.freenode.net/?channels=owncloud" target="_blank">Webchat</a>)</li>
         <li>See ownCloud development questions on <a href="https://stackoverflow.com/questions/tagged/owncloud">Stack Overflow</a> and <a href="https://community.spiceworks.com/">Spiceworks</a></li>
         <li>Discuss ownCloud on on <a href="https://mailman.owncloud.org/mailman/listinfo/user">the ownCloud user mailing list</a></li>
-        <li>Follow us on social media:
-        <ul>
-	    <li><a target="_blank" href="https://plus.google.com/107072838776436530705/about"><img width=25 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/googleplus.png"></img> Google Plus</a></li>
-	    <li><a target="_blank" href="https://www.facebook.com/ownClouders"><img width=25 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/facebook.png"></img> Facebook</a></li>
-	    <li><a target="_blank" href="https://twitter.com/ownClouders"><img width=25 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/twitterround.png"></img> Twitter</a></li>
-	    <li><a target="_blank" href="https://owncloud.org/blogfeed"><img class="img-circle" width=25 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/rss.png"></img> Blog feed</a></li>
+        <li>Follow us on social media: <br />
+	    <a target="_blank" href="https://plus.google.com/107072838776436530705/about"><img width=50 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/googleplus.png"></img></a>
+	    <a target="_blank" href="https://www.facebook.com/ownClouders"><img width=50 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/facebook.png"></img></a>
+	    <a target="_blank" href="https://twitter.com/ownClouders"><img width=50 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/twitterround.png"></img></a>
+	    <a target="_blank" href="https://owncloud.org/blogfeed"><img class="img-circle" width=50 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/rss.png"></img></a>
+	    <a target="_blank" href="https://joindiaspora.com/u/owncloud"><img width=50 src="<?php echo get_template_directory_uri(); ?>/assets/img/social/diasporaround.png"></img></a>
       </ul>
-
-<br />
 <div class="page-header">
 	<h1>Become an Insider</h1>
 </div>
