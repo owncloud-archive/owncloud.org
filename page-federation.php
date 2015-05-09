25d85d9b4f6541da985e3c85e1e968fbfbe542b6
jospoortvliet <jospoortvliet@gmail.com>
2015-05-09 13:19:15 +0200

    first stab at a federation page

---
+++ page-federation.php
@@ -0,0 +1,25 @@
+<div class="row col-md-12">
+	<h2 class="conf-location">Share accross ownClouds!</h2>
+</div>
+<div class="row conf-info">
+	<div class="col-lg-4 col-md-5 text-center">
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/img/federation/federation.gif" alt="in action" />
+	</div>
+	<div class="col-lg-8 col-md-7">
+		<strong>How do I share to another ownCloud user?</strong>
+		<p>Sharing files accross ownCloud servers is as easy as sending an email from a GMail address to a Yahoo account! Instead of an email address, you use a Federation ID. And just like email, it looks like <strong>username@server</strong>, although there often is a string like 'ownCloud' attached at the end, like <strong>guy@fawkes.org/myowncloud</strong></p>
+		<p>To share a file with somebody, just enter their Federation ID in the 'share' dialog and when they log into their own cloud, they will get a notification to accept your share.</p>
+		<p>You can find your Federation ID in your user settings. Give it to your friends so they can share files with you!</p>
+	</div>
+</div>
+<hr>
+<div class="row conf-partners-title">
+	<div class="col-md-12">
+		<h2>More details</h2>
+	</div>
+</div>
+<div     class="row">
+        <div class="col-md-6 col-md-offset-3">
+                You can find more information in the <a href="http://doc.owncloud.org/bla/federation" target="_blank">user documentation</a> and <a href="http://owncloud.com/introducing-federation" target="_blank">technical details here</a>.
+        </div>
+</div>
