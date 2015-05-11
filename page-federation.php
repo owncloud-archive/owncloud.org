652551608c19e5a81efa8bb504ff3ae4f8f12250
jospoortvliet <jospoortvliet@gmail.com>
2015-05-11 14:43:31 +0200

    fix thingies Lukas noticed

--- page-federation.php
+++ page-federation.php
@@ -1,12 +1,12 @@
 <div class="row col-md-12">
-	<h2 class="conf-location">Share accross ownClouds!</h2>
+	<h2 class="conf-location">Share across ownClouds!</h2>
 </div>
 <div class="row conf-info">
 	<div class="col-lg-4 col-md-5 text-center">
 		<img src="<?php echo get_template_directory_uri(); ?>/assets/img/federation/federation.gif" alt="in action" />
 	</div>
 	<div class="col-lg-8 col-md-7">
-		<p>Sharing files accross ownCloud servers is as easy as sending an email from a GMail address to a Yahoo account! Instead of an email address, you use a Federation ID. And just like email, it looks like <strong>username@server</strong>, although there often is a string like 'ownCloud' attached at the end, like <strong>guy@fawkes.org/myowncloud</strong></p>
+		<p>Sharing files across ownCloud servers is as easy as sending an email from a Gmail address to a Yahoo account! Instead of an email address, you use a Federation ID. And just like email, it looks like <strong>username@server</strong>, although there often is a string like 'ownCloud' attached at the end, like <strong>guy@fawkes.org/myowncloud</strong></p>
 		<p>To share a file with somebody, just enter their Federation ID in the 'share' dialog and when they log into their own cloud, they will get a notification to accept your share.</p>
 		<p>You can find your Federation ID in your user settings. Give it to your friends so they can share files with you!</p>
 	</div>
