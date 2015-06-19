6f0b2b0fadf28cb5bd736f14554041feab6cf528
Jos Poortvliet <jospoortvliet@gmail.com>
2015-06-19 16:07:48 +0200

    remove extra inclusion of jQuery

--- page-federation.php
+++ page-federation.php
@@ -1,5 +1,3 @@
-<script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
-
 <script>
 $(document).ready(function() {
         if(window.location.hash) {
