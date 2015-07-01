5eb11f09d9347352c5de84798b0bc08d4f3f394c
Jos Poortvliet <jospoortvliet@gmail.com>
2015-07-01 13:41:48 +0200

    update release channels and some other stuff

--- page-release-channels.php
+++ page-release-channels.php
@@ -2,8 +2,18 @@
 <div class="page-header">
 	<h1>Tracking ownCloud Releases</h1>
 </div>
-<p>With millions of ownCloud users, there are a wide variety of needs and requirements. To give our users more flexibility in choosing an ownCloud version to meet their needs, we offer Release Channels. Below you can find and pick the best Channel for you. We recommend using Linux packages if possible as they offer easier updates.</p>
-  <table class="table table-striped">
+
+<div class="row">
+      <div class="col-lg-9 col-md-8 col-sm-6 col-xs-12">
+	      <p>With millions of ownCloud users, there are a wide variety of needs and requirements. We offer Release Channels to give our users more flexibility in choosing an ownCloud version to meet their needs. Below you can find and pick the best Channel for you.</p>
+	      <p>You can choose a channel of your preference in the Administrator settings in your ownCloud instance if you have installed ownCloud from an archive. If you prefer using Linux packages, you can pick a repository for a specific version, a repository with daily snapshots or follow the repository which always has the latest stable feature release.</p>
+      </div>
+      <div class="col-lg-3 col-md-4 col-sm-6 col-xs-10">
+	      <a href="<?php echo get_template_directory_uri(); ?>/assets/img/screenshots/server/eight-one/update-channels.png" title="Update Channel selection in Updater app" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/screenshots/server/eight-one/update-channels-wee.png" style="width: 100%" alt="..." class="img-thumbnail"></a>
+      </div>
+</div>
+
+<table class="table table-striped">
     <tr>
       <td class="warning" style="min-width:250px; max-widt:250px"><i class="icon-sitemap"></i>&nbsp;&nbsp;Release channel</td>
       <td class="info" style="min-width:300px"><i class="icon-briefcase"></i>&nbsp;&nbsp;Use case</td>
@@ -12,9 +22,8 @@
     <tr>
       <td class="warning"><h1>Production</h1></td>
       <td class="info">This channel delivers the latest fully tested release of ownCloud.</br>
-That does not imply that these release are bug free, but issues are known and can be worked around. The downside of this channel is that the releases are behind in terms of features and performance.</td>
-            <td class="info"><a href="<?php echo $DOWNLOAD_SERVER_PRODUCTION_TAR; ?>" class="btn btn-lg btn-default"><i class="icon-archive"></i> Archive File</a> (<a href="<?php echo $DOWNLOAD_SERVER_PRODUCTION_TAR_MD5; ?>">MD5</a>)<br />
-      <a href="<?php echo $DOWNLOAD_SERVER_PACKAGES_PRODUCTION; ?>" class="btn btn-lg btn-default"><i class="icon-linux"></i> Linux Packages</a></td>
+		      That does not imply that these release are bug free. The issues are known and can be worked around. The downside of this channel is that the releases are behind in terms of features and performance.</td>
+            <td class="info"><a href="<?php echo $DOWNLOAD_SERVER_PACKAGES_OBS_HOME; ?>" class="btn btn-lg btn-default"><i class="icon-linux"></i> Linux Repositories</a><br />older archives are available from our <a href="/changelog">changelog</a>.</td>
     </tr>
     <tr>
       <td class="warning"><h1>Stable</h1></td>
@@ -24,16 +33,15 @@ That does not imply that these release are bug free, but issues are known and ca
     </tr>
     <tr>
       <td class="warning"><h1>Beta</h1></td>
-      <td class="info">Betas and release candidates are the version of choice for testers in preparation of a ownCloud release. Grab these to make sure the upcoming release works well on your infrastructure.</td>
-      <td class="info"><a href="<?php echo $DOWNLOAD_SERVER_BETA_TAR; ?>" class="btn btn-lg btn-default"><i class="icon-archive"></i> Archive File</a> (<a href="<?php echo $DOWNLOAD_SERVER_BETA_TAR_MD5; ?>">MD5</a>)<br />
-      <a href="<?php echo $DOWNLOAD_SERVER_PACKAGES_BETA; ?>" class="btn btn-lg btn-default"><i class="icon-linux"></i> Linux Packages</a></td>
+      <td class="info">Betas and release candidates are the versions of choice for testers in preparation for an ownCloud release. Grab these to make sure the upcoming release works well on your infrastructure.</td>
+      <td class="info">Follow our <a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>/testing/index.html">testpilots mailing list</a> to get the latest test version.</td>
     </tr>
     <tr>
       <td class="warning"><h1>Daily</h1></td>
-      <td class="info">Daily builds are meant to to reproduce bugs and enable testers to follow the development of the nest major version.</td>
+      <td class="info">Daily builds are meant to reproduce bugs as well as to enable testers to follow the development of the next major version.</td>
       <td class="info"><a href="<?php echo $DOWNLOAD_SERVER_DAILY_TAR; ?>" class="btn btn-lg btn-default"><i class="icon-archive"></i> Archive File</a> (<a href="<?php echo $DOWNLOAD_SERVER_DAILY_TAR_MD5; ?>">MD5</a>)<br />
       <a href="<?php echo $DOWNLOAD_SERVER_PACKAGES_DAILY; ?>" class="btn btn-lg btn-default"><i class="icon-linux"></i> Linux Packages</a></td>
     </tr>
-  </table>
-<p>If you are looking to track a specific version, you can find our <a href="<?php echo $DOWNLOAD_SERVER_PACKAGES_OBS_HOME; ?>">Linux package repositories here</a> and older tarballs are available from our <a href="/changelog">changelog</a>.</p>
+</table>
+<p>If you are looking to track a specific version, you can find our <a href="<?php echo $DOWNLOAD_SERVER_PACKAGES_OBS_HOME; ?>">Linux package repositories here</a> and older archives are available from our <a href="/changelog">changelog</a>.</p>
 <p><strong>Security note:</strong></br>To receive information about updates and security issues, we recommend a subscription to our low-traffic <a href="http://mailman.owncloud.org/mailman/listinfo/announcements">announcement mailing list</a>.</p>
