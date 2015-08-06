831203bb476f1cde2a495586237e340d6462144b
Jos Poortvliet <jospoortvliet@gmail.com>
2015-08-06 10:08:25 +0200

    fix link

--- page-appform.php
+++ page-appform.php
@@ -1,7 +1,7 @@
 <div class="page-header">
 	<h1>Thanks for creating an ownCloud app!</h1>
 </div>
-<p>With this form, you submit your app for review to the ownCloud app review team. Make sure to read the <a href="<?php echo $DOCUMENTATION_DEVEL; ?>general/publishing.html" target="_blank">Publishing documentation</a> very carefully. Once you feel your app fullfills the requirements for approval, enter the details below to submit the app for review.</p>
+<p>With this form, you submit your app for review to the ownCloud app review team. Make sure to read the <a href="<?php echo $DOCUMENTATION_DEVEL; ?>app/publishing.html" target="_blank">Publishing documentation</a> very carefully. Once you feel your app fullfills the requirements for approval, enter the details below to submit the app for review.</p>
 <p>You will be automatically subscribed to the <a href="https://mailman.owncloud.org/mailman/listinfo/appsreview">app review mailing list</a>. You will receive a confirmation link in your inbox, please make sure to <strong>confirm the subscription as soon as possible</strong> so you do not miss any important replies to your submission.</p>
 <form name="contactform" method="post" action="appformsubmit">
 <div class="row">
@@ -58,7 +58,7 @@
         <tr>
         <td valign="top">
         <label for="comments">Why is the app ready for users</label> <br />
-        Please note how you applied <a href="<?php echo $DOCUMENTATION_DEVEL; ?>general/publishing.html#app-guidelines">the app guidelines</a> and share your thoughts on why the app would be a great addition for the ownCloud ecosystem.
+        Please note how you applied <a href="<?php echo $DOCUMENTATION_DEVEL; ?>app/publishing.html#app-guidelines">the app guidelines</a> and share your thoughts on why the app would be a great addition for the ownCloud ecosystem.
         </td>
         <td valign="top">
         <textarea  name="comments" maxlength="1000" cols="40" rows="6"></textarea>
