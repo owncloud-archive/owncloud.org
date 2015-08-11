9c751222cf37ca81f4136f531b41f5fc698c59da
Jos Poortvliet <jospoortvliet@gmail.com>
2015-08-11 16:12:12 +0200

    improve newsletter look

--- page-newsletter.php
+++ page-newsletter.php
@@ -2,8 +2,13 @@
   <h1>ownCloud Newsletter</h1>
 </div>
 <p>Once a month, we send out the ownCloud.org Newsletter. We cover what has happened over the last 4 weeks - releases, announcements, events and technical developments. Subscribe below to never miss any important ownCloud news!</p>
-<!-- Constant Contact Email List Form Button -->
+<div class="wrap container pad-bottom">
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3 text-center">
+		<!-- Constant Contact Email List Form Button -->
 		    <a href="http://visitor.r20.constantcontact.com/d.jsp?llr=ixens9uab&amp;p=oi&amp;m=1121878184680&amp;sit=mf4qye7jb&amp;f=cc4cd13f-acdf-45d3-b132-75252542c37d" class="btn btn-block btn-primary btn-lg join-button">Subscribe to our<br />monthly newsletter!</a><br />
-
+		</div>
+	</div>
+</div>
 <p>To follow ownCloud in more 'real-time' fashion, you can also subscribe to our news feed on <a href="/news">owncloud.org/news</a>.</p>
 
