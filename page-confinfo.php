dcb474266746c2baeedae5c57a0df4c2b42d6529
Jos Poortvliet <jospoortvliet@gmail.com>
2015-08-13 14:38:50 +0200

    first conf info page

---
+++ page-confinfo.php
@@ -0,0 +1,72 @@
+<div class="page-header">
+    <h1>Practical Conference Info 2015</h1>
+    <p>Here you find everything you need to know while participating in the ownCloud Contributor Conference!</p>
+</div>
+
+<div class="row conf-info">
+    <div class="col-md-6">
+    <h2>Registration and badge</h2>
+    <p>Be sure to have <a href="https://conference.owncloud.org/conference/oCC2015/register/new">registered in advance for the conference</a>! Pick up your badge and some food tickets at the entree of the "<em>Mathematik</em>" building, Strasse des 17. Juni 136.</br>
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/frontdoor.jpg" style="width: 95%" alt="front door" class="aligncenter" /></p>
+    <p>Note that carrying your badge is mandatory, especially after 18:00 and in the weekend!</p>
+    </div>
+    <div class="col-md-6">
+    <h2>Finding your way</h2>
+    <h3>Hackathon</h3>
+    <p>Friday, registration and hacking rooms open at 9. Walk from registration desk to the elevators. This is where we are:
+    <ul>
+        <li>5th floor:  food-and-drink room <code>MA542</code> and <a href="https://github.com/owncloud/core/wiki/Conference-meetings">meeting room</a> <code>MA548</code></li>
+        <li>6th floor the hacking rooms: <code>MA642</code> (<em>starters room</em>), <code>MA644</code>, <code>MA648</code>, <code>MA649</code> and <code>MA650</code></li>
+        <li>8th floor:interview room (<code>MA841</code>)</li>
+        <li>All the way to the top and up the stairs one more floor for lunch (with your food ticket!) and a great view over the city in the restaurant</li>
+    </ul>
+    </p>
+    <h3>Conference days - Saturday and Sunday</h3>
+    <p>Registration opens at 9 and at 10 we start with opening, keynotes and lightning talks in room <code>MA004</code>, main hall of the "<em>Mathematik</em>" building. After lunch the hacking rooms open while the main hall features workshops in the <code>MA004</code>, <code>MA005</code> and <code>MA144</code> rooms.</p>
+    </div>
+</div>
+
+<div class="row conf-info">
+    <div class="col-md-6">
+      <h2>Connectivity</h2>
+    <p>We've got wifi and many rooms are equiped with a (limited) number of network cables.</p>
+    <p>At registration, you sign up for a guest account. Use the how-to's here to connect:
+    <ul>
+        <li><a href="https://www.tubit.tu-berlin.de/wlan/">in German</a></li>
+        <li><a href="https://www.tubit.tu-berlin.de/wlan/parameter/en/">in English</a></li>
+    </ul>
+    </div>
+    <div class="col-md-6">
+    <h2>Getting stuff done and finding who to talk to</h2>
+    <p>New to ownCloud, looking for an introduction or answers about API's, infrastructure or a pull request?
+    <br/> <strong>ask your questions in <em>starters room</em> <code>MA642</code></strong></p>
+    <p>If you want to make sure a certain topic is discussed, make an agreement with the relevant people and <a href="https://github.com/owncloud/core/wiki/Conference-meetings">book the meeting room</a>.</p>
+    </div>
+</div>
+
+<div class="row conf-info">
+    <h2 class="text-center">Food and drinks</h2>
+    <div class="col-md-6">
+    <p>You get a number of food tickets at badge pickup:
+    <ul>
+        <li>Daily lunch tickets for the cafetaria on the top floor of the "<em>Mathematik</em>" building (during the week)</li>
+        <li>Ticket for the social event on Sunday evening</li>
+        <li>Ticket for a burger for Sunday lunch (available from 13:00 to 15:00)</li>
+    </ul>
+    We always have vegetarian and meat options.</p></div>
+    <div class="col-md-6">
+    <p>We have a wide variety of free drinks and some fruit and snacks in room <code>MA542</code> (5th floor of the "<em>Mathematik</em>" building).</p>
+    <p>During the hackathon we have free bread with a variety of things to put on it in room <code>MA542</code> - make your own dinner. Restaurants can be found at <a href="http://www.openstreetmap.org/?mlat=52.50602&mlon=13.32248#map=18/52.50602/13.32248">Savignyplatz</a>, a 10 minute walk from the venue.</p>
+    <p>During weekdays, there is an open cafe at the entree of the "<em>Mathematik</em>" building.</p>
+    </div>
+</div>
+
+
+
+
+
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+    <h1 class="text-center">Have fun and build some awesome stuff!</h1>
+    </div>
+</div>
