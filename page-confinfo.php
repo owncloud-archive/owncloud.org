b3005c09dc8549c939c3f46dd1d8347b7fd17453
Jos Poortvliet <jospoortvliet@gmail.com>
2015-08-13 15:17:23 +0200

    implement suggestions from Jan

--- page-confinfo.php
+++ page-confinfo.php
@@ -6,9 +6,9 @@
 <div class="row conf-info">
     <div class="col-md-6">
     <h2>Registration and badge</h2>
-    <p>Be sure to have <a href="https://conference.owncloud.org/conference/oCC2015/register/new">registered in advance for the conference</a>! Pick up your badge and some food tickets at the entree of the "<em>Mathematik</em>" building, Strasse des 17. Juni 136.</br>
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/frontdoor.jpg" style="width: 95%" alt="front door" class="aligncenter" /></p>
-    <p>Note that carrying your badge is mandatory, especially after 18:00 and in the weekend!</p>
+    <p>Be sure to have <a href="https://conference.owncloud.org/conference/oCC2015/register/new">registered in advance for the conference</a>! Pick up your badge and some food tickets at the entree of the »<em>Mathematik</em>« building, Straße des 17. Juni 136.</br>
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/conference/frontdoor.jpg" style="width: 100%" alt="front door" class="aligncenter" /></p>
+    <p>Note that carrying your badge is mandatory, especially after 18:00 and on the weekend!</p>
     </div>
     <div class="col-md-6">
     <h2>Finding your way</h2>
@@ -22,14 +22,14 @@
     </ul>
     </p>
     <h3>Conference days - Saturday and Sunday</h3>
-    <p>Registration opens at 9 and at 10 we start with opening, keynotes and lightning talks in room <code>MA004</code>, main hall of the "<em>Mathematik</em>" building. After lunch the hacking rooms open while the main hall features workshops in the <code>MA004</code>, <code>MA005</code> and <code>MA144</code> rooms.</p>
+    <p>Registration opens at 9 and at 10 we start with opening, keynotes and lightning talks in room <code>MA004</code>, main hall of the »<em>Mathematik</em>« building. After lunch the hacking rooms open while the main hall features workshops in the <code>MA004</code>, <code>MA005</code> and <code>MA144</code> rooms.</p>
     </div>
 </div>
 
 <div class="row conf-info">
     <div class="col-md-6">
       <h2>Connectivity</h2>
-    <p>We've got wifi and many rooms are equiped with a (limited) number of network cables.</p>
+    <p>We've got wifi and many rooms are equipped with a (limited) number of network cables.</p>
     <p>At registration, you sign up for a guest account. Use the how-to's here to connect:
     <ul>
         <li><a href="https://www.tubit.tu-berlin.de/wlan/">in German</a></li>
@@ -38,8 +38,8 @@
     </div>
     <div class="col-md-6">
     <h2>Getting stuff done and finding who to talk to</h2>
-    <p>New to ownCloud, looking for an introduction or answers about API's, infrastructure or a pull request?
-    <br/> <strong>ask your questions in <em>starters room</em> <code>MA642</code></strong></p>
+    <p>New to ownCloud, looking for an introduction or answers about APIs, infrastructure or a pull request?
+    <br/> <strong>ask your questions in the <em>starters room</em> <code>MA642</code></strong></p>
     <p>If you want to make sure a certain topic is discussed, make an agreement with the relevant people and <a href="https://github.com/owncloud/core/wiki/Conference-meetings">book the meeting room</a>.</p>
     </div>
 </div>
@@ -49,15 +49,15 @@
     <div class="col-md-6">
     <p>You get a number of food tickets at badge pickup:
     <ul>
-        <li>Daily lunch tickets for the cafetaria on the top floor of the "<em>Mathematik</em>" building (during the week)</li>
+        <li>Daily lunch tickets for the cafetaria on the top floor of the »<em>Mathematik</em>« building (during the week)</li>
         <li>Ticket for the social event on Sunday evening</li>
         <li>Ticket for a burger for Sunday lunch (available from 13:00 to 15:00)</li>
     </ul>
     We always have vegetarian and meat options.</p></div>
     <div class="col-md-6">
-    <p>We have a wide variety of free drinks and some fruit and snacks in room <code>MA542</code> (5th floor of the "<em>Mathematik</em>" building).</p>
+    <p>We have a wide variety of free drinks and some fruit and snacks in room <code>MA542</code> (5th floor of the »<em>Mathematik</em>« building).</p>
     <p>During the hackathon we have free bread with a variety of things to put on it in room <code>MA542</code> - make your own dinner. Restaurants can be found at <a href="http://www.openstreetmap.org/?mlat=52.50602&mlon=13.32248#map=18/52.50602/13.32248">Savignyplatz</a>, a 10 minute walk from the venue.</p>
-    <p>During weekdays, there is an open cafe at the entree of the "<em>Mathematik</em>" building.</p>
+    <p>During weekdays, there is an open cafe at the entree of the »<em>Mathematik</em>« building.</p>
     </div>
 </div>
 
