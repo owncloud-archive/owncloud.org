341ec5ef9d7b08cad8ce74c24bbeba2599307f4f
Jos Poortvliet <jospoortvliet@gmail.com>
2015-09-17 11:01:44 +0200

    Create an Android page

---
+++ page-android.php
@@ -0,0 +1,8 @@
+<div class="page-header">
+	<h1>ownCloud Android Client</h1>
+</div>
+<h2>Get it</h2>
+Find the ownCloud Android Client <a href="/install">on our install page</a>.
+
+<h2>Find Help</h2>
+Find help in <a href="https://doc.owncloud.org/android/">our documentation</a>.
