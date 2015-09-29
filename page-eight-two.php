501c5f62db33c9dbf6d94f6a46cbc92a301fde5b
Jos Poortvliet <jospoortvliet@gmail.com>
2015-09-29 11:10:15 +0200

    added text with 8.2 features

--- page-eight-two.php
+++ page-eight-two.php
@@ -34,14 +34,14 @@
 
 			<div class="col-md-4 featureblock">
 				<i class="icon-picture-o"></i>
-				<h3 class="ft-title">Pictures becomes Gallery</h3>
+				<h3 class="ft-title">Pictures Becomes Gallery</h3>
 				<p class="featuredesc">The Gallery app replaces the Pictures with a much nicer user experience introducing image sorting, image zoom, easy switching to the gallery and more.</p>
 			</div>
 
 			<div class="col-md-4 featureblock">
 				<i class="icon-bell"></i>
-				<h3 class="ft-title">Notifications and deletions</h3>
-				<p class="featuredesc">Admins now have the ability to send notifications to users, set limits on file retention in the trash and encrypt and decrypt data.</p>
+				<h3 class="ft-title">Notifications and Deletions</h3>
+				<p class="featuredesc">Admins now have the ability to send notifications to users and set limits on file retention in the trash.</p>
 			</div>
 		</div>
 	</div>
@@ -61,43 +61,19 @@
 		<div class="row featurerow">
 			<div class="col-md-4 featureblock">
 				<img class="thumbnail" alt="sharing" src="<?php bloginfo('template_directory'); ?>/assets/img/features/sharing.png" />
-				<h3 class="featuretitle">Federated Cloud ID</h3></br>
-				<p class="featuredesc">You can now find your Federated Cloud ID in the settings, making it easy to start sharing with other ownCloud servers. Federated Cloud ID's added to your address book will auto-complete in the share dialog.</p>
+				<h3 class="featuretitle">Pictures Becomes Gallery</h3></br>
+				<p class="featuredesc">The Gallery app replaces the Pictures app for browsing and viewing images. Supporting a wider range of image formats, better performance and new features, Gallery presents a significant enhancement. You can now sort your images, zoom in and out while viewing, and you can configure many specific settings in a <a href="https://github.com/owncloud/gallery/wiki/Gallery-configuration">simple text file</a>.</p>
 			</div>
 
 			<div class="col-md-4 featureblock">
 				<img class="thumbnail" alt="documentation" src="<?php bloginfo('template_directory'); ?>/assets/img/features/tips.png" />
-				<h3 class="featuretitle">Integrated Documentation</h3></br>
-				<p class="featuredesc">To make ownCloud easier to use, documentation has been massively improved and integrated in the interface. Warnings and notifications come with documentation links, options are explained in the user interface and a tips and tricks section in the admin settings bring the ownCloud documentation closer.</p>
+				<h3 class="featuretitle">New Sidebar</h3></br>
+				<p class="featuredesc">A new sidebar in the Files app makes it easy to view details on a file without cluttering the interface. A larger preview with meta data, file activity, sharing state and older versions all reside in the new sidebar. In the future, ownCloud apps can <a href="<?php echo $DOCUMENTATION_DEVELOPER; ?>app">add further functionality</a> through panels in the sidebar.</p>
 			</div>
-
 			<div class="col-md-4 featureblock">
 				<img class="thumbnail" alt="faster" src="<?php bloginfo('template_directory'); ?>/assets/img/features/api.png" />
-				<h3 class="featuretitle">Performance</h3></br>
-				<p class="featuredesc">This ownCloud release brings much improved performance, with up to 4x faster up- and downloads and an order of magnitude faster file manipulation. Better scalability through improved handling of parallel requests means ownCloud can now handle up to 50% more active users on each ownCloud Server instance.</p>
-			</div>
-		</div>
-
-		<div class="row featurerow">
-			<div class="col-md-4 featureblock">
-				<img class="thumbnail" alt="faster" src="<?php bloginfo('template_directory'); ?>/assets/img/features/documents.png" />
-				<h3 class="featuretitle">Improved Documents</h3></br>
-				<p class="featuredesc">An updated ownCloud Documents app improves on document compatibility and introduces new features like indentation buttons and annotations support as well as working keyboard shortcuts for Firefox users.</p>
-			</div>
-			<div class="col-md-8 featureblock">
-				<img class="thumbnail" alt="other features" src="<?php bloginfo('template_directory'); ?>/assets/img/features/design.png" />
-				<h3 class="featuretitle">Papercuts</h3>
-				<p class="featuredesc">There have been many small changes to ownCloud, including:</p></br></br>
-				<ul>
-				    <li>Improved thumbnail generation engine and added preview support for fonts and 3D images.</li>
-				    <li>Automatic database, trash and version cleanups don't cause system wide slow-downs anymore</li>
-				    <li>With certain mail server configurations, setting the correct sender address now works</li>
-				    <li>File meta data like sharing or favorite state is preserved when files are deleted and undeleted</li>
-				    <li><a href="http://morrisjobke.de/2015/07/22/Conditional-Logging-in-ownCloud/" target="_blank">Conditional logging</a> extends logging to debug level when conditions you set are met</li>
-				    <li>Upgrading is far more reliable due to better handling of maintenance mode and more (optional) <a href="http://morrisjobke.de/2015/07/23/Verbose-ownCloud-upgrade/" target="_blank">verbosity during the upgrade</a></li>
-				    <li>User avatars are shown in sharing dialogs</li>
-				    <li>And much more.</li>
-				</ul>
+				<h3 class="featuretitle">Refined User Interface</h3></br>
+				<p class="featuredesc">By reducing the number of colors and shades, the ownCloud interface is cleaner. Actions on files like download, rename and delete can now be found in the three-dot 'more' menu and this release also introduces new, more modern icons.</p>
 			</div>
 		</div>
 
@@ -119,52 +95,68 @@
 		<div class="featurewrapper">
 			<h2>Administration and Management Enhancements</h2>
 		</div>
-		<p>System administrators are strongly encouraged to check out the <a href="https://doc.owncloud.org/server/8.1/admin_manual/release_notes.html">release notes for ownCloud 8.1</a> before upgrading or doing a fresh installation!</p>
+		<p>System administrators are strongly encouraged to check out the <a href="https://doc.owncloud.org/server/8.2/admin_manual/release_notes.html">release notes for ownCloud Server 8.2</a> before upgrading or doing a fresh installation!</p>
 		<div class="row featurerow">
 			<div class="col-md-4 featureblock">
 				<img class="thumbnail" alt="external_storage" src="<?php bloginfo('template_directory'); ?>/assets/img/features/external.png" />
-				<h3 class="featuretitle">New Mount Options for External Storage</h3></br>
-				<p class="featuredesc">External storage now features new mount options. Performance can be fine-tuned per mount by enabling or disabling encryption and previews and choosing how often to check for changes to the underlying storage. Some authentication options have been added to CIFS/SMB and FTPS (which now features SSH key support).</p>
+				<h3 class="featuretitle">Notification Mechanism</h3></br>
+				<p class="featuredesc">ownCloud now offers a notification window where an admin can push notifications out to users of the ownCloud system, such as notification about a maintenance window. This is currently only in the web interface but future mobile and desktop client releases will incorporate the notification feature.</p>
 			</div>
 
 			<div class="col-md-4 featureblock">
 				<img class="thumbnail" alt="ldap" src="<?php bloginfo('template_directory'); ?>/assets/img/features/avatar.png" />
-				<h3 class="featuretitle">LDAP Wizard Improvements</h3></br>
-				<p class="featuredesc">The LDAP wizard has been updated to be faster and offer several (optional) auto-detection features and test buttons to verify settings. The user interface was also improved, offering visual user and group selection options among other things.</p>
+				<h3 class="featuretitle">Trash and Versions Retention Control</h3></br>
+				<p class="featuredesc">The new guaranteed file deletion option allows an admin to set a policy for retaining deleted files and file versions – setting the file retention time to automatic, min, max, both or none. In this way, admins have the flexibility to manage storage and quota for their users as they require.</p>
 			</div>
 			<div class="col-md-4 featureblock">
 				<img class="thumbnail" alt="encryption" src="<?php bloginfo('template_directory'); ?>/assets/img/features/encryption.png" />
-				<h3 class="featuretitle">Encryption 2.0</h3></br>
-				<p class="featuredesc">A modularization of the Encryption capabilities in ownCloud allows for external key management, provides improved reliability and management, and sets the foundation for future encryption enhancements.</p>
+				<h3 class="featuretitle">Encrypt / Decrypt Command</h3></br>
+				<p class="featuredesc">After enabling, disabling or changing encryption keys, existing user data can be (re) encrypted or decrypted with the command line occ tool.</p>
 			</div>
 		</div>
 
 		<div class="row featurerow">
 			<div class="col-md-4 featureblock">
 				<img class="thumbnail" alt="security" src="<?php bloginfo('template_directory'); ?>/assets/img/features/security.png" />
-				<h3 class="featuretitle">Security Improvements</h3></br>
-				<p class="featuredesc">There have been a raft of architectural improvements and changes made which harden ownCloud against security threats. Some of these <a href="https://statuscode.ch/2015/05/security-and-owncloud-8.1/">are described here</a>.</p>
-			</div>
-
-			<div class="col-md-4 featureblock">
-				<img class="thumbnail" alt="Updates" src="<?php bloginfo('template_directory'); ?>/assets/img/features/update.png" />
-				<h3 class="featuretitle">Release Channels</h3></br>
-				<p class="featuredesc">The built in Updater app and our package repositories now offer different Release Channels to keep ownCloud updated from. Administrators can choose to track the latest released code, a more conservative stable branch, release candidates, or daily builds to align their ownCloud installation to their needs. <a href="/release-channels" target="_blank">Make your choice here</a>.</p>
-			</div>
-			<div class="col-md-4 featureblock">
-				<img class="thumbnail" alt="Updates" src="<?php bloginfo('template_directory'); ?>/assets/img/features/store.png" />
-				<h3 class="featuretitle">Experimental apps</h3></br>
-				<p class="featuredesc">The built in app store now has the ability to install all apps from the <a href="https://apps.owncloud.com">online app store</a>, including the apps not yet approved and verified. This comes, of course, at a security- and stability risk, which the administrator should be aware off.</p>
+				<h3 class="featuretitle">Improved Mimetype Handling</h3></br>
+				<p class="featuredesc">Administrators can now can define custom mimetype mappings and aliases. The occ tool has been updated to allow scanning for new mimetypes and update the database to ensure files are properly identified.</p>
 			</div>
 		</div>
+
 
 		<div class="row featurerow">
-			<div class="col-md-4 featureblock">
-				<img class="thumbnail" alt="example theme" src="<?php bloginfo('template_directory'); ?>/assets/img/features/design.png" />
-				<h3 class="featuretitle">Example Theme</h3></br>
-				<p class="featuredesc">To make developing a custom ownCloud theme easier, a new example theme was included with ownCloud 8.1. Find details on how to theme ownCloud in the <a href="<?php echo $DOCUMENTATION_ADMIN; ?>operations/theming.html">documentation</a>.</p>
+                        <div class="col-md-6 featureblock">
+				<img class="thumbnail" alt="security" src="<?php bloginfo('template_directory'); ?>/assets/img/features/security.png" />
+				<h3 class="featuretitle">Security Improvements</h3></br>
+				<p>ownCloud Server 8.1 has received a significant amount of security hardening, including the following:</br>
+				<ul class="featureslist">
+				<li>ownCloud now encrypts user keys in the ownCloud session. This means that, even if a server is compromised and the session data is discovered, user private keys stored in the session – required to unencrypt files stored on the server – can no longer be read.</li>
+				<li>$.get and $.post cannot be used to execute remote JavaScript anymore.</li>
+				<li>The CSRF token is not added to cross-domain requests initiated by jQuery anymore and is displayed differently on each request to mitigate BREACH alike attacks.</li>
+				<li>Referers are not sent anymore to prevent leaking of potential sensitive information within the URL such as the filename.</li>
+				<li>Autoloader is now only loading files from folders that are explicitly whitelisted and only allows loading files from enabled apps.</li>
+				<li>Password reset tokens are now expiring after 12 hours or if a user logged-in again.</li>
+				</ul>
+				Find <a href="https://github.com/owncloud/core/wiki/ownCloud-8.2-Features#security">more changes and details here</a>.
+				</p>
 			</div>
-		</div>
+                        <div class="col-md-6 featureblock">
+				<img class="thumbnail" alt="Updates" src="<?php bloginfo('template_directory'); ?>/assets/img/features/update.png" />
+				<h3 class="featuretitle">Other Improvements</h3></br>
+				<p>There have been many other small changes that benefit system administrators, including:</br>
+				<ul class="featureslist">
+				<li>SMB external storage can have a domain specified, separate from the username, for use with session credentials.</li>
+				<li>Made temporary directory configurable in config.php.</li>
+				<li>Unavailable external storages are not rechecked every request, their failed state is saved and they are tried again after 10 minutes.</li>
+				<li>The upgrade page now states which apps are being upgraded.</li>
+				<li>Downgrades are now prevented as they are unsupported and dangerous.</li>
+				<li>The admin page now warns for PHP versions which are End of Life so admins can prepare to upgrade and avoid security problems which come with unsupported PHP versions.</li>
+				<li>New occ commands to simplify logging configuration and commands to get/set system and app configuration values.</li>
+				</ul>
+				</p>
+			</div>
+                </div>
+
 		<div class="row">
 			<div class="col-md-3"><a href="<?php echo get_template_directory_uri(); ?>/assets/img/screenshots/server/eight-one/tips-and-tricks.png" title="Tips and tricks as well as links to documentation are well integrated in the admin panel" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/screenshots/server/eight-one/tips-and-tricks-wee.png" style="width: 100%" alt="..." class="img-thumbnail"></a></div>
 			<div class="col-md-3"><a href="<?php echo get_template_directory_uri(); ?>/assets/img/screenshots/server/eight-one/mount-options.png" title="You can set encryption, update policy and previews for each external storage mount" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/screenshots/server/eight-one/mount-options-wee.png" style="width: 100%" alt="..." class="img-thumbnail"></a></div>
@@ -195,16 +187,35 @@
 		<div class="featurewrapper">
 			<h2>Improvements for Developers</h2>
 		</div>
-		ownCloud is <a href="http://karlitschek.de/2015/06/building-a-platform/">meant to be a platform</a>. For this release, much effort was put in the infrastructure for building and distributing apps for ownCloud.
-		<div class="row">
-			<ul class="featureslist">
-				<li>The process of getting apps in the appstore <a href="https://doc.owncloud.org/server/8.1/developer_manual/app/publishing.html">is now documented</a>.</li>
-				<li>We made many improvements to the ownCloud API: there have been over 190 additions and 50 deprecated functions.</li>
-				<li>Sharing capabilities are now exposed to allow external applications (desktop client, mobile clients etc.) to provide a better UX.</li>
-				<li>And much, much more.</li>
-			</ul>
-			<p>For more details, see this blog about <a href="https://owncloud.org/blog/owncloud-8-1-brings-massively-improved-api-for-developers/">ownCloud 8.1 for app developers</a>.</p>
+		ownCloud is <a href="http://karlitschek.de/2015/06/building-a-platform/">meant to be a platform</a>. This release introduces many improvements to the API, both to make more functionality available for app developers and to improve the existing interfaces.
+                        
+                <div class="row featurerow">
+			<div class="col-md-4 featureblock">
+				<img class="thumbnail" alt="external_storage" src="<?php bloginfo('template_directory'); ?>/assets/img/features/external.png" />
+				<h3 class="featuretitle">Modular Authentication Mechanism Support</h3></br>
+				<p class="featuredesc">External storage backends are now built upon a 'BackendService' class, decoupling authentication from data handling. This reduces complexity in each external storage backend and makes writing new backends easier. It also makes the existing backends more flexible and capable of dealing with a wider range of authentication mechanisms than just 'username-password'.</p>
+			</div>
+
+			<div class="col-md-4 featureblock">
+				<img class="thumbnail" alt="ldap" src="<?php bloginfo('template_directory'); ?>/assets/img/features/avatar.png" />
+				<h3 class="featuretitle">Capabilities and Query Building</h3></br>
+				<p class="featuredesc">A major change is the addition of a capabilities manager which allow apps to expose capabilities in a more generic way. App authors now also have access to a public API for the doctrine query builder.</p>
+			</div>
+			<div class="col-md-4 featureblock">
+				<img class="thumbnail" alt="encryption" src="<?php bloginfo('template_directory'); ?>/assets/img/features/encryption.png" />
+				<h3 class="featuretitle">New and Expanded Functions for Apps</h3></br>
+				<p class="featuredesc">As always, there have been many changes to the ownCloud API. For example, applications can now send notifications to end users and mimetype.js makes it possible to request a mimetype icon from JS. To further aid developers, the command 'occ app-check-code'  will notify about deprecated methods and classes.</p>
+			</div>
 		</div>
+
+               <div class="row featurerow">
+			<div class="col-md-4 featureblock">
+				<img class="thumbnail" alt="external_storage" src="<?php bloginfo('template_directory'); ?>/assets/img/features/external.png" />
+				<h3 class="featuretitle">Improved External API</h3></br>
+				<p class="featuredesc">The OCS Share API now returns a URL when fetching shares, allows setting expire date on new shares directly and returns permissions and expire date on creation.</p>
+			</div>
+                </div>
+
 		<p>See all features of ownCloud on the <a href="/features">Features page</a>.</p>
 		<h2>Get involved!</h2>
 		<p><a target="_blank" href="/promote/" class="btn btn-lg btn-default"> <i class="icon-bullhorn"></i> Discuss ownCloud with others</a></p>
