f9b95ef39e0159bf71864ff338c1fe2720cab186
Lukas Reschke <lukas@owncloud.com>
2015-09-30 17:57:17 +0200

    Add oC-SA-2015-017 and oC-SA-2015-018

--- advisories/advisory-side.php
+++ advisories/advisory-side.php
@@ -1,8 +1,8 @@
-<p>ownCloud server 8.1.1</p>
-<a href="/security/advisory?id=oc-sa-2015-014">Information Exposure Through Directory Listing in the file scanner</a><br/>
-<a href="/security/advisory?id=oc-sa-2015-015">Calendar export: Authorization Bypass Through User-Controlled Key</a><br/>
-<p>ownCloud desktop 2.0.1</p>
+<br/><p>ownCloud server 8.1.2</p>
+<a href="/security/advisory?id=oc-sa-2015-017">Command injection when using external SMB storage</a><br/>
+<a href="/security/advisory?id=oc-sa-2015-018">PHP arbitrary class instantiiation in "files_external"</a><br/>
+<br/><p>ownCloud desktop 2.0.1</p>
 <a href="/security/advisory?id=oc-sa-2015-016">Improper validation of certificates when using self-signed certificates</a><br/>
-<p>ownCloud mobile iOS 3.4.4</p>
+<br/><p>ownCloud mobile iOS 3.4.4</p>
 <a href="/security/advisory?id=oc-sa-2015-012">Improper validation of certificates within the iOS application</a><br/>
 <a href="/security/advisory?id=oc-sa-2015-013">Credentials potentially leaked to other configured ownCloud instance</a><br/>
