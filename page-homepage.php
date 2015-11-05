940370608d86764b1c3bf8534160244e392a3e71
Jos Poortvliet <jospoortvliet@gmail.com>
2015-11-05 19:06:33 +0100

    cut nr of social thingies

--- page-homepage.php
+++ page-homepage.php
@@ -112,14 +112,14 @@
 <div class="row 3-column-lg-md-sm">
 	<div class="col-lg-4 col-md-4 col-xs-12 col-sm-4">
 		<h2><i class="icon-twitter"></i> The latest <a href="https://twitter.com/ownclouders">twitter posts</a></h2>
-		<?php if(CONTRIBOOK) { CONTRIBOOK_MICROBLOG::show(0,3); } ?>
+		<?php if(CONTRIBOOK) { CONTRIBOOK_MICROBLOG::show(0,2); } ?>
 	</div>
 	<div class="col-lg-4 col-md-4 col-xs-12 col-sm-4">
 		<h2><i class="icon-comment"></i> The latest <a href="https://forum.owncloud.org">from the forum</a></h2>
-		<?php if(CONTRIBOOK) { CONTRIBOOK_FORUM::show(0,7); }?>
+		<?php if(CONTRIBOOK) { CONTRIBOOK_FORUM::show(0,5); }?>
 	</div>
 	<div class="col-lg-4 col-md-4 col-xs-12 col-sm-4">
 		<h2><i class="icon-cloud"></i> <a href="/news">ownCloud News</a></h2>
-		<?php if(CONTRIBOOK) { CONTRIBOOK_NEWS::show(0,9); } ?>
+		<?php if(CONTRIBOOK) { CONTRIBOOK_NEWS::show(0,6); } ?>
 	</div>
 </div>
