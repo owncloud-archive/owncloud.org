cf4c7d61f02174ed60e08db6bc024131019b9ae9
Jos Poortvliet <jospoortvliet@gmail.com>
2015-11-10 18:44:59 +0100

    text and design updates

--- page-consulting.php
+++ page-consulting.php
@@ -17,7 +17,10 @@ foreach($consultants as $consultant) {
 
 
 ?>
-<p>ownCloud consultants help organizations with deployment, maintenance and integration of ownCloud in their workflow. Find ownCloud consulting that fits your needs.</p>
+<p>While most ownCloud users are able to install and use ownCloud without any help or with assistance of volunteers in our community support channels, some deployments, especially by corporate users, may have more complex requirements that require professional services. This page lists individuals and companies offering services with regards to deployment, maintenance and integration of ownCloud in your workflow.
+</p>
+
+<div class="alert alert-info">If you offer ownCloud Consulting, you can be <a href="/consultants/apply">listed on this page</a>. To find out more about becoming an enterprise consultant with a support contract from <a target="_blank" href="https://owncloud.com">ownCloud Inc</a> please see <a target="_blank" href="https://owncloud.com/partner/">the partner page on ownCloud.com</a>.</div>
 
 <div class="row col-xs-12">
 	<h2>Partners</h2>
@@ -27,7 +30,7 @@ foreach($consultants as $consultant) {
 
 <div class="row col-xs-12">
 	<h2>Independent consultants</h2>
-	<p>For smaller ownCloud deployments, a wide variety of consulting firms and individuals offers support installing and deploying ownCloud or developing custom functionality.</p>
+	<p>The following individuals and firms offer ownCloud related services that may be of interest. The information provided here was provided by the entities named, and is not verified or endorsed by the ownCloud community or ownCloud, Inc. We offer this listing as a service to the ecosystem.</p>
 </div>
 <!-- <?php displayConsultants($supportedConsultants); ?>-->
 <?php displayConsultants($otherConsultants); ?>
@@ -41,21 +44,23 @@ function displayConsultants($consultants) {
 		echo '<div class="row">';
 		for($consultant=0; $consultant<$numConsultants; $consultant++) {
 			echo '<div class="col-xs-12 col-sm-6 col-md-4 ">';
+			echo '<div class="consulting thumbnail"><div class="bannerhead">';
+			echo '<a href="' . $url . '" target="_blank" rel="noreferrer" title="' . $consultants[$consultant]->title . '">';
+			echo '<img class="logo" src="' . get_template_directory_uri() . '/assets/img/consultants/' . $consultants[$consultant]->imagename . '"/>';
+
+			echo '</a><br \>';
 			if($free && isset($consultants[$consultant]->freeurl)) {
 				$url = $consultants[$consultant]->freeurl;
 			} else {
 				$url = $consultants[$consultant]->url;
 			}
-			echo '<div class="thumbnail"><div class="bannerhead">';
-			echo  $consultants[$consultant]->title . '<br \>';
+			echo  $consultants[$consultant]->title;
 			foreach($consultants[$consultant]->flags as $flag) {
 				echo '<img class="flag" src="' . get_template_directory_uri() . '/assets/img/flags/' . $flag . '.gif"/>';
 			}
-			echo '<a href="' . $url . '" target="_blank" rel="noreferrer" title="' . $consultants[$consultant]->title . '">';
-			echo '</div>';
-			echo '<img class="banner" src="' . get_template_directory_uri() . '/assets/img/consultants/' . $consultants[$consultant]->imagename . '"/>';
-
-			echo '</a><div class="bannerfoot">';
+			
+			echo '<br \></div>';
+			echo '<div class="bannerfoot">';
 			if(!empty($consultants[$consultant]->supports)) {
 				echo '<span>Ideal for organizations: </span>';
 				echo '<ul class="list-unstyled list-inline">';
@@ -72,7 +77,7 @@ function displayConsultants($consultants) {
 				echo '</ul>';
 			}
 			if($consultants[$consultant]->hosting) {
-				echo '<span class="text-primary freeplans">Provides hosting</br></span>';
+				echo '<span class="hosting">Provides hosting</br></span>';
 			} else {
 			echo '<br/>';
 			}
@@ -85,5 +90,5 @@ function displayConsultants($consultants) {
 }
 
 ?>
-<div class="alert alert-info">If you offer ownCloud Consulting, you can be <a href="/consultants/apply">listed on this page</a>. To find out more about becoming an enterprise consultant with a support contract from <a target="_blank" href="https://owncloud.com">ownCloud Inc</a> please see <a target="_blank" href="https://owncloud.com/partner/">the partner page on ownCloud.com</a>.</div>
+
 
