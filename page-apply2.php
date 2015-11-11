849f573a2d571593ce8e63cbacf16834d948a8dc
Jos Poortvliet <jospoortvliet@gmail.com>
2015-11-11 16:07:56 +0100

    add consulting & update provider guidelines

--- page-apply2.php
+++ page-apply2.php
@@ -1,6 +1,18 @@
 <div class="page-header">
     <h1>Apply to be a listed Consultant</h1>
 </div>
+<p>The ownCloud community <a href="/consulting">offers a list of parties providing services to ownCoud users</a>. No endorsement is implied by these listings. If you would like to have your organization added to this page, please follow these instructions:
+<ul>
+	<li>Entries must be factual and relevant to ownCloud. Generic services are not a good fit. Make sure that the link you submit contains references (and a link) to ownCloud.</li>
+	<li>Your organization and in particular the website at the link provided has to respect the <a href="/trademarks">ownCloud trademark policy</a>.</li>
+	<li>The description can be up to 135 characters. It should give an idea of the offered services so readers can quickly judge if an entry is of interest. Review the current list for the expected contents and format.</li>
+	<li>A logo has to be of the size of 290x70pixels in png format.</li>
+        <li>Note that being listed does not apply approval, endorsement or affiliation with the ownCloud community or project or ownCloud, Inc. and your website and/or description should not, incorrectly, claim so.</li>
+        <li>Entries with invalid links may be removed. Please keep us informed about changes.</li>
+        <li>Entries with incorrect data (wrong image size, too many characters, invalid email addresses or web links) might be silently ignored.</li>
+        <li>Submit a new entry if you made a mistake or if something has to change.</li>
+        <li>It can take up to 4 weeks to be listed. Please do not submit duplicate entries (unless there were changes). A new submission <strong>will delay being listed</strong> as we work from old to new.</li>
+</ul>
 <div class="row providers">
     <div class="span12">
         <p>To apply to be listed on the <a href="/consulting">ownCloud.org/consulting</a> webpage as an ownCloud
@@ -17,3 +29,4 @@
             <a target="_blank" href="https://owncloud.com/partner/">here</a>.</p>
     </div>
 </div>
+
