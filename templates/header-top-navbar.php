cb196a65c02c720df3dd165cfa36bda87ef51314
Jos Poortvliet <jospoortvliet@gmail.com>
2015-11-25 21:11:48 +0100

    avoid wrapping sentences badly

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -28,7 +28,7 @@
 		<div class="col-xs-offset-0 col-sm-offset-1 col-md-offset-2 col-lg-offset-3  col-xs-12 col-sm-10 col-md-8 col-lg-6">
 			<div class="text-center intro-text">
 				<h1>A safe home for all your data</h1>
-				<p>Access & share your files, calendars, contacts, mail & more from any device, on your terms</p>
+				<p><span class="avoidwrap">Access & share your files</span>, calendars, contacts, <span class="avoidwrap">mail & more</span> <span class="avoidwrap">from any device, on your terms</span></p>
 				<br>
 				<a class="btn btn-primary btn-lg" role="button" href="/install">Get your ownCloud &raquo;</a>
 			</div>
