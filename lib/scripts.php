a124eb60f418c87deee48c520661d12ee6792dba
Tom Needham <tom@owncloud.com>
2016-02-25 16:18:17 +0000

    Updrage to FontAwesome 4.5.0

--- lib/scripts.php
+++ lib/scripts.php
@@ -29,7 +29,7 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.7.0.min.js', false, null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, 'be373268f9b8ecda7c3a45154676e637', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '9a9c4afed9ced6b53968c922ddb8aefe', true);
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
   wp_enqueue_script('roots_scripts');
