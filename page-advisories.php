66be3ff57a3b8a2896d98dba4eabff05854c343b
Jos Poortvliet <jospoortvliet@gmail.com>
2016-04-08 13:18:14 +0200

    link to RSS feed

--- page-advisories.php
+++ page-advisories.php
@@ -1,8 +1,8 @@
 <div class="row page-content-header">
-	<div class="col-md-4">
+	<div class="col-md-5">
 		<h1>Security Advisories</h1>
 	</div>
-	<div class="col-md-5 col-md-offset-3">
+	<div class="col-md-5 col-md-offset-2">
 		<form role="form" action="/security/advisory/" method="get">
 			<input name="id" type="text" class="form-control" placeholder="Advisory identifier">
 			<button type="submit" class="btn btn-default">Go</button>
@@ -10,8 +10,9 @@
 	</div>
 </div>
 <div class="sub-nav"><a href="/security">Security page</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="/security/hall-of-fame">Hall of Fame</a></div>
+<div style="margin-top: 20px;"><p>You can <a href="<?php echo get_template_directory_uri(); ?>/advisories/advisories.rss">follow our advisories via RSS</a>.</p></div>
 <div class="row" style="
-    margin-top: 40px;
+    margin-top: 20px;
 ">
 	<div class="col-md-4">
 		<h2>ownCloud Server</h2><br>
