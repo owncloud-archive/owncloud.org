7f823716308d2303e4f05164ee92a7db3b3b02aa
Jos Poortvliet <jospoortvliet@gmail.com>
2016-05-04 12:40:14 +0200

    update release channels

--- page-release-channels.php
+++ page-release-channels.php
@@ -26,11 +26,10 @@
 		       That does not imply that these releases are bug free. The issues are known and can be worked around. The downside of this channel is that the releases are behind in terms of features and performance.</td>
             <td class="info">Older archives are available from our <a href="/changelog">changelog</a>.<br />
 		    Production repositories are available for the following major versions:
-<!--		    <a href="https://download.owncloud.org/download/repositories/9.0/owncloud/">9.0</a> - coming when 9.0.1 is out-->
+		    <a href="https://download.owncloud.org/download/repositories/9.0/owncloud/">9.0</a> - coming when 9.0.1 is out
 		    <a href="https://download.owncloud.org/download/repositories/8.2/owncloud/">8.2</a>
 		    <a href="http://software.opensuse.org/download/package?project=isv:ownCloud:community:8.1&package=owncloud">8.1</a>
 		    <a href="http://software.opensuse.org/download/package?project=isv:ownCloud:community:8.0&package=owncloud">8.0</a>
-		    <a href="http://software.opensuse.org/download/package?project=isv:ownCloud:community:7.0&package=owncloud">7.0</a>
 		    <br>Register and activate one of these repositories with your package manager to get updates within the same major version<a href="#warning">*</a>
             </td>
     </tr>
@@ -65,7 +64,7 @@
         If you pick a <strong>specific Linux Package repository</strong> rather than following stable, you assume the responsibility for a timely upgrade to a new stable release once your current release is no longer supported. We support releases for a significant time, but not forever, and at some point you have to move to a newer version to continue to receive security and stability improvements.</br>
         If at all possible, we suggest to <strong>upgrade sooner rather than later</strong>. Doing a test on a copy of your production environment is a better way of ensuring the new version works for you than waiting and hoping all issues are found and fixed – that way, you lose out on many of the benefits of the new release, not only in terms of features and user interface enhancements but also when it comes to performance improvements and scalability.</p>
         <p>Note that from 8.2 onward, ownCloud packages can be found on download.owncloud.org rather than on the openSUSE Open Build Service. This means you will have to move repositories if you want to upgrade from the Stable 8.1 release.</p>
-        <p>To <em>upgrade in the safest way possible</em>, always update to the latest minor release before upgrading to a new version. As an extreme example, to upgrade from 7.0 all the way to 9.0, upgrade 7.0.x to 7.0.13, then upgrade to 8.0.11, 8.1.7, 8.2.4, then 9.0.1.</p>
+        <p>To <em>upgrade in the safest way possible</em>, always update to the latest minor release before upgrading to a new version. As an extreme example, to upgrade from 7.0 all the way to 9.0, upgrade 7.0.x to 7.0.14, then upgrade to 8.0.13, 8.1.8, 8.2.5, then 9.0.2.</p>
     </div>
     <div class="col-lg-3 col-md-4 col-sm-6 col-xs-10">
         <a href="<?php echo get_template_directory_uri(); ?>/assets/img/screenshots/server/eight-one/update-channels.png" title="Update Channel selection in Updater app" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/screenshots/server/eight-one/update-channels-wee.png" style="width: 100%" alt="..." class="img-thumbnail"></a>
