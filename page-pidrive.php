0e6355acae1115f8aebd168e1b9f44dd954daeaf
Jos Poortvliet <jospoortvliet@gmail.com>
2016-05-23 12:28:18 +0200

    more changes

--- page-pidrive.php
+++ page-pidrive.php
@@ -1,7 +1,7 @@
 <div class="row conf-header">
 	<div class="col-sm-6 col-sm-offset-3">
 		<img class="conf-img img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/pidrive/evolution.jpeg" alt="succession of ownCloud devices" />
-		<h2 class="conf-location">ownCloud and WD Labs collaboration: the ownCloud Pi Project</h2>
+		<h2 class="conf-location">ownCloud and WDLabs collaboration: the ownCloud PiDriveProject</h2>
 	</div>
 </div>
 <div class="row conf-info">
@@ -15,15 +15,15 @@
 			<li>Open</li>
 			<li>Safe</li>
 		</ul>
-		<p>on which people can host their data at home. Western Digital offers the <a href="http://wdlabs.wd.com/products/wd-pidrive-1tb-kit/" target="_blank">amazing PiDrive kit</a> which we used to prototype the project. This brings a 1TB hard drive in an enclosure together with a miniature computer board powered by a Micro USB phone charger.</p>
+		<p>on which people can host their data at home. Western Digital offers the <a href="http://wdlabs.wd.com/products/wd-pidrive-1tb-kit/" target="_blank">amazing PiDrive kit</a> which we used to prototype the project. This brings a 1TB hard drive in an enclosure together with a miniature computer board powered by an included Micro USB charger (>2A).</p>
 	</div>
 	<div class="col-sm-6">
 		<h1 class="text-center"><i class="icon-code-fork"></i></br> Technology</h1>
 		<p>Technology is still in flux but we've largely settled on these choices</p>
 		<ul>
 			<li>Raspberry Pi 2 for initial batch; looking at the oDroid C2 for after that</li>
-			<li>1 or 2 TB USB3 hard drive from WD</li>
-			<li>Special case for the ownCloud Pi project</li>
+			<li>314GB, 1TB or 2TB USB3 hard drive from WD (still looking at options there)</li>
+			<li>Special case for the ownCloud Pi project (wh00t!)</li>
 			<li><a href="https://developer.ubuntu.com/en/snappy/" target="_blank">Snappy Ubuntu Core</a> as OS</li>
 			<li>Apache, MySQL, latest ownCloud</li>
 		</ul>
