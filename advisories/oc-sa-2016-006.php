0abc551e42b1f152f348d2a875dcff05e326ff2e
Thomas Müller <thomas.mueller@tmit.eu>
2016-07-21 11:12:50 +0200

    Adding advisories for oC-SA-2016-006, oC-SA-2016-007, oC-SA-2016-008, oC-SA-2016-009 and oC-SA-2016-010

---
+++ advisories/oc-sa-2016-006.php
@@ -0,0 +1,42 @@
+<div class="row page-content-header">
+<div class="col-md-4">
+    <h1>Security Advisory</h1>
+    <a href="/security/advisories/">Back to advisories</a>
+</div>
+</div>
+<div class="row">
+    <div class="col-md-4">
+        <?php get_template_part('advisories/advisory-side'); ?>
+    </div>
+    <div class="col-md-8">
+        <h2>Insecure Direct Object References in Gallery (oC-SA-2016-006)</h2>
+        <p>13th July 2016</p>
+        <p>Risk level: <strong>Medium</strong></p>
+        <p>CVSS v2 Base Score: 5 (<a href="https://nvd.nist.gov/cvss.cfm?calculator&version=2&vector=(AV:N/AC:L/Au:N/C:P/I:N/A:N)">AV:N/AC:L/Au:N/C:P/I:N/A:N</a>)</p>
+        <p>CWE: <a href="https://cwe.mitre.org/data/definitions/932.html">Insecure Direct Object References (CWE-932)</a></p>
+        <h3>Description</h3>
+        <p><p>ownCloud was vulnerable to a insecure direct object reference. Any unauthenticated user would be able to download any image from the server if the gallery app is enabled.</p>
+</p>
+        <h3>Affected Software</h3>
+        <ul>
+            <li>ownCloud Server &lt; <strong>8.2.5</strong> (CVE-2016-xxxx)</li>
+<ul>
+<li><a href="https://github.com/owncloud/gallery/commit/2e8f1f2509d15876ab09396dfe6c463aacdf5c5b">gallery/2e8f1f2509d15876ab09396dfe6c463aacdf5c5b</a></li>
+</ul>
+<li>ownCloud Server &lt; <strong>9.0.2</strong> (CVE-2016-xxxx)</li>
+<ul>
+<li><a href="https://github.com/owncloud/gallery/commit/e3021ab6cc4e37eccb27eab6374722632c85eed7">gallery/e3021ab6cc4e37eccb27eab6374722632c85eed7</a></li>
+</ul>
+
+        </ul>
+        <h3>Action Taken</h3>
+        <p><p>The share token is verified.</p>
+</p>
+        <h3>Acknowledgements</h3>
+        <p>The ownCloud team thanks the following people for their research and responsible disclosure of the above advisory:</p>
+        <ul>
+            <li>Lukas Reschke - Vulnerability discovery and disclosure.</li>
+        </ul>
+        <br/>
+    </div>
+</div>
---
+++ advisories/oc-sa-2016-007.php
@@ -0,0 +1,32 @@
+<div class="row page-content-header">
+<div class="col-md-4">
+    <h1>Security Advisory</h1>
+    <a href="/security/advisories/">Back to advisories</a>
+</div>
+</div>
+<div class="row">
+    <div class="col-md-4">
+        <?php get_template_part('advisories/advisory-side'); ?>
+    </div>
+    <div class="col-md-8">
+        <h2> (oC-SA-2016-007)</h2>
+        <p>1st January 1970</p>
+        <p>Risk level: <strong></strong></p>
+        
+        
+        <h3>Description</h3>
+        <p></p>
+        <h3>Affected Software</h3>
+        <ul>
+            
+        </ul>
+        <h3>Action Taken</h3>
+        <p></p>
+        <h3>Acknowledgements</h3>
+        <p>The ownCloud team thanks the following people for their research and responsible disclosure of the above advisory:</p>
+        <ul>
+            
+        </ul>
+        <br/>
+    </div>
+</div>
