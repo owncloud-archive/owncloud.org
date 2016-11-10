668ddbe052499f02731c8d0d6406c48ebb839579
Peter Prochaska <info@peter-prochaska.de>
2016-11-10 14:12:49 +0100

    new advisories

--- advisories/advisory-side.php
+++ advisories/advisory-side.php
@@ -1,11 +1,11 @@
-<br/><p>ownCloud server 9.0.4</p>
-<a href="/security/advisory?id=oc-sa-2016-011">Stored XSS in gallery application</a><br/>
-<a href="/security/advisory?id=oc-sa-2016-013">Content-Spoofing in files app</a><br/>
-<a href="/security/advisory?id=oc-sa-2016-012">Log pollution can potentially lead to local HTML injection</a><br/>
-<a href="/security/advisory?id=oc-sa-2016-015">Read-only share recipient can restore old versions of file</a><br/>
-<a href="/security/advisory?id=oc-sa-2016-014">Edit permission check not enforced on WebDAV COPY action</a><br/>
+<br/><p>ownCloud server 9.1.2</p>
+<a href="/security/advisory?id=oc-sa-2016-016">SMB User Authentication Bypass</a><br/>
+<a href="/security/advisory?id=oc-sa-2016-017">Stored XSS in CardDAV image export</a><br/>
+<a href="/security/advisory?id=oc-sa-2016-018">Reflected XSS in Gallery application</a><br/>
+<a href="/security/advisory?id=oc-sa-2016-019">Content-Spoofing in "files" app</a><br/>
+<a href="/security/advisory?id=oc-sa-2016-020">Content-Spoofing in "dav" app</a><br/>
 <br/><p>ownCloud desktop 2.2.3</p>
 <a href="/security/advisory?id=oc-sa-2016-016">Local Code Injection</a><br/>
 <br/><p>ownCloud mobile iOS 3.4.4</p>
-<a href="/security/advisory?id=oc-sa-2015-013">Credentials potentially leaked to other configured ownCloud instance</a><br/>
 <a href="/security/advisory?id=oc-sa-2015-012">Improper validation of certificates within the iOS application</a><br/>
+<a href="/security/advisory?id=oc-sa-2015-013">Credentials potentially leaked to other configured ownCloud instance</a><br/>
