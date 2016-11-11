b8b4e39e756943f6025836c596c5c0dc9ac99a0f
Peter Prochaska <Peter-Prochaska@users.noreply.github.com>
2016-11-11 12:50:57 +0100

    fixed number confusion (#1101) (#1102)

--- advisories/advisory-side.php
+++ advisories/advisory-side.php
@@ -1,9 +1,9 @@
 <br/><p>ownCloud server 9.1.2</p>
-<a href="/security/advisory?id=oc-sa-2016-016">SMB User Authentication Bypass</a><br/>
-<a href="/security/advisory?id=oc-sa-2016-017">Stored XSS in CardDAV image export</a><br/>
-<a href="/security/advisory?id=oc-sa-2016-018">Reflected XSS in Gallery application</a><br/>
-<a href="/security/advisory?id=oc-sa-2016-019">Content-Spoofing in "files" app</a><br/>
-<a href="/security/advisory?id=oc-sa-2016-020">Content-Spoofing in "dav" app</a><br/>
+<a href="/security/advisory?id=oc-sa-2016-017">SMB User Authentication Bypass</a><br/>
+<a href="/security/advisory?id=oc-sa-2016-018">Stored XSS in CardDAV image export</a><br/>
+<a href="/security/advisory?id=oc-sa-2016-019">Reflected XSS in Gallery application</a><br/>
+<a href="/security/advisory?id=oc-sa-2016-020">Content-Spoofing in "files" app</a><br/>
+<a href="/security/advisory?id=oc-sa-2016-021">Content-Spoofing in "dav" app</a><br/>
 <br/><p>ownCloud desktop 2.2.3</p>
 <a href="/security/advisory?id=oc-sa-2016-016">Local Code Injection</a><br/>
 <br/><p>ownCloud mobile iOS 3.4.4</p>
