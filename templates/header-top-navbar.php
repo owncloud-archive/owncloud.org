1929510f1be0c12e3feabaf1c540e79e928d78c0
Stefan Kremer <stefan@redaktiv.de>
2017-01-11 13:49:31 +0100

    deactivated link to demo.owncloud.org
    as demo.owncloud.org went down and will be revitalized by Feb/17 link temporarily disabled

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -14,7 +14,9 @@
       <ul id="menu-header" class="nav navbar-nav">
         <li class="<?php if(is_page('news')){ echo 'top-nav-active '; } ?>menu-news"><a href="/news/">News</a></li>
         <li class="<?php if(is_page('features')){ echo 'top-nav-active '; } ?>menu-features"><a href="/features/">Features</a></li>
+<!-- 
         <li class="menu-demo"><a target="_blank" href="https://demo.owncloud.org">Demo</a></li>
+ -->
         <li class="menu-documentation"><a href="https://doc.owncloud.org">Documentation</a></li>
         <li class="<?php if(is_page('contribute')){ echo 'top-nav-active '; } ?>menu-contribute"><a href="/contribute/">Contribute</a>
         <li class="<?php if(is_page('support')){ echo 'top-nav-active '; } ?>menu-support"><a href="/support/">Support</a></li>
