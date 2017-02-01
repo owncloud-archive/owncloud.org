5f5df2a5ecfb44c57246980e8df256358e23e3f0
Peter Prochaska <info@peter-prochaska.de>
2017-02-01 11:41:14 +0100

    fixed version numbers

--- advisories/advisory-side.php
+++ advisories/advisory-side.php
@@ -1,9 +1,5 @@
-<br/><p>ownCloud server 9.1.2</p>
-<a href="/security/advisory?id=oc-sa-2016-017">SMB User Authentication Bypass</a><br/>
-<a href="/security/advisory?id=oc-sa-2016-018">Stored XSS in CardDAV image export</a><br/>
-<a href="/security/advisory?id=oc-sa-2016-019">Reflected XSS in Gallery application</a><br/>
-<a href="/security/advisory?id=oc-sa-2016-020">Content-Spoofing in "files" app</a><br/>
-<a href="/security/advisory?id=oc-sa-2016-021">Content-Spoofing in "dav" app</a><br/>
+<br/><p>ownCloud server 9.1.3</p>
+<a href="/security/advisory?id=oc-sa-2016-022">Bypass received read-only share permissions using read-write reshare</a><br/>
 <br/><p>ownCloud desktop 2.2.3</p>
 <a href="/security/advisory?id=oc-sa-2016-016">Local Code Injection</a><br/>
 <br/><p>ownCloud mobile iOS 3.4.4</p>
--- advisories/server-list-part.php
+++ advisories/server-list-part.php
@@ -1,10 +1,12 @@
-<p>Version 9.1.2</p>
+<p>Version 9.1.3</p>
+<a href="/security/advisory?id=oc-sa-2016-022">Bypass received read-only share permissions using read-write reshare</a><br>
+<br/><p>Version 9.1.2</p>
 <a href="/security/advisory?id=oc-sa-2016-017">SMB User Authentication Bypass</a><br>
 <a href="/security/advisory?id=oc-sa-2016-018">Stored XSS in CardDAV image export</a><br>
 <a href="/security/advisory?id=oc-sa-2016-019">Reflected XSS in Gallery application</a><br>
 <a href="/security/advisory?id=oc-sa-2016-020">Content-Spoofing in "files" app</a><br>
 <a href="/security/advisory?id=oc-sa-2016-021">Content-Spoofing in "dav" app</a><br>
-<br/><p>Version 9.1.*</p>
+<br/><p>Version 9.0.7</p>
 <a href="/security/advisory?id=oc-sa-2016-022">Bypass received read-only share permissions using read-write reshare</a><br>
 <br/><p>Version 9.0.6</p>
 <a href="/security/advisory?id=oc-sa-2016-017">SMB User Authentication Bypass</a><br>
@@ -24,8 +26,6 @@
 <a href="/security/advisory?id=oc-sa-2016-007">Open Redirector involving user interaction</a><br>
 <a href="/security/advisory?id=oc-sa-2016-008">Disclosure of arbitrary certificate files</a><br>
 <a href="/security/advisory?id=oc-sa-2016-009">Incorrect setup of external storage</a><br>
-<br/><p>Version 9.0.*</p>
-<a href="/security/advisory?id=oc-sa-2016-022">Bypass received read-only share permissions using read-write reshare</a><br>
 <br/><p>Version 8.2.9</p>
 <a href="/security/advisory?id=oc-sa-2016-017">SMB User Authentication Bypass</a><br>
 <br/><p>Version 8.2.7</p>
