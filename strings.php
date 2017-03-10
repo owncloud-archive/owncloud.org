0ea7fe5e3807b3f621bab519aa5818805637b67e
Vincent Petry <PVince81@owncloud.com>
2017-03-10 11:50:52 +0100

    Set 10.0.0 alpha as development version

--- strings.php
+++ strings.php
@@ -4,8 +4,8 @@
 /* VERSIONS */
 $VERSIONS_SERVER_MAJOR_STABLE = '9.1';
 $VERSIONS_SERVER_FULL_STABLE = '9.1.4';
-$VERSIONS_SERVER_MAJOR_DEVELOPMENT = '9.2';
-$VERSIONS_SERVER_MAJOR_DEV_DOCS = '9.2'; // Used in dev docs links
+$VERSIONS_SERVER_MAJOR_DEVELOPMENT = '10.0';
+$VERSIONS_SERVER_MAJOR_DEV_DOCS = '10.0'; // Used in dev docs links
 $VERSIONS_CLIENT_DESKTOP_STABLE_FULL = '2.3.0';
 $VERSIONS_CLIENT_DESKTOP_STABLE_SHORT = '2.3'; // For use in documentation link
 
@@ -63,9 +63,9 @@ $DOWNLOAD_SERVER_PACKAGES_DAILY = 'https://download.owncloud.org/download/reposi
 
 // Testing
 $DOWNLOAD_SERVER_PACKAGES_TESTING = 'https://download.owncloud.org/download/repositories/testing/owncloud';
-$SERVER_TESTING_VERSION ='9.1.4 RC2';
-$DOWNLOAD_SERVER_TAR_TESTING ='https://download.owncloud.org/community/testing/owncloud-9.1.4RC2.tar.bz2';
-$DOWNLOAD_SERVER_ZIP_TESTING ='https://download.owncloud.org/community/testing/owncloud-9.1.4RC2.zip';
+$SERVER_TESTING_VERSION ='10.0.0 alpha';
+$DOWNLOAD_SERVER_TAR_TESTING ='https://download.owncloud.org/community/testing/owncloud-10.0.0alpha.tar.bz2';
+$DOWNLOAD_SERVER_ZIP_TESTING ='https://download.owncloud.org/community/testing/owncloud-10.0.0alpha.zip';
 
 // web installer
 $DOWNLOAD_SERVER_WEB_INSTALLER = 'https://download.owncloud.com/download/community/setup-owncloud.php';
