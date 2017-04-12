01e16585b43e4de84402225980295ae9a99efc7f
lefherz <emil.lefherz@fau.de>
2017-04-12 13:39:57 +0200

    reworked events page to feature 2017 events. added an events archive page to keep track of events 2015-last year.

---
+++ page-events-archive.php
@@ -0,0 +1,238 @@
+<?php get_template_part('templates/parts/title'); ?>
+<div class="sub-nav">
+	<!-- a href="/events/meetups">Meetups</a>&nbsp;&nbsp;
+	|&nbsp;&nbsp; -->
+	<a href="/events/conference-program">Conference Program</a>
+	|&nbsp;&nbsp;<a href="/events/contactform">Contact</a>
+</div>
+<div class="row">
+	<div class="col-md-12">
+		<h2>Event Archive</h2>
+		<p>This is our event archive. Here you can find the events where we participated in the last years, beginning from 2015. For current events, take a look at our <a target="_blank" href="page-events.php">events page</a>.</p>
+
+		<h3>Events we've been at in 2016</h3>
+		<table class="table table-striped">
+			<tr>
+				<th>Event</th>
+				<th>Location</th>
+				<th>Date</th>
+				<th>Link (report)</th>
+			</tr>
+			<tr>
+				<td>ownCloud Contributor Conference 2016</td>
+				<td>Berlin, Germany</td>
+				<td>September 9-15</td>
+				<td><a target="_blank" href="https://owncloud.org/blog/you-rocked-the-owncloud-conference-2016/">report</a></td>
+			</tr>
+			<tr>
+				<td>ownCloud Hackathon at openSUSE Conference 2016</td>
+				<td>Nuremberg, Germany</td>
+				<td>June 22-23</td>
+				<td><a target="_blank" href="https://events.opensuse.org/conference/oSC16">website</a>, <a href="/hackathon">our info page</a></td>
+			</tr>
+			<tr>
+				<td>ownCloud Business User Event</td>
+				<td>Frankfurt, Germany</td>
+				<td>June 1st</td>
+				<td><a target="_blank" href="https://owncloud.org/blog/owncloud-connects-business-users/">blog</a></td>
+			</tr>
+			<tr>
+				<td>Talk at DevOps Pro conference 2016</td>
+				<td>Vilnius, Lithuania</td>
+				<td>May 26, 2016</td>
+				<td><a target="_blank" href="http://devopspro.lt/">website</a></td>
+			</tr>
+			<tr>
+				<td>Talk at NLUUG voorjaarsconferentie 2016</td>
+				<td>Bunnink/Utrecht, Netherlands</td>
+				<td>May 26, 2016</td>
+				<td><a target="_blank" href="https://www.nluug.nl/activiteiten/events/vj16/index.html">website</a></td>
+			</tr>
+			<tr>
+				<td>Workshop at OpenTechSummit 2016 Berlin</td>
+				<td>Berlin, Germany</td>
+				<td>May 5, 2016</td>
+				<td><a target="_blank" href="http://www.meetup.com/OpenXLab/events/230446672/">website</a></td>
+			</tr>
+			<tr>
+				<td>Talk at Vault conference</td>
+				<td>Rayleigh, NC, USA</td>
+				<td>April 20-21, 2016</td>
+				<td><a target="_blank" href="http://events.linuxfoundation.org/events/vault">website</a></td>
+			</tr>
+			<tr>
+				<td>PGConf US 2016</td>
+				<td>New York, USA</td>
+				<td>April 18-20, 2016</td>
+				<td><a target="_blank" href="http://www.pgconf.us/2016/">website</a></td>
+			</tr>
+			<tr>
+				<td>FOSSCOMM 2016</td>
+				<td>Pireaus (Athens), Greece</td>
+				<td>April 15-18, 2016</td>
+				<td><a target="_blank" href="http://fosscomm.cs.unipi.gr/">website</a> and <a target="_blank" href="http://eiosifidis.blogspot.gr/2016/04/opensuse-and-owncloud-at-fosscomm-2016.html">blog</a></td>
+			</tr>
+			<tr>
+				<td>Özgür Yazılım ve Linux Günleri (Free Software And Linux Days)</td>
+				<td>Istanbul Bilgi University, Turkey</td>
+				<td>March 26-26, 2016</td>
+				<td><a target="_blank" href="http://ozguryazilimgunleri.org.tr/2016/">website</a>, <a href="http://ozguryazilimgunleri.org.tr/2016/etkinlik-programi/">program</a></td>
+			</tr>
+			<tr>
+				<td>Chemnitzer Linux Tage 2016</td>
+				<td>Chemnitz, Germany</td>
+				<td>March 18-20, 2016</td>
+				<td><a target="_blank" href="http://chemnitzer.linux-tage.de/">website</a></td>
+			</tr>
+			<tr>
+				<td>Workshop introduction to ownCloud</th>
+				<td>College of Engineering, Pune (COEP)</th>
+				<td>February 6</th>
+				<td><a href="http://meetu.ps/2VgNf8">Site</a></th>
+			</tr>
+			<tr>
+				<td>Workshop on setting up your owncloud</th>
+				<td>mini Debian Conference in Mumbai</th>
+				<td>January 31st</th>
+				<td><a href="http://in2016.mini.debconf.org/#schedule">schedule</a></th>
+			</tr>
+			<tr>
+				<td>FOSDEM 2016</td>
+				<td>Brussels, Belgium</td>
+				<td>Jan 30-31, 2016</td>
+				<td><a target="_blank" href="https://archive.fosdem.org/2016/">website</a></td>
+			</tr>
+			<tr>
+				<td>SCALE 2016</td>
+				<td>Pasedena, Los Angelos, CA, USA</td>
+				<td>Jan 21-24, 2016</td>
+				<td><a target="_blank" href="http://scale16.co/">website</a></td>
+			</tr>
+		</table>
+
+		<h3>Events we've been at in 2015</h3>
+		<table class="table table-striped">
+			<tr>
+				<th>Event</th>
+				<th>Location</th>
+				<th>Date</th>
+				<th>Link (report)</th>
+			</tr>
+			<tr>
+				<td>FOSSCOMM 2015</td>
+				<td>Athens, Greece</td>
+				<td>Nov 6-8, 2015</td>
+				<td><a target="_blank" href="http://fosscomm.teiath.gr/">website</a></td>
+			</tr>
+			<tr>
+				<td>ownCloud Contributor Conference</td>
+				<td>Berlin, Germany</td>
+				<td>August 28-Sept 3 2015</td>
+				<td><a target="_blank" href="/conf/">ownCloud Contributor Conference site</a>, <a target="_blank" href="https://owncloud.org/blog/owncloud-contributor-conference-well-underway">blog post</a></td>
+			</tr>
+			<tr>
+				<td>Talk at South East Linux Fest</td>
+				<td>Charlotte, NC, USA</td>
+				<td>June 12-14 2015</td>
+				<td><a target="_blank" href="http://www.southeastlinuxfest.org/">SELF website</a></td>
+			</tr>
+			<tr>
+				<td>Keynote at DORSLUC</td>
+				<td>Zagreb, Croatia</td>
+				<td>May 18-20 2015</td>
+				<td><a target="_blank" href="http://2015.dorscluc.org/en/">DORSLUC website</a></td>
+			</tr>
+			<tr>
+				<td>ownCloud workshop at OpenTechSummit</td>
+				<td>Berlin, Germany</td>
+				<td>May 14 2015</td>
+				<td><a target="_blank" href="http://opentechsummit.net/">Open Tech Summit site</a>, <a target="_blank" href="http://opentechsummit.net/#schedule">Schedule</a></td>
+			</tr>
+			<tr>
+				<td>ownCloud workshop at TechMinistry</td>
+				<td>Thessaloniki, Greece</td>
+				<td>May 10 2015</td>
+				<td><a target="_blank" href="http://wiki.techministry.gr/index.php/OwnCloud_Workshop">wiki</a>, <a target="_blank" href="hhttps://plus.google.com/events/c78ap075bb4falrnmg3v0knmtao">G+</a> and <a target="_blank" href="https://www.facebook.com/events/1462326250725901/">Facebook</a></td>
+			</tr>
+			<tr>
+				<td>Linuxwochen 2015 Wien</td>
+				<td>Viena, Austria</td>
+				<td>May 7-9 2015</td>
+				<td><a target="_blank" href="http://www.linuxwochen.at/Wien/">website</a></td>
+			</tr>
+			<tr>
+				<td>ownCloud workshop at Happy Hacking Day 2015</td>
+				<td>Helsinki, Finland</td>
+				<td>May 7-9 2015</td>
+				<td><a target="_blank" href="http://happyhackingday.org/">Happy Hacking Day website</a> (<a target="_blank" href="http://blog.jospoortvliet.com/2015/05/owncloud-workshops-two-down-two-to-go.html">report blog</a>)</td>
+			</tr>
+			<tr>
+				<td>ownCloud talk at "Monades Aristias"</td>
+				<td>Aristotele University of Thessaloniki, Greece</td>
+				<td>May 6 2015, 19:00</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>ownCloud workshop openSUSE Conference</td>
+				<td>Den Hague, Netherlands</td>
+				<td>May 1-4 2015</td>
+				<td><a target="_blank" href="https://conference.opensuse.org/">oSC website</a>, <a target="_blank" href="https://events.opensuse.org/conference/osc15/proposal/510">the workshop</a>, <a target="_blank" href="http://blog.jospoortvliet.com/2015/05/open-tech-summit-berlin-opensuse.html">report blog</a></td>
+			</tr>
+			<tr>
+				<td>ownCloud talk, booth at FLISOL 2015, Paraguay</td>
+				<td>UPE University City Pdtes. Franco, Alto Paraná - Paraguay</td>
+				<td>April 25 2015</td>
+				<td><a target="_blank" href="http://flisol.info/FLISOL2015/Paraguay/AltoParana">AltoParana website</a></td>
+			</tr>
+			<tr>
+				<td>ownCloud talk at LinuxFest Northwest</td>
+				<td>Bellingham, WA, USA</td>
+				<td>April 25-26 2015</td>
+				<td><a target="_blank" href="http://linuxfestnorthwest.org/2015">LinuxFest Northwest website</a></td>
+			</tr>
+			<tr>
+				<td>Cloud and ownCloud presentation at GreekLUG</td>
+				<td>Thessaloniki, Greece</td>
+				<td>April 5th</td>
+				<td> <a href="https://plus.google.com/events/clde2a4kdlbfedngqlof4feds5o">Google+ event page</a> or <a href="https://www.facebook.com/events/601862629958000/">Facebook event page</a>, <a target="_blank" href="http://www.greeklug.gr/">GreekLUG site</a>, <a target="_blank" href="http://eiosifidis.blogspot.com/2015/04/how-to-choose-cloud-why-owncloud.html">blog about the event</a></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>ownCloud talk and Booth at Chemnitzer Linux Tage</td>
+				<td>Chemnitz, Germany</td>
+				<td>March 21-22 2015</td>
+				<td><a target="_blank" href="https://chemnitzer.linux-tage.de/">CLT website</a></td>
+			</tr>
+			<tr>
+				<td>ownCloud talk at LiberPlanet</td>
+				<td>MIT, Cambridge, USA</td>
+				<td>March 21-22 12015</td>
+				<td><a target="_blank" href="https://libreplanet.org/2015/">LibrePlanet website</a></td>
+			</tr>
+			<tr>
+				<td>University of the Aegean ownCloud workshop</td>
+				<td>Néon Karlóvasi, Samos, Greece</td>
+				<td>March 14, 2015</td>
+				<td><a target="_blank" href="https://www.facebook.com/events/295437027246832/">Facebook info page</a></td>
+			</tr>
+			<tr>
+				<td>SCALE 13x</td>
+				<td>Los Angelos, CA, USA</td>
+				<td>February 19-22 2015</td>
+				<td><a target="_blank" href="http://www.socallinuxexpo.org/scale13x/">SCALE website</a> (<a target="_blank" href="https://owncloud.org/blog/owncloud-at-fosdem-2015-and-scale13x/">report</a>)</td>
+			</tr>
+			<tr>
+				<td>GNUify.in</td>
+				<td>Pune, India</td>
+				<td>14 February 2015</td>
+				<td>ownCloud session: <a target="_blank" href="http://gnunify.in/event/79/51">Creating your own cloud server using Owncloud</a></td>
+			</tr>
+			<tr>
+				<td>FOSDEM 2015</td>
+				<td>Brussels, Belgium</td>
+				<td>January 31 and February 1</td>
+				<td><a target="_blank" href="https://fosdem.org/2015/">FOSDEM 2014</a> (<a target="_blank" href="https://owncloud.org/blog/owncloud-at-fosdem-2015-and-scale13x/">report</a>)</td>
+			</tr>
+		</table>
+	</div>
+</div>
