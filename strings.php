d5f27c52a47679475171236a94ab72bd8eae7637
Vincent Petry <pvince81@owncloud.com>
2017-04-18 23:02:48 +0200

    Update dev 10.0 version to RC1

--- strings.php
+++ strings.php
@@ -63,9 +63,9 @@ $DOWNLOAD_SERVER_PACKAGES_DAILY = 'https://download.owncloud.org/download/reposi
 
 // Testing
 $DOWNLOAD_SERVER_PACKAGES_TESTING = 'https://download.owncloud.org/download/repositories/testing/owncloud';
-$SERVER_TESTING_VERSION ='10.0.0 beta';
-$DOWNLOAD_SERVER_TAR_TESTING ='https://download.owncloud.org/community/testing/owncloud-10.0.0beta.tar.bz2';
-$DOWNLOAD_SERVER_ZIP_TESTING ='https://download.owncloud.org/community/testing/owncloud-10.0.0beta.zip';
+$SERVER_TESTING_VERSION ='10.0.0 RC1';
+$DOWNLOAD_SERVER_TAR_TESTING ='https://download.owncloud.org/community/testing/owncloud-10.0.0RC1.tar.bz2';
+$DOWNLOAD_SERVER_ZIP_TESTING ='https://download.owncloud.org/community/testing/owncloud-10.0.0RC1.zip';
 
 // web installer
 $DOWNLOAD_SERVER_WEB_INSTALLER = 'https://download.owncloud.com/download/community/setup-owncloud.php';
