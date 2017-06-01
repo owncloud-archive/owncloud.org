0e75e4f29574a9e68c709a9765e014b15782f866
Emil Lefherz <emil.lefherz@fau.de>
2017-06-01 15:32:22 +0200

    apparently 10.0 will not be in the stable release channel for now

--- page-changelog.php
+++ page-changelog.php
@@ -26,7 +26,7 @@ Download: <a href="https://download.owncloud.org/community/owncloud-10.0.2.tar.b
 MD5: <a href="https://download.owncloud.org/community/owncloud-10.0.2.tar.bz2.md5">owncloud-10.0.2.tar.bz2.md5</a> or <a href="https://download.owncloud.org/community/owncloud-10.0.2.zip.md5">owncloud-10.0.2.zip.md5</a></br>
 SHA256: <a href="https://download.owncloud.org/community/owncloud-10.0.2.tar.bz2.sha256">owncloud-10.0.2.tar.bz2.sha256</a> or <a href="https://download.owncloud.org/community/owncloud-10.0.2.zip.sha256">owncloud-10.0.2.zip.sha256</a></br>
 PGP (<a href="https://owncloud.org/owncloud.asc">Key</a>): <a href="https://download.owncloud.org/community/owncloud-10.0.2.tar.bz2.asc">owncloud-10.0.2.tar.bz2.asc</a> or <a href="https://download.owncloud.org/community/owncloud-10.0.2.zip.asc">owncloud-10.0.2.zip.asc</a></br>
-Packages on our Build Service: <a href="https://download.owncloud.org/download/repositories/10.0/owncloud">10.0 Release Channel</a> and <a href="https://download.owncloud.org/download/repositories/stable/owncloud/">Stable Release Channel</a>. Learn more about <a href="/release-channels">Release Channels</a>.
+Packages on our Build Service: <a href="https://download.owncloud.org/download/repositories/10.0/owncloud">10.0 Release Channel</a><!-- and <a href="https://download.owncloud.org/download/repositories/stable/owncloud/">Stable Release Channel</a>-->. Learn more about <a href="/release-channels">Release Channels</a>.
 
 <a name="latest9.1"></a></li>
 <h3>Version 9.1.6 <small>May 30 2017</small></h3></li>
@@ -123,7 +123,7 @@ Download: <a href="https://download.owncloud.org/community/owncloud-10.0.1.tar.b
 MD5: <a href="https://download.owncloud.org/community/owncloud-10.0.1.tar.bz2.md5">owncloud-10.0.1.tar.bz2.md5</a> or <a href="https://download.owncloud.org/community/owncloud-10.0.1.zip.md5">owncloud-10.0.1.zip.md5</a></br>
 SHA256: <a href="https://download.owncloud.org/community/owncloud-10.0.1.tar.bz2.sha256">owncloud-10.0.1.tar.bz2.sha256</a> or <a href="https://download.owncloud.org/community/owncloud-10.0.1.zip.sha256">owncloud-10.0.1.zip.sha256</a></br>
 PGP (<a href="https://owncloud.org/owncloud.asc">Key</a>): <a href="https://download.owncloud.org/community/owncloud-10.0.1.tar.bz2.asc">owncloud-10.0.1.tar.bz2.asc</a> or <a href="https://download.owncloud.org/community/owncloud-10.0.1.zip.asc">owncloud-10.0.1.zip.asc</a></br>
-Packages on our Build Service: <a href="https://download.owncloud.org/download/repositories/10.0/owncloud">10.0 Release Channel</a> and <a href="https://download.owncloud.org/download/repositories/stable/owncloud/">Stable Release Channel</a>. Learn more about <a href="/release-channels">Release Channels</a>.
+Packages on our Build Service: <a href="https://download.owncloud.org/download/repositories/10.0/owncloud">10.0 Release Channel</a><!-- and <a href="https://download.owncloud.org/download/repositories/stable/owncloud/">Stable Release Channel</a>-->. Learn more about <a href="/release-channels">Release Channels</a>.
 
 <h3>Version 10.0.0 <small>Apr 27 2017</small></h3></li>
 <b>General</b>
@@ -216,7 +216,7 @@ Download: <a href="https://download.owncloud.org/community/owncloud-10.0.0.tar.b
 MD5: <a href="https://download.owncloud.org/community/owncloud-10.0.0.tar.bz2.md5">owncloud-10.0.0.tar.bz2.md5</a> or <a href="https://download.owncloud.org/community/owncloud-10.0.0.zip.md5">owncloud-10.0.0.zip.md5</a></br>
 SHA256: <a href="https://download.owncloud.org/community/owncloud-10.0.0.tar.bz2.sha256">owncloud-10.0.0.tar.bz2.sha256</a> or <a href="https://download.owncloud.org/community/owncloud-10.0.0.zip.sha256">owncloud-10.0.0.zip.sha256</a></br>
 PGP (<a href="https://owncloud.org/owncloud.asc">Key</a>): <a href="https://download.owncloud.org/community/owncloud-10.0.0.tar.bz2.asc">owncloud-10.0.0.tar.bz2.asc</a> or <a href="https://download.owncloud.org/community/owncloud-10.0.0.zip.asc">owncloud-10.0.0.zip.asc</a></br>
-Packages on our Build Service: <a href="https://download.owncloud.org/download/repositories/10.0/owncloud">10.0 Release Channel</a> and <a href="https://download.owncloud.org/download/repositories/stable/owncloud/">Stable Release Channel</a>. Learn more about <a href="/release-channels">Release Channels</a>.
+Packages on our Build Service: <a href="https://download.owncloud.org/download/repositories/10.0/owncloud">10.0 Release Channel</a><!-- and <a href="https://download.owncloud.org/download/repositories/stable/owncloud/">Stable Release Channel</a>-->. Learn more about <a href="/release-channels">Release Channels</a>.
 
 <h3>Version 9.1.5 <small>Apr 18 2017</small></h3></li>
 <ul>
