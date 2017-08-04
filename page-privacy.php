2a46ab962a4edc897e9677f1dc561c4230c85630
Stefan Kremer <stefan@redaktiv.de>
2017-08-04 10:36:00 +0200

    disable constant contact

--- page-privacy.php
+++ page-privacy.php
@@ -4,7 +4,8 @@
             <h1>Privacy Policy</h1>
         </div>
         <p>We recognize that privacy is extremely important to our users. We do not share individual user data with third parties without your permission.</p>
-        <p>We use our own install of <a href="http://piwik.org/">Piwik</a> and, sometimes, other third-party analytics services on our web sites to understand how people use our web pages. Our news letter is handled via Constant Contact, see their <a href="http://www.constantcontact.com/legal/privacy-statement">privacy policy</a>. Registration for ownCloud events is handled by websites we deploy and manage ourselves. If we experience a privacy breach, we promise to disclose it as soon as possible.</p>
+        <p>We use our own install of <a href="http://piwik.org/">Piwik</a> and, sometimes, other third-party analytics services on our web sites to understand how people use our web pages.
+        <!-- Our news letter is handled via Constant Contact, see their <a href="http://www.constantcontact.com/legal/privacy-statement">privacy policy</a>. --> Registration for ownCloud events is handled by websites we deploy and manage ourselves. If we experience a privacy breach, we promise to disclose it as soon as possible.</p>
         <p>Note that content on <a href="/news">owncloud.org/news</a> is aggregated from community blogs and we are not responsible for their opinions or content.</p>
         <p>If you spot a privacy leak or see anything unseemly on our website, please report it discreetly to <a href="mailto:abuse@owncloud.com">abuse@owncloud.com</a>.</p>
         <p>ownCloud.org is run by <a href="https://owncloud.com">ownCloud GmbH</a>, Leipziger Platz 21, 90491 Nürnberg, Germany.</div>
