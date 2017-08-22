ceac9146c170e508692da7568da2524d65ae27e6
Kawohl <john@owncloud.com>
2017-08-22 18:43:07 +0200

    fix missing slashes

--- page-agreement.php
+++ page-agreement.php
@@ -4,7 +4,7 @@
 <p><a href="<?php echo get_template_directory_uri(); ?>/assets/files/owncloud-copyright-assignment-common.pdf">Download the agreement (pdf)</a><br /></p>
 <p><a href="<?php echo get_template_directory_uri(); ?>/assets/files/Addendum_iOS.pdf">iOS Addendum (pdf)</a><br /></p>
 <p>We release the ownCloud <a href="https://github.com/owncloud/core">core</a> and the <a href="https://github.com/owncloud/client">desktop</a>, <a href="https://github.com/owncloud/ios">iOS</a> and <a href="https://github.com/owncloud/android">Android</a> clients under a dual license. Because of that we require a signed
-contributor agreement from developers who want to commit code to them. This only applies to these repositories, not the apps or other parts.<p>
+contributor agreement from developers who want to commit code to them. This only applies to these repositories, not the apps or other parts.</p>
 <h3>A contributor agreement? Does this mean that ownCloud is no longer free software?</h3>
 <p>No, the contributor agreement guarantees that every community contribution is always released
 as AGPL (or GPL in the case of the desktop and Android clients and GPLv3 in case of the iOS app). This is not different from the old situation.</p>
@@ -12,15 +12,15 @@ as AGPL (or GPL in the case of the desktop and Android clients and GPLv3 in case
 This thus helps pay the bills for ownCloud contributors.</p>
 <h3>Does this mean that everybody who wants to contribute to ownCloud has to sign this agreement?</h3>
 <p>No. 
-<p>We can accept your core contribution if you release your code under the MIT license. Of course we 
-recommend that you sign the agreement because AGPL/GPL protects the freedom of your work better than MIT. But the choice is yours.<p>
+We can accept your core contribution if you release your code under the MIT license. Of course we 
+recommend that you sign the agreement because AGPL/GPL protects the freedom of your work better than MIT. But the choice is yours.</p>
 <h3>How difficult is it to sign the agreement?</h3>
 <p>Very easy. When you submit your pull request you will prompted to sign the cla by clicking a button. We Use <a href="https://cla-assistant.io/" cla-assistant.io> </a> for this.
 </p>
 <br />
 <h3>Is there any risk for me when I sign this agreement?</h3>
 <p>We don&#39;t think so. You have to give us the right to use your software patents if you have any and if they apply to your patch.
-That’s all. Probably not a problem for you because software patents are bad and we all don&#39;t hold any patents anyway.<p>
+That’s all. Probably not a problem for you because software patents are bad and we all don&#39;t hold any patents anyway.</p>
 <h3>Why do you require a contributor agreement now?</h3>
 <p>We have set up a company to speed up ownCloud development and pay the bills of ownCloud developers.
 We thought a lot about the relationship between the company and community and about a solution that is best for both parties.</p>
@@ -29,10 +29,10 @@ Many customers are willing to pay for a non-AGPL version of ownCloud, some would
 Aside from the <em>'Enterprise Edition'</em> under a closed license, we of course sell support and other services.
 The revenue is used to fund development of ownCloud, marketing, services, sponsoring and so on.</p>
 <p>The community wants to be sure that all the contributions and all the hard work stays Free Software.
-That is guaranteed by the AGPL license.<p>
+That is guaranteed by the AGPL license.</p>
 <p>And the community wants to make ownCloud better and more successful. That is where the company helps.
 ownCloud benefits enormously from the resources that the company invests into it, including coding, marketing and more.
 The company pays ownCloud coders to write features the customers need and works with the community to make ownCloud a better product.
 Because the company works in the open and as part of the community, and because the code is released under the AGPL,
 ownCloud itself continues to be free. Essentially, ownCloud GmbH customers pay for ownCloud contributors writing more AGPL code for ownCloud.
-So we think that we found a solution here which is good for all parties.<p>
+So we think that we found a solution here which is good for all parties.</p>
